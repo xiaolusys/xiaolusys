78e5418f1018575d1664e34ae66875f676a7a310
Zifei Zhong <zifei.zhong@huyi.so>
2012-02-16 18:27:27 +0800

    use mysql

--- shopmanager/settings.py
+++ shopmanager/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'shopmgr',                      # Or path to database file if using sqlite3.
-        'USER': 'meixqhi',                      # Not used with sqlite3.
+        'USER': 'shopmgr',                      # Not used with sqlite3.
         'PASSWORD': '123123',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
