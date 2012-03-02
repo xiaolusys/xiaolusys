100308300a7b54aee7585ea98cc561f2c6a83c00
meixqhi <xiuqing.mei@huyi.so>
2012-03-02 19:23:55 +0800

    fix scrawurldata.py add head line records

--- shopmanager/search/scrawurldata.py
+++ shopmanager/search/scrawurldata.py
@@ -8,8 +8,10 @@ import logging
 logger = logging.getLogger('taobao.urlcraw')
 
 baseurl = 'http://s.taobao.com/search'
-itemhref_xpath = ['/html/body/div/div/div/div/div/form/ul/li/h3/a']
-itemuser_xpath = ['/html/body/div/div/div/div/div/form/ul/li/p/a','/html/body/div/div/div/div/div/form/ul/li/ul/li/a']
+head_href_xpath = ['/html/body/div/div/div/div/div/form/div/ul/li/h3/a','/html/body/div/div/div/div/div/form/div/ul/li/h3/a']
+head_user_xpath = ['/html/body/div/div/div/div/div/form/div/ul/li/p/a','/html/body/div/div/div/div/div/form/div/ul/li/ul/li/a']
+item_href_xpath = ['/html/body/div/div/div/div/div/form/ul/li/h3/a']
+item_user_xpath = ['/html/body/div/div/div/div/div/form/ul/li/p/a','/html/body/div/div/div/div/div/form/ul/li/ul/li/a']
 
 
 def scraw_url(url):
@@ -20,25 +22,21 @@ def scraw_url(url):
     return tree
 
 
-def parseElementAttr(tree):
+def parseElementAttr(tree,itemhref_xpath,itemuser_xpath):
     merge_list = []
 
     itemhref_list = []
     itemuser_list = []
 
-    model = 'a'       #a,b instance taobao two styles page list
-
     for xpath in itemhref_xpath:
         itemhref_list = tree.xpath(xpath)
         if itemhref_list:
             break
-        model = 'b'
 
     for xpath in itemuser_xpath:
         itemuser_list = tree.xpath(xpath)
         if itemuser_list:
             break
-        model = 'b'
 
     if len(itemuser_list)>len(itemhref_list):
         for user in itemuser_list:
@@ -57,10 +55,15 @@ def parseElementAttr(tree):
         itemdict = {}
 
         o = urlparse.urlparse(itemhref.attrib['href'])
-        itemdict['model'] = model
 
         itemdict['title'] = itemhref.attrib['title']
-        itemdict['item_id'] = urlparse.parse_qs(o.query)['id'][0]
+
+        params = urlparse.parse_qs(o.query)
+        if not params.has_key('id') and params.has_key('url'):
+            o = urlparse.urlparse(params['url'][0])
+            itemdict['item_id'] = urlparse.parse_qs(o.query)['id'][0]
+        else:
+            itemdict['item_id'] = params['id'][0]
 
         o = urlparse.urlparse(itemuser.attrib['href'])
         try:
@@ -81,7 +84,11 @@ def scrawTaoBaoPage(q,page_nums):
         params ={'q':q,'s':i*40}
         req_url = '%s?%s'%(baseurl,urllib.urlencode(params))
         tree = scraw_url(req_url)
-        perpage_list = parseElementAttr(tree)
+        if i == 0:
+            head_list = parseElementAttr(tree,head_href_xpath,head_user_xpath)
+            merge_list.extend(head_list)
+
+        perpage_list = parseElementAttr(tree,item_href_xpath,item_user_xpath)
         merge_list.extend(perpage_list)
 
     return merge_list
@@ -94,10 +101,7 @@ def getTaoBaoPageRank(keyword,page_nums):
 
     for i in xrange(0,len(results)):
         item = results[i]
-        if item['model'] == 'a':
-            item['rank'] = i+3
-        elif item['model'] == 'b':
-            item['rank'] = i+4
+        item['rank'] = i+i
 
     return results
 
@@ -119,10 +123,7 @@ def getCustomShopsPageRank(nicks,keywords,page_nums):
                 item = results[i]
 
                 if item['nick'] == nick:
-                    if item['model'] == 'a':
-                        item['rank'] = i+3
-                    elif item['model'] == 'b':
-                        item['rank'] = i+4
+                    item['rank'] = i+1
                     peruser_results.append(item)
 
             nick_results[nick] = peruser_results
