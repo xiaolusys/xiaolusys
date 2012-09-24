ceb5a5c18a978468c9765865367618ae1f74f114
meixqhi <xiuqing.mei@huyi.so>
2012-09-24 15:11:46 +0800

    change taobao_gunicorn_conf.py

--- shopmanager/taobao_gunicorn_conf.py
+++ shopmanager/taobao_gunicorn_conf.py
@@ -2,4 +2,10 @@ pidfile='/home/user1/deploy/taobao/gunicorn.pid'
 daemon=True
 workers=4
 bind="127.0.0.1:9000"
+
+accesslog='/home/user1/deploy/taobao/gunicorn.out'
+access_log_format="%(h)s %(l)s %(u)s %(t)s "
+
+errorlog='/home/user1/deploy/taobao/gunicorn.err'
+loglevel='warning'
   
