3cd0a0a9724439276ada30bb76e4ff05dc7d9b7e
meixqhi <xiuqing.mei@huyi.so>
2013-05-25 23:47:42 +0800

    fix shopback/trades/models save fenxiao

--- shopmanager/shopback/trades/models.py
+++ shopmanager/shopback/trades/models.py
@@ -1143,7 +1143,7 @@ def save_fenxiao_orders_to_mergetrade(sender, trade, *args, **kwargs):
         
         first_pay_load = not merge_trade.sys_status 
         #如果交易是等待卖家发货，第一次入库，或者没有卖家收货信息，则更新其物流信息
-        if not merge_trade.receiver_name and trade.status not in ('',pcfg.TRADE_NO_CREATE_PAY):
+        if not merge_trade.receiver_name and trade.status not in ('',pcfg.TRADE_NO_CREATE_PAY,pcfg.WAIT_BUYER_PAY):
             logistics = Logistics.get_or_create(trade.user.visitor_id,tid)
             location = json.loads(logistics.location or 'null')
             
