928b1f111df23a3c74155257beaf29c7d1589299
acer1_user1 <user1@acer1.com>
2013-08-20 16:53:11 +0800

    fix insert zones

--- shopmanager/insert_zones.py
+++ shopmanager/insert_zones.py
@@ -9,10 +9,14 @@ def parse_address(f):
         
     return addrs
 
-def insertzones(fn):
+def delete_all():
+    
+    ClassifyZone.objects.all().delete()
+    
+def insert_zone(fn):
     with open(fn,'r') as f:
         pads = parse_address(f)
         
         for l in pads:
             print l[0],l[1],l[2],l[3]
-            ClassifyZone.objects.get_or_create(state=l[0],city=l[1],district=l[2],zone=l[3])
+            ClassifyZone.objects.get_or_create(state=l[0].strip(),city=l[1].strip(),district=l[2].strip(),zone=l[3].strip())
