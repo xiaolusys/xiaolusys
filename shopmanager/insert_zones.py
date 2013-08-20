8e9ea68c23a98493686ccc3315b717981eb96a4e
acer1_user1 <user1@acer1.com>
2013-08-20 15:14:21 +0800

    add insert zones

---
+++ shopmanager/insert_zones.py
@@ -0,0 +1,18 @@
+#-*- coding:utf8 -*-
+from shopapp.shipclassify.models import ClassifyZone
+
+def parse_address(f):
+    
+    addrs = []
+    for line in f.readlines():
+        addrset.append(line.decode('gbk').split(','))
+        
+    return addrs
+
+def insertzones(fn):
+    with open(fn,'r') as f:
+        pads = parse_address(f)
+        
+        for l in pads:
+            print l[0],l[1],l[2],l[3]
+            ClassifyZone.objects.get_or_create(state=l[0],city=l[1],district=l[2],zone=l[3])
