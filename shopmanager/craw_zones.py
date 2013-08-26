e3e4d8731ee123c153768bf97e7c3f86e33f1839
acer1_user1 <user1@acer1.com>
2013-08-26 23:57:06 +0800

    fix craw_zones exception

--- shopmanager/craw_zones.py
+++ shopmanager/craw_zones.py
@@ -2,10 +2,10 @@ from django.db.models import Q
 from shopapp.shipclassify.models import ClassifyZone
 from shopback.trades.models import MergeTrade
 
-def get_addr_zones(state,city,district):
-    lstate = len(state)>1 and state[0:2] or ''
-    lcity  = len(city)>1  and city[0:2]  or ''
-    ldistrict  = len(district)>1  and district[0:2]  or ''
+def get_addr_zones(s,c,d):
+    lstate = len(s)>1 and s[0:2] or ''
+    lcity  = len(c)>1  and c[0:2]  or ''
+    ldistrict  = len(d)>1  and d[0:2]  or ''
     
     if district:
         czones = ClassifyZone.objects.filter(Q(city__startswith=lcity)|Q(district__startwith=ldistrict),state__startswith=lstate)
@@ -14,14 +14,14 @@ def get_addr_zones(state,city,district):
             return czones[0].zone
         
         for czone in czones:
-            if czone.city == district or czone.district == district:
+            if czone.city == d or czone.district == d:
                 return czone.zone
         
     if city:
         czones = ClassifyZone.objects.filter(state__startswith=lstate,
                                                   city__startswith=lcity,district='')
         if czones.count():
-            return czone[0].zone
+            return czones[0].zone
     
     return ''
 
