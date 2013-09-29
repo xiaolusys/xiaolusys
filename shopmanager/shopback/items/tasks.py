398558f13fbd5419e81e281828c4b2b11e52643e
meixqhi <xiuqing.mei@huyi.so>
2013-09-29 11:35:58 +0800

    fix item tasks update item num tasks

--- shopmanager/shopback/items/tasks.py
+++ shopmanager/shopback/items/tasks.py
@@ -306,7 +306,7 @@ def updateItemNum(user_id,num_iid):
                 
             #针对小小派，测试线上库存低量促销效果
             if product.outer_id == '3116BG7':
-                sync_num = total_num > 0 and min(sync_num,product.warn_num+10) or min(sync_num,15)
+                sync_num = product_sku.warn_num > 0 and min(sync_num,product_sku.warn_num+10) or min(sync_num,15)
                 
             #同步库存数不为0，或者没有库存警告，同步数量不等于线上库存，并且店铺，商品，规格同步状态正确
             if not (sync_num == 0 and product_sku.is_assign) and sync_num != sku['quantity'] \
