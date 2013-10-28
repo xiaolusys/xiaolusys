a92c060f902f968e93448c44658bb220e65775f9
meixqhi <xiuqing.mei@huyi.so>
2013-10-28 13:53:57 +0800

    fix gen order xml func

--- shopmanager/shopapp/yunda/qrcode.py
+++ shopmanager/shopapp/yunda/qrcode.py
@@ -113,7 +113,7 @@ def gen_orders_xml(objs):
         _xml_list.append('<collection_value></collection_value><special></special>')
         _xml_list.append('<item></item><remark></remark>')
         _xml_list.append(u'<cus_area1>分拣号:%s</cus_area1>'%obj['zone'])
-        _xml_list.append('<cus_area2>宝贝亲，给5星+20字以上好评，就会有惊喜哦.</cus_area2>')
+        _xml_list.append(u'<cus_area2>宝贝亲，给5星+20字以上好评，就会有惊喜哦.</cus_area2>')
         _xml_list.append('<callback_id></callback_id>')
         _xml_list.append('<wave_no></wave_no></order>')
         
