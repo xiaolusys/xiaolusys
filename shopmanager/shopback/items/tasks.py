e371415c2364d9de5aa400173a5e69f695098359
meixqhi <xiuqing.mei@huyi.so>
2014-02-13 18:58:14 +0800

    fix items update num task

--- shopmanager/shopback/items/tasks.py
+++ shopmanager/shopback/items/tasks.py
@@ -294,17 +294,17 @@ def updateItemNum(user_id,num_iid):
                 #如果自动更新库存状态开启，并且计算后库存不等于在线库存，则更新
                 if sync_num>0 and user_percent>0:
                     sync_num = round(user_percet*sync_num)
-                elif sync_num > product_sku.remain_num:
-                    product_sku.is_assign = False
                 elif sync_num >0 and sync_num <= product_sku.warn_num:
                     total_num,user_order_num = MergeOrder.get_yesterday_orders_totalnum(item.user.id,outer_id,outer_sku_id)
                     if total_num>0 and user_order_num>0:
                         sync_num = round(float(user_order_num)/float(total_num)*sync_num)
                     elif total_num == 0:
                         item_count = Item.objects.filter(outer_id=outer_id,approve_status=pcfg.ONSALE_STATUS).count() or 1
-                        sync_num = sync_num/item_count or sync_num
+                        sync_num = int(sync_num/item_count) or sync_num
                     else:
                         sync_num = (real_num - wait_nums)>10 and 2 or 0 
+                elif sync_num > 0:
+                    product_sku.is_assign = False
                 else:
                     sync_num = 0
                 #当前同步库存值，与线上拍下未付款商品数，哪个大取哪个 
@@ -316,7 +316,6 @@ def updateItemNum(user_id,num_iid):
                 #同步库存数不为0，或者没有库存警告，同步数量不等于线上库存，并且店铺，商品，规格同步状态正确
                 if not (sync_num == 0 and product_sku.is_assign) and sync_num != sku['quantity'] \
                     and user.sync_stock and product.sync_stock and product_sku.sync_stock:
-                    sync_num = int(sync_num)
                     response = apis.taobao_item_quantity_update\
                             (num_iid=item.num_iid,quantity=sync_num,outer_id=outer_sku_id,tb_user_id=user_id)
                     item_dict = response['item_quantity_update_response']['item']
@@ -343,17 +342,17 @@ def updateItemNum(user_id,num_iid):
         #如果自动更新库存状态开启，并且计算后库存不等于在线库存，则更新
         if sync_num>0 and user_percent>0:
             sync_num = round(user_percet*sync_num)
-        elif sync_num > product.remain_num:
-            product.is_assign = False
         elif sync_num >0 and sync_num <= product.warn_num:
             total_num,user_order_num = MergeOrder.get_yesterday_orders_totalnum(item.user.id,outer_id,outer_sku_id)
             if total_num>0 and user_order_num>0:
                 sync_num = round(float(user_order_num)/float(total_num)*sync_num)
             elif total_num == 0:
                 item_count = Item.objects.filter(outer_id=outer_id,approve_status=pcfg.ONSALE_STATUS).count() or 1
-                sync_num = sync_num/item_count or sync_num
+                sync_num = int(sync_num/item_count) or sync_num
             else:
                 sync_num = (real_num - wait_nums)>10 and 2 or 0
+        elif sync_num > 0:
+            product.is_assign = False
         else:
             sync_num = 0    
             
@@ -361,7 +360,6 @@ def updateItemNum(user_id,num_iid):
         sync_num = max(sync_num,item.with_hold_quantity)
         #同步库存数不为0，或者没有库存警告，同步数量不等于线上库存，并且店铺，商品同步状态正确
         if not (sync_num == 0 and product.is_assign) and sync_num != item.num and user.sync_stock and product.sync_stock: 
-            sync_num = int(sync_num)   
             response = apis.taobao_item_update(num_iid=item.num_iid,num=sync_num,tb_user_id=user_id)
             item_dict = response['item_update_response']['item']
             Item.objects.filter(num_iid=item_dict['num_iid']).update(modified=item_dict['modified'],num=sync_num)
