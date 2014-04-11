cd2ac53cf1e0ea5a2e5e2f4f77a62fbd3f441f09
dp <dapeng.huyi@huyi.so>
2014-04-11 18:21:35 +0800

    accountComment

--- shopmanager/shopapp/comments/urls.py
+++ shopmanager/shopapp/comments/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('shopapp.comments.views',
 
     url('ignore/(?P<id>[^/]+)/$','comment_not_need_explain',name='comment_ignore'),
-    url('explain/$','explain_for_comment',name='comment_explain')
+    url('explain/$','explain_for_comment',name='comment_explain'),
+    url(r'^count/$','count'),
 )
--- shopmanager/shopapp/comments/views.py
+++ shopmanager/shopapp/comments/views.py
@@ -3,13 +3,17 @@ import json
 import datetime
 from django.http import HttpResponse
 from django.conf import settings
+from django.db.models import Sum,Count
+from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 from shopback.base.authentication import login_required_ajax
 from shopapp.comments.models import Comment
+from django.template import RequestContext 
 import logging
 
 
 logger = logging.getLogger('django.request')
+
 
 @login_required_ajax
 def comment_not_need_explain(request,id):
@@ -36,8 +40,79 @@ def explain_for_comment(request):
         return HttpResponse(json.dumps({'code':1,'error_response':exc.message}),mimetype="application/json")
     
     return  HttpResponse(json.dumps({'code':0,'response_content':'success'}),mimetype="application/json")
+    
+    
+def calcCommentCountJson(fdt,tdt):
+    """
+        {'vikey':{'20140304':2}}    
+    """
+    comment_dict = {}
+    comments = Comment.objects.filter(
+        replay_at__gte=fdt,replay_at__lte=tdt,is_reply=True)
+    
+    for c in comments:
+        try:
+            replayer = c.replayer
+        except:
+            continue
+        day_date = c.replay_at.strftime('%Y%m%d')
+        if comment_dict.has_key(replayer):
+            comment_dict[replayer][day_date] = comment_dict[replayer].get(day_date,0)+1
+        else:
+            comment_dict[replayer] = {day_date:1}
+    
+    return comment_dict   
 
-
+""" 
+   if 'fromDate' in request.POST:
+        
+        gg = [10,20,30,40,50,60,70, 280]
+        dd = [11,21,31,41,51,61,71, 287]
+    
+        data = {'gg':gg,'dd':dd}
+        dates = [20140403,20140404,20140405,20140406,20140407,20140408,20140409]
+    else:
+        vicky = [10,20,30,40,50,60,70, 280]
+        spring = [11,21,31,41,51,61,71, 287]
+    
+        data = {'vicky':vicky,'spring':spring}
+        dates = [20140403,20140404,20140405,20140406,20140407,20140408,20140409]
+    """     
+        
+@csrf_exempt
+def count(request):
+   
+    
+    content  =  request.POST
+    fromDate = content.get('fromDate')
+    toDate   = content.get('endDate')
+    
+    toDate   = toDate and datetime.datetime.strptime(toDate, '%Y%m%d').date() or datetime.datetime.now().date()
+        
+    fromDate = fromDate and datetime.datetime.strptime(fromDate, '%Y%m%d').date() or toDate - datetime.timedelta(days=1)  
+          
+    
+    commentDict = calcCommentCountJson(fromDate,toDate)
+    #print 'commentDict',commentDict
+    date_array  = []
+    resultDict  = {}
+    for d in range(0,(toDate-fromDate).days):
+        
+        day_str = (fromDate+datetime.timedelta(days=d)).strftime('%Y%m%d')
+        date_array.append(day_str)
+        
+        for name,data in commentDict.iteritems():
+            
+            day_count = data.get(day_str,0)
+            if resultDict.has_key(name):
+                resultDict[name].append(day_count)
+            else:
+                resultDict[name] = [day_count]
+            
+    for name ,vl in resultDict.iteritems():
+        vl.append(sum(vl))
+    
+    return render_to_response('comments/comment_counts.html', {'data': resultDict, 'dates':date_array},  context_instance=RequestContext(request))
 
 
 
