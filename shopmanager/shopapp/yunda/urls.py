74584863dc39ac613a3409d934518d176127ebf4
lenovo <xiuqing.mei@huyi.so>
2014-04-24 21:00:19 +0800

    fix yunda urls

--- shopmanager/shopapp/yunda/urls.py
+++ shopmanager/shopapp/yunda/urls.py
@@ -1,8 +1,18 @@
 from django.conf.urls.defaults    import patterns, include, url
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from .views import PackageByCsvFileView
+from .resources import PackageListResource
+from shopback.base.renderers  import BaseJsonRenderer
+from shopback.base.permissions import IsAuthenticated
+from shopback.base.authentication import UserLoggedInAuthentication,login_required_ajax
 
 urlpatterns = patterns('shopapp.yunda.views',
 
-    url('^$',csrf_exempt(WeixinAcceptView.as_view())),
+     (r'^impackage/$',PackageByCsvFileView.as_view(
+        resource=PackageListResource,
+        renderers=(BaseJsonRenderer,),
+        authentication=(UserLoggedInAuthentication,),
+        permissions=(IsAuthenticated,)
+    )),    
 )
