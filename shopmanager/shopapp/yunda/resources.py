2ec45e7d2de1a875a97fa6b612e0bdf05a5b6ace
lenovo <xiuqing.mei@huyi.so>
2014-04-24 21:01:41 +0800

    add yunda resource

---
+++ shopmanager/shopapp/yunda/resources.py
@@ -0,0 +1,14 @@
+#-*- coding:utf8 -*-
+__author__ = 'meixqhi'
+from djangorestframework.resources import ModelResource
+from .models import TodaySmallPackageWeight,TodayParentPackageWeight
+
+
+class PackageListResource(ModelResource):
+    """ docstring for ProductList ModelResource """
+
+    model = TodayParentPackageWeight
+    fields = ('package_id','parent_package_id','weight','upload_weight','weighted','is_jzhw','success','redirect_url') 
+    exclude = ('url',)
+    
+    
