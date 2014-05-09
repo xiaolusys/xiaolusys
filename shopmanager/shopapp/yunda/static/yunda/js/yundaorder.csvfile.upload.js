61da38b2b64a5424b989fe2ee0d11b1b484527c5
meixqhi <xiuqing.mei@huyi.so>
2014-05-09 23:00:23 +0800

    fix yunda app add order upload js and upload_yunda_package.html

---
+++ shopmanager/shopapp/yunda/static/yunda/js/yundaorder.csvfile.upload.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+       $(document.body).append('<div id="upload_csvfile" style="display:none;margin:auto;"></div>');
+       $('#upload_csvfile').append('<div class="upload_submit" ></div>');
+       $('.upload_submit').append('<div style="height:80px;width:600px;"></div>')
+       $('.upload_submit').append('<button id="attach_files" style="background:#999;color:white;width:200px;'+
+               'height:40px;display:inline;">导入客户数据CSV文件</button>')
+       $('.upload_submit').append('<span class="import_status" style="display:none;color:red;">文件导入失败，请检查文件格式！</span>')
+
+       $('.upload-csvfile').click(function(e){
+               e.preventDefault();
+               
+               $("#attach_files").upload({
+                       name: 'attach_files',
+                       action: '/app/yunda/order/upload/',
+                       enctype: 'multipart/form-data',
+                       //params: {},
+                       autoSubmit: true,
+                       //onSubmit: function() {},
+                       onComplete: function(e) {
+                               var res = $.parseJSON(e);
+                               if ( res.code == 0 && res.response_content.success){
+                                       window.location = res.response_content.redirect_url;
+                               }else{
+                                       $('.import_status').css('display','inline');
+                               }
+                       },
+                       onSelect: function() {
+                               $('.import_status').css('display','none');                      
+                       }
+               });
+               $('#upload_csvfile').dialog({title: "CSV文件导入客户订单",width:'700',height:'200'});
+       });
+});
