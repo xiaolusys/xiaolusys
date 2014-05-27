4f6f2dcb22f0fe04bebe59a3b3f58f19ad4fbc7b
meixqhi <xiuqing.mei@huyi.so>
2014-05-27 15:39:42 +0800

    add calc project code tools

---
+++ shopmanager/tools/change_tid_and_oid_to_string.py
@@ -0,0 +1,31 @@
+from shopback.trades.models import MergeTrade,MergeOrder
+
+mgo = MergeOrder.objects.filter(merge_trade__user=None)
+print mgo.count()
+for o in mgo:
+    o.delete()
+
+mgt = MergeTrade.objects.filter(user=None)
+print mgt.count()
+for m in mgt:
+    m.delete()
+
+mts = MergeTrade.objects.filter(tid='')
+mts.count()
+
+index = 10001
+oindex = 10001
+for m in mts:
+    m.tid = 'HYD%d'%index
+    m.save()
+    for o in m.merge_orders.all():
+        o.oid = 'HYO%d'%oindex
+        o.save()
+    index = index+1
+    oindex = oindex+1
+    
+mos = MergeOrder.objects.filter(oid='')
+for o in mos:
+    o.oid = 'HYO%d'%oindex
+    o.save()
+    oindex = oindex+1
