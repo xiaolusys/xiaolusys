6157215c5e12f060635332c51b980ebef4543fb1
meixqhi <xiuqing.mei@huyi.so>
2014-06-21 16:11:48 +0800

    add base service.py

---
+++ shopmanager/shopback/base/service.py
@@ -0,0 +1,41 @@
+#-*- coding:utf8 -*-
+
+class LocalService():
+    
+    def __init__(self,t):
+        raise Exception(u'方法未实现')
+    
+    @classmethod
+    def createTrade(cls,user_id,tid,*args,**kwargs):
+        raise Exception(u'方法未实现')
+    
+    @classmethod
+    def createMergeTrade(cls,trade,*args,**kwargs):
+        raise Exception(u'方法未实现')
+    
+    def payTrade(self,*args,**kwargs):
+        raise Exception(u'方法未实现')
+    
+    def sendTrade(self,*args,**kwargs):
+        raise Exception(u'方法未实现')
+    
+    def ShipTrade(self,*args,**kwargs):
+        raise Exception(u'方法未实现')
+    
+    def finishTrade(self,*args,**kwargs):
+        raise Exception(u'方法未实现')
+    
+    def closeTrade(self,*args,**kwargs):
+        raise Exception(u'方法未实现')
+    
+    def memoTrade(self,*args,**kwargs):
+        raise Exception(u'方法未实现')
+    
+    def changeTrade(self,*args,**kwargs):
+        raise Exception(u'方法未实现')
+    
+    def changeTradeOrder(self,*args,**kwargs):
+        raise Exception(u'方法未实现')
+    
+    def remindTrade(self,*args,**kwargs):
+        raise Exception(u'方法未实现')
