e59c809934da7021949604ca5d8a13fef47db10d
Zifei Zhong <zifei.zhong@huyi.so>
2014-07-31 16:25:24 +0800

    add templatetags

---
+++ shopmanager/shopapp/weixin/templatetags/custom_filter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python 
+#coding:utf-8
+
+from django import template
+
+register = template.Library()
+@register.filter(name='displayName')
+def displayName(value, arg):
+    return apply(eval('value.get_'+arg+'_display'), ())
+
