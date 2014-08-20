f40235ef0d16bc22dbcd110ef952d2ed60151b21
Zifei Zhong <zifei.zhong@huyi.so>
2014-08-20 10:38:01 +0800

    ambass effect mobiles

--- shopmanager/shopapp/weixin/views.py
+++ shopmanager/shopapp/weixin/views.py
@@ -480,6 +480,7 @@ class ReferalView(View):
                                    'referal_bonus':referal_bonus,
                                    'vipcode':vipcode, 'coupon':coupon,
                                    'payment':payment, 'num_orders':len(effect_mobiles),
+                                   'effect_mobiles':effect_mobiles,
                                    'coupon_click_count':coupon_click_count}, 
                                   context_instance=RequestContext(request))
         response.set_cookie("openid",user_openid)
