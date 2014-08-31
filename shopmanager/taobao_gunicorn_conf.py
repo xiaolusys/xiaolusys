d3e7a5ea544c553cc7c84ce05e66286cbe3dbac5
meixqhi <xiuqing.mei@huyi.so>
2014-08-31 17:49:54 +0800

    fix gunicorn conf

--- shopmanager/taobao_gunicorn_conf.py
+++ shopmanager/taobao_gunicorn_conf.py
@@ -6,11 +6,13 @@ workers=multiprocessing.cpu_count() * 2 + 1
 bind="127.0.0.1:9000"
 
 timeout=30
-worker_connections=500
+worker_connections=multiprocessing.cpu_count()*200
+max_requests=200
+backlog=multiprocessing.cpu_count()*300
 
-accesslog='/var/www/deploy/taobao/gunicorn.out'
+accesslog='/var/log/taobao/gunicorn.out'
 access_log_format="%(h)s %(l)s %(u)s %(t)s "
 
-errorlog='/var/www/deploy/taobao/gunicorn.err'
+errorlog='/var/log/taobao/gunicorn.err'
 loglevel='warning'
   
