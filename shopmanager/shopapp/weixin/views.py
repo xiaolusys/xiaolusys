0692e3536a2a4e5980efe9faef2665eb4d6e8132
Zifei Zhong <zifei.zhong@huyi.so>
2014-09-02 12:29:09 +0800

    fix

--- shopmanager/shopapp/weixin/views.py
+++ shopmanager/shopapp/weixin/views.py
@@ -598,7 +598,7 @@ class RefundRecordView(View):
             wx_users = WeiXinUser.objects.filter(mobile=mobile)
             if wx_users.count() > 0:
                 openid = wx_users[0].openid
-                orders = SampleOrder.objects.filter(user_openid=openid).filter(status__gt=0)
+                orders = SampleOrder.objects.filter(user_openid=openid).filter(status__gt=10).filter(status__lt=15)
                 if orders.count() > 0:
                     sample_order = orders[0]
 
