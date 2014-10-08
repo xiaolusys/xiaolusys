c0101309a6a8c3afac7e75b5fc01738e02e6f1ec
Zifei Zhong <zifei.zhong@huyi.so>
2014-10-08 17:20:20 +0800

    survey ratio

--- shopmanager/shopapp/weixin/views.py
+++ shopmanager/shopapp/weixin/views.py
@@ -1089,10 +1089,14 @@ class SurveyView(View):
             
         total = Survey.objects.all().count()
         choice1 = Survey.objects.filter(selection=1).count()
-        ratio1 = choice1 * 100.0/ total
-        ratio2 = 100 - ratio1
-        ratio1 = "%.2f" % ratio1
-        ratio2 = "%.2f" % ratio2
+        
+        ratio1,ratio2 = 0,0
+        if total > 0:
+            ratio1 = choice1 * 100.0/ total
+            ratio2 = 100 - ratio1
+            ratio1 = "%.2f" % ratio1
+            ratio2 = "%.2f" % ratio2
+            
         response = render_to_response('weixin/survey.html', 
                                       {"exist":exist, "total":total, "ratio1":ratio1, "ratio2":ratio2},
                                       context_instance=RequestContext(request))
