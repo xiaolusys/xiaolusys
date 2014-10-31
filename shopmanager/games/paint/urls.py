87c2922e76642f8f5a28f9c5ddc5fa97d46bb6e6
Zifei Zhong <zifei.zhong@huyi.so>
2014-10-31 10:09:41 +0800

    games.paint

---
+++ shopmanager/games/paint/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import TemplateView
+
+
+from games.paint.views import CreateAccountView
+
+
+urlpatterns = patterns('supplychain.wavepick.views',
+    #url('^$',csrf_exempt(WeixinAcceptView.as_view())),
+    
+    url(r'^createaccount/$', CreateAccountView.as_view(), 
+        name='paint_create_account'),
+)
