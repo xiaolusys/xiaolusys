69a15d84c711357776d4fc64f80004296567fcdf
meron <xiuqing.mei@huyi.so>
2014-12-03 20:26:21 +0800

    fix weixin user admin filter

--- shopmanager/shopapp/weixin/admin.py
+++ shopmanager/shopapp/weixin/admin.py
@@ -103,7 +103,7 @@ class WeiXinUserAdmin(admin.ModelAdmin):
                     ,'subscribe_time','modified','user_group','isvalid')
     
     list_filter = ('subscribe','isvalid','sex','user_group',)
-    search_fields = ['openid','nickname','mobile','vmobile']
+    search_fields = ['openid','referal_from_openid','nickname','mobile','vmobile']
     
 
 admin.site.register(WeiXinUser, WeiXinUserAdmin) 
