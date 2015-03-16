046564cc080a84050ca4529be2749a886ce51b44
meron <xiuqing.mei@huyi.so>
2015-03-16 21:35:15 +0800

    fix items permissions

--- shopmanager/shopback/items/permissions.py
+++ shopmanager/shopback/items/permissions.py
@@ -1,4 +1,4 @@
 #-*- coding:utf8 -*-
 
 def has_change_product_skunum_permission(user):
-    return user.is_superuser and user.has_perm('items.change_product_skunum')
+    return user.is_superuser or user.has_perm('items.change_product_skunum')
