1216310ff6844dc69989eaa3e2509abda7821175
meixqhi <xiuqing.mei@huyi.so>
2015-03-25 09:37:32 +0800

    change weixin views

--- shopmanager/taobao_gunicorn_conf.py
+++ shopmanager/taobao_gunicorn_conf.py
@@ -5,14 +5,14 @@ import multiprocessing
 workers=multiprocessing.cpu_count() * 2 + 1
 bind="127.0.0.1:9000"
 
-timeout=20
-worker_connections=multiprocessing.cpu_count() * 200
+timeout=60
+worker_connections=multiprocessing.cpu_count() * 500
 max_requests=500
 backlog=multiprocessing.cpu_count()*300
 
-accesslog='/var/log/taobao/gunicorn.out'
+#accesslog='/var/log/taobao/gunicorn.out'
 access_log_format="%(h)s %(l)s %(u)s %(t)s "
 
 errorlog='/var/log/taobao/gunicorn.err'
-loglevel='warning'
+loglevel='error'
   
