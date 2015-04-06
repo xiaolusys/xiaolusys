e22fc5b865efdb779f484805923028849844afbe
meron <xiuqing.mei@huyi.so>
2015-04-06 15:40:01 +0800

    fix https static  css file out link

--- shopmanager/shopback/trades/models.py
+++ shopmanager/shopback/trades/models.py
@@ -553,11 +553,11 @@ class MergeOrder(models.Model):
     title  =  models.CharField(max_length=128,blank=True,verbose_name=u'商品标题')
     price  = models.FloatField(default=0.0,verbose_name=u'单价')
 
-    sku_id = models.CharField(max_length=20,blank=True,verbose_name=u'属性编码')
+    sku_id = models.CharField(max_length=20,blank=True,verbose_name=u'规格ID')
     num = models.IntegerField(null=True,default=0,verbose_name=u'商品数量')
     
-    outer_id = models.CharField(max_length=64,blank=True,verbose_name=u'商品外部编码')
-    outer_sku_id = models.CharField(max_length=20,blank=True,verbose_name=u'规格外部编码')
+    outer_id = models.CharField(max_length=64,blank=True,verbose_name=u'商品编码')
+    outer_sku_id = models.CharField(max_length=20,blank=True,verbose_name=u'规格编码')
     
     total_fee    = models.FloatField(default=0.0,verbose_name=u'总费用')
     payment      = models.FloatField(default=0.0,verbose_name=u'实付款')
