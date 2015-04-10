6039c091e691026902b9db737120382ee4311907
Zifei Zhong <zifei.zhong@huyi.so>
2015-04-10 18:55:22 +0800

    stats view

--- shopmanager/shopapp/xiaolumm/urls.py
+++ shopmanager/shopapp/xiaolumm/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.decorators.csrf import csrf_exempt
-from .views import logclicks
+from views import logclicks
+from views import StatsView
 
 urlpatterns = patterns('',
+    url(r'^stats/$',StatsView.as_view()),
     url(r'^(?P<linkid>\d+)/$',logclicks),
 )
