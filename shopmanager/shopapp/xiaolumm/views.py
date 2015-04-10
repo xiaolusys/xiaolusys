2878dd59041a45b92d3139bfee318e031cba8730
Zifei Zhong <zifei.zhong@huyi.so>
2015-04-10 11:57:15 +0800

    bug fix

--- shopmanager/shopapp/xiaolumm/views.py
+++ shopmanager/shopapp/xiaolumm/views.py
@@ -18,7 +18,7 @@ def logclicks(request, linkid):
     openid = get_user_openid(request, code)
 
     if not valid_openid(openid):
-        redirect_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc2848fa1e1aa94b5&redirect_uri=http://weixin.huyi.so/m/t/&response_type=code&scope=snsapi_base&state=135#wechat_redirect"
+        redirect_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc2848fa1e1aa94b5&redirect_uri=http://weixin.huyi.so/m/%d/&response_type=code&scope=snsapi_base&state=135#wechat_redirect" % linkid
         return redirect(redirect_url)
 
     Clicks.objects.create(linkid=linkid,openid=openid)
