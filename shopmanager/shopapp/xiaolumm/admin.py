461b4cf626011f9f314240c1f020983b97df29b9
imeron <xiuqing.mei@huyi.so>
2015-04-13 18:20:41 +0800

    fix xiaolumm admin

--- shopmanager/shopapp/xiaolumm/admin.py
+++ shopmanager/shopapp/xiaolumm/admin.py
@@ -5,15 +5,17 @@ from .models import Clicks,XiaoluMama,AgencyLevel,CashOut
 
 
 class XiaoluMamaAdmin(admin.ModelAdmin):
-    list_display = ('pk','mobile','province','weikefu','agencylevel','manager','created','status')
+    list_display = ('id','mobile','province','weikefu','agencylevel','manager','created','status')
     list_filter = ('weikefu','agencylevel','manager','status')
-
+    search_fields = ['id','mobile','manager']
+    
 admin.site.register(XiaoluMama, XiaoluMamaAdmin) 
     
 
 class AgencyLevelAdmin(admin.ModelAdmin):
     list_display = ('category','deposit','cash','basic_rate','target','extra_rate','created')
-
+    search_fields = ['category']
+    
 admin.site.register(AgencyLevel, AgencyLevelAdmin) 
 
 
@@ -27,6 +29,8 @@ admin.site.register(Clicks, ClicksAdmin)
 
 class CashOutAdmin(admin.ModelAdmin):
     list_display = ('mobile','value','status','created')
+    list_filter  = ('status',)
+    search_fields = ['mobile']
     
 admin.site.register(CashOut, CashOutAdmin) 
 
