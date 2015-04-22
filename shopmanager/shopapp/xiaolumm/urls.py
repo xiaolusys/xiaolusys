52d3057c15f1cc61de584efedc9f90bcaa3ac583
Zifei Zhong <zifei.zhong@huyi.so>
2015-04-22 11:53:42 +0800

    xiaolumm individual data

--- shopmanager/shopapp/xiaolumm/urls.py
+++ shopmanager/shopapp/xiaolumm/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.decorators.csrf import csrf_exempt
 from views import logclicks
-from views import StatsView
+from views import StatsView,MamaStatsView
 
 urlpatterns = patterns('',
+    url(r'^$',MamaStatsView.as_view()),
     url(r'^stats/$',StatsView.as_view()),
     url(r'^(?P<linkid>\d+)/$',logclicks),
 )
--- shopmanager/shopapp/xiaolumm/views.py
+++ shopmanager/shopapp/xiaolumm/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 
 from shopapp.weixin.views import get_user_openid,valid_openid
 from shopapp.weixin.models import WXOrder
+from shopapp.weixin.service import WeixinUserService
 
 from models import Clicks, XiaoluMama
 
@@ -18,6 +19,56 @@ import datetime
 
 SHOPURL = "http://mp.weixin.qq.com/bizmall/mallshelf?id=&t=mall/list&biz=MzA5NTI1NjYyNg==&shelf_id=2&showwxpaytitle=1#wechat_redirect"
 
+class MamaStatsView(View):
+    def get(self, request):
+
+        content = request.REQUEST
+        code = content.get('code',None)
+
+        openid = get_user_openid(request, code)
+
+        if not valid_openid(openid):
+            redirect_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc2848fa1e1aa94b5&redirect_uri=http://weixin.huyi.so/m/&response_type=code&scope=snsapi_base&state=135#wechat_redirect"
+            return redirect(redirect_url)
+
+        service = WeixinUserService(openid)
+        wx_user = service._wx_user
+        
+        today = datetime.date.today()
+        time_from = datetime.datetime(today.year, today.month, today.day)
+        time_to = datetime.datetime(today.year, today.month, today.day, 23, 59, 59)
+        
+        mobile = wx_user.mobile
+
+        data = {}
+        try:
+            xlmm = XiaoluMama.objects.get(mobile=mobile)
+            clicks = Clicks.objects.filter(linkid=xlmm.pk)
+            openid_list = clicks.values("openid").distinct()
+            
+            order_num = 0
+            order_list = []
+            for item in openid_list:
+                orders = WXOrder.objects.filter(buyer_openid=item["openid"],
+                                                order_create_time__gt=time_from,
+                                                order_create_time__lt=time_to)
+                                                
+                if orders.count() > 0:
+                    order_num += 1
+                for order in orders:
+                    order_list.append(order)
+
+            click_num = len(openid_list)
+            weikefu = xlmm.weikefu
+            mobile_revised = "%s****%s" % (mobile[:3], mobile[-4:])
+
+            data = {"mobile":mobile_revised, "click_num":click_num, "weikefu":weikefu,
+                    "order_num":order_num, "order_list":order_list, "pk":xlmm.pk}
+
+            return render_to_response("mama_stats.html", data, context_instance=RequestContext(request))
+        except:
+            return render_to_response("mama_stats.html", data, context_instance=RequestContext(request))
+        
 
 class StatsView(View):
     
