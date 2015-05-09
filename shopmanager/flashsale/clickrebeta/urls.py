71a89028c56296ef758b43766011bdc59df51955
huangyan <907790764@qq.com>
2015-05-09 15:36:39 +0800

    add clickrebeta

--- shopmanager/flashsale/clickrebeta/urls.py
+++ shopmanager/flashsale/clickrebeta/urls.py
@@ -2,5 +2,5 @@ __author__ = 'yann'
 from django.conf.urls.defaults import patterns, url
 from .views import StatisticTongJi
 urlpatterns = patterns('',
-    url(r'^shopstats/$',StatisticTongJi.as_view())
+    url(r'^$',StatisticTongJi.as_view())
 )
