fcf4185be94ca82fb212923adae0b4b021392571
imeron <xiuqing.mei@huyi.so>
2015-05-13 20:54:17 +0800

    fix weixin get skuname

--- shopmanager/shopapp/weixin/models.py
+++ shopmanager/shopapp/weixin/models.py
@@ -547,7 +547,7 @@ class WXProductSku(models.Model):
         sku_name = ''
         skuid_list = [s for s in sku_id.split(';') if s.strip()]
         for sku_tair in skuid_list:
-            k_id,vid = sku_tair.strip(':')
+            k_id,vid = sku_tair.split(':')
             wx_skus = WXSkuProperty.objects.filter(sku_id=vid)
             if wx_skus.count() > 0:
                 sku_name += wx_skus[0].name
