7d25951e1f53248cad3997e573039fce9600e066
imeron <xiuqing.mei@huyi.so>
2015-05-16 22:31:06 +0800

    fix flashsale mmexam view

--- shopmanager/flashsale/mmexam/views.py
+++ shopmanager/flashsale/mmexam/views.py
@@ -5,7 +5,7 @@ import random
 from django.conf import settings
 from django.http import HttpResponse,HttpResponseRedirect  
 from django.template import RequestContext 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.decorators.csrf import csrf_exempt
 from flashsale.mmexam.models import Question,Choice,Result
 from django.shortcuts import get_object_or_404, render
@@ -23,8 +23,8 @@ def index(request):
                                                 request=request)
         
     if not valid_openid(user_openid) or not valid_openid(user_unionid):
-         redirect_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc2848fa1e1aa94b5&redirect_uri=http://weixin.huyi.so/sale/exam/&response_type=code&scope=snsapi_base&state=135#wechat_redirect"
-        #return redirect(redirect_url)
+        redirect_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc2848fa1e1aa94b5&redirect_uri=http://weixin.huyi.so/sale/exam/&response_type=code&scope=snsapi_base&state=135#wechat_redirect"
+        return redirect(redirect_url)
     #    if not user_openid  or user_openid.upper() == 'NONE':
             #render(request, 'invalid_user.html')#无效用户
 
