e4297111c4a34c4e80101219dcbb56abccc2ae1f
imeron <xiuqing.mei@huyi.so>
2015-05-28 17:48:37 +0800

    fix xiaolumm model carrydate default

--- shopmanager/flashsale/xiaolumm/models.py
+++ shopmanager/flashsale/xiaolumm/models.py
@@ -362,7 +362,7 @@ class CarryLogTest(models.Model):
                                   choices=STATUS_CHOICES,
                                   default=CONFIRMED,verbose_name=u'状态')
 
-    carry_date = models.DateField(default=datetime.date.now,verbose_name=u'业务日期')
+    carry_date = models.DateField(default=datetime.date.today,verbose_name=u'业务日期')
     created    = models.DateTimeField(auto_now_add=True,verbose_name=u'创建时间')
 
     class Meta:
