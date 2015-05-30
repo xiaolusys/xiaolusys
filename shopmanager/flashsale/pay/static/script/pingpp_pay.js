96fbe54fe9dfc4b4f6109eaccd758b0e47ef94b3
imeron <xiuqing.mei@huyi.so>
2015-05-30 21:23:54 +0800

    update flashsale pay pingpp_pay to 2.0.5

--- shopmanager/flashsale/pay/static/script/pingpp_pay.js
+++ shopmanager/flashsale/pay/static/script/pingpp_pay.js
@@ -1,14 +1,16 @@
 (function(){
 var
-  version = "2.0.2",
+  version = "2.0.5",
   hasOwn = {}.hasOwnProperty,
   PingppSDK = function(){},
   cfg = {
     PINGPP_NOTIFY_URL: 'https://api.pingxx.com/notify/charges/',
     UPACP_WAP_URL: 'https://gateway.95516.com/gateway/api/frontTransReq.do',
-    ALIPAY_WAP_URL: 'http://wappaygw.alipay.com/service/rest.htm?_input_charset=utf-8',
+    ALIPAY_WAP_URL: 'http://wappaygw.alipay.com/service/rest.htm',
     UPMP_WAP_URL: 'uppay://uppayservice/?style=token&paydata=',
     BFB_SUCCESS: '<html><head><meta name="VIP_BFB_PAYMENT" content="BAIFUBAO"></head><body></body></html>',
+    YEEPAY_WAP_URL: 'https://ok.yeepay.com/paymobile/api/pay/request',
+    YEEPAY_WAP_TEST_URL: 'http://mobiletest.yeepay.com/paymobile/api/pay/request',
     PINGPP_MOCK_URL: 'http://sissi.pingxx.com/mock.php'
   },
   channels = {
@@ -16,7 +18,8 @@ var
     upmp_wap: 'upmp_wap',
     upacp_wap: 'upacp_wap',
     bfb_wap: 'bfb_wap',
-    wx_pub: 'wx_pub'
+    wx_pub: 'wx_pub',
+    yeepay_wap: 'yeepay_wap'
   };
 
 PingppSDK.prototype = {
@@ -27,9 +30,19 @@ PingppSDK.prototype = {
 
   _jsApiParameters: {},
 
-  createPayment: function(charge_json, callback) {
+  _debug: false,
+
+  _signature: undefined,
+
+  createPayment: function(charge_json, callback, signature, debug) {
     if (typeof callback == "function") {
       this._resultCallback = callback;
+    }
+    if (typeof signature != "undefined") {
+      this._signature = signature;
+    }
+    if (typeof debug == "boolean") {
+      this._debug = debug;
     }
     var charge;
     if(typeof charge_json == "string"){
@@ -86,17 +99,35 @@ PingppSDK.prototype = {
       form_submit(cfg.UPACP_WAP_URL, 'post', credential);
     } else if (channel == channels.alipay_wap) {  // 调起支付宝手机网页支付
       credential['_input_charset'] = 'utf-8';
-      form_submit(cfg.ALIPAY_WAP_URL, 'get', credential);
+      if (typeof _AP != "undefined") {
+        var query = stringify_data(credential, channel, true);
+        _AP.pay(cfg.ALIPAY_WAP_URL + "?" + query);
+      } else {
+        form_submit(cfg.ALIPAY_WAP_URL, 'get', credential);
+      }
     } else if (channel == channels.bfb_wap) {
       if (!hasOwn.call(credential, 'url')) {
         this._innerCallback("fail", this._error("invalid_credential", "missing_field:url"));
         return;
       }
-      location.href = credential['url'] + '?' + stringify_data(credential);
+      location.href = credential['url'] + '?' + stringify_data(credential, channel);
+    } else if (channel == channels.yeepay_wap) {
+      var fields = ["merchantaccount", "encryptkey", "data"];
+      for(var k in fields){
+        if(typeof fields[k] !== "function" && !hasOwn.call(credential, fields[k])){
+          this._innerCallback("fail", this._error("invalid_credential", "missing_field_"+fields[k]));
+          return;
+        }
+      }
+      if (hasOwn.call(credential, "mode") && credential["mode"] == "test") {
+        location.href = cfg.YEEPAY_WAP_TEST_URL + '?' + stringify_data(credential, channel, true);
+      } else {
+        location.href = cfg.YEEPAY_WAP_URL + '?' + stringify_data(credential, channel, true);
+      }
     } else if (channel == channels.wx_pub) {
       var fields = ["appId", "timeStamp", "nonceStr", "package", "signType", "paySign"];
-      for(var k in fields){
-        if(!hasOwn.call(credential, fields[k])){
+      for (var k in fields) {
+        if (typeof fields[k] !== "function" && !hasOwn.call(credential, fields[k])) {
           this._innerCallback("fail", this._error("invalid_credential", "missing_field_"+fields[k]));
           return;
         }
@@ -127,13 +158,52 @@ PingppSDK.prototype = {
 
   _callpay: function(){
     var self = this;
-    if (typeof WeixinJSBridge == "undefined"){
+    if (typeof wx != "undefined" && typeof self._signature != "undefined") {
+      var wxConfigFailed = false;
+      wx.config({
+        debug: self._debug,
+        appId: self._jsApiParameters["appId"],
+        timestamp: self._jsApiParameters["timeStamp"],
+        nonceStr: self._jsApiParameters["nonceStr"],
+        signature: self._signature,
+        jsApiList: ['chooseWXPay']
+      });
+      wx.ready(function(){
+        if (wxConfigFailed) {
+          return;
+        }
+        wx.chooseWXPay({
+          timestamp: self._jsApiParameters["timeStamp"],
+          nonceStr: self._jsApiParameters["nonceStr"],
+          "package": self._jsApiParameters["package"],
+          signType: self._jsApiParameters["signType"],
+          paySign: self._jsApiParameters["paySign"],
+          success: function(res) {
+            if (res.errMsg == "chooseWXPay:ok") {
+              self._innerCallback("success");
+            } else {
+              self._innerCallback("fail", self._error("wx_result_fail", res.errMsg));
+            }
+          },
+          cancel: function(res) {
+            self._innerCallback("cancel");
+          },
+          fail: function(res) {
+            self._innerCallback("fail", self._error("wx_result_fail", res.errMsg));
+          }
+        });
+      });
+      wx.error(function(res){
+        wxConfigFailed = true;
+        self._innerCallback("fail", self._error("wx_config_error", res.errMsg));
+      });
+    } else if (typeof WeixinJSBridge == "undefined") {
       function eventCallback(){
         self._jsApiCall();
       }
-      if(document.addEventListener){
+      if (document.addEventListener) {
         document.addEventListener('WeixinJSBridgeReady', eventCallback, false);
-      }else if(document.attachEvent){
+      } else if(document.attachEvent) {
         document.attachEvent('WeixinJSBridgeReady', eventCallback);
         document.attachEvent('onWeixinJSBridgeReady', eventCallback);
       }
@@ -152,8 +222,8 @@ PingppSDK.prototype = {
   },
 
   _innerCallback: function(result, err) {
-    if(typeof this._resultCallback == "function"){
-      if(typeof err == "undefined"){
+    if (typeof this._resultCallback == "function") {
+      if (typeof err == "undefined") {
         err = this._error();
       }
       this._resultCallback(result, err);
@@ -218,13 +288,19 @@ function form_submit(url, method, params) {
   form.submit();
 }
 
-function stringify_data(data) {
+function stringify_data(data, channel, urlencode) {
+  if (typeof urlencode == "undefined") {
+    urlencode = false;
+  }
   var output = [];
   for (var i in data) {
-    if (i == 'url') {
+    if (channel == "bfb_wap" && i == "url") {
       continue;
     }
-    output.push(i + '=' + data[i]);
+    if (channel == "yeepay_wap" && i == "mode") {
+      continue;
+    }
+    output.push(i + '=' + (urlencode ? encodeURIComponent(data[i]) : data[i]));
   }
   return output.join('&');
 }
