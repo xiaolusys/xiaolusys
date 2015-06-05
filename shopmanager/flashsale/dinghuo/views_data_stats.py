68a339d88b670d102809bd91d6c391cc7a180e4b
huangyan <907790764@qq.com>
2015-06-05 19:19:23 +0800

    [xlmm][flashsale][dinghuo] add delay

--- shopmanager/flashsale/dinghuo/views_data_stats.py
+++ shopmanager/flashsale/dinghuo/views_data_stats.py
@@ -12,10 +12,10 @@ class DailyStatsView(View):
         try:
             prev_day = int(prev_day)
             task_stats_daily_order_by_group.delay(prev_day)
-            function_of_task.get_daily_order_stats(prev_day)
-            function_of_task.get_daily_ding_huo_stats(prev_day)
-            function_of_task.get_daily_goods_arrival_stats(prev_day)
-            function_of_task.get_daily_out_order_stats(prev_day)
+            function_of_task.get_daily_order_stats.delay(prev_day)
+            function_of_task.get_daily_ding_huo_stats.delay(prev_day)
+            function_of_task.get_daily_goods_arrival_stats.delay(prev_day)
+            function_of_task.get_daily_out_order_stats.delay(prev_day)
         except:
             return HttpResponse("False")
         return HttpResponse(prev_day)
