54f116d3e6ebc4de784befa7d661777106960ec9
huangyan <907790764@qq.com>
2015-06-06 11:59:30 +0800

    [xlmm][flashsale][dinghuo] add daily stats

--- shopmanager/flashsale/dinghuo/views_data_stats.py
+++ shopmanager/flashsale/dinghuo/views_data_stats.py
@@ -2,8 +2,8 @@
 __author__ = 'yann'
 from django.views.generic import View
 from django.shortcuts import HttpResponse
-from flashsale.dinghuo.tasks import task_stats_daily_order_by_group
-import function_of_task
+from flashsale.dinghuo.tasks import task_stats_daily_order_by_group,task_stats_daily_product
+
 
 
 class DailyStatsView(View):
@@ -12,10 +12,7 @@ class DailyStatsView(View):
         try:
             prev_day = int(prev_day)
             task_stats_daily_order_by_group.delay(prev_day)
-            function_of_task.get_daily_order_stats.delay(prev_day)
-            function_of_task.get_daily_ding_huo_stats.delay(prev_day)
-            function_of_task.get_daily_goods_arrival_stats.delay(prev_day)
-            function_of_task.get_daily_out_order_stats.delay(prev_day)
+            task_stats_daily_product.delay(prev_day)
         except:
             return HttpResponse("False")
         return HttpResponse(prev_day)
