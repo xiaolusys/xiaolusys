af6522ae5dfd4b93f00f36b1d99d05ea389131d6
imeron <xiuqing.mei@huyi.so>
2015-06-10 20:46:17 +0800

    fix xiaolumm mama register

---
+++ shopmanager/flashsale/xiaolumm/xlmm_kefukeys.py
@@ -0,0 +1,49 @@
+
+KEY_LIST = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+
+KEY_MAP = [  
+         'qianxun01',
+        'ningmeng02',
+            'qiqi03',
+         'chengzi04',
+          'xiaoyi05',
+           'xibao06',
+        'tongtong07',
+#           'yunyun08',
+        'huanhuan09',
+          'lanlan10',
+#         'mingming11',
+        'jingjing12',
+        'yuanyuan13',
+           'xinyu14',
+        'mengmeng15'
+           ]
+
+NAME_KEY_MAP = {'dapeng.song':'qianxun01',
+                'hua.yan':'xinyu14',
+                'jasmine.jiang':'chengzi04',
+                'xiaobo.dai':'huanhuan09',
+                'xiaoguang.huo':'lanlan10',
+                'yechu.li':'jingjing12',
+                'yu.xia':'lanlan10',
+                'zhehui.zhou':'mengmeng15'
+                }
+
+IMG_URL_PREFIX = "https://mmbiz.qlogo.cn/mmbiz/"
+KEY_URLS_MAP = {
+         'qianxun01':'yMhOQPTKhLvZc2FUku1FliaQ8B8SqeF6Q55G8shIVOgzIpUI5bQ6hF7BJ9ic7DFmCUSTTA7uO9491pvEY9hdbTTg/0',
+        'ningmeng02':'yMhOQPTKhLt8UfGVxAqDTnhPOxglygBpTQoyJU7SPkpD8uQDZta0IhGUSA7CDCaJJdtXOicHVicfHGI7jmuTV0zQ/0',
+            'qiqi03':'yMhOQPTKhLt8UfGVxAqDTnhPOxglygBp3iaiacMzibmULmM4qWcybzPHZAnojDz9jHEeibhWkibm4TZRLGjKIo91Obg/0',
+         'chengzi04':'yMhOQPTKhLvCTbjPEx63GRglflnPllY1jrufW2eTb3KhQaFpEIBSjBN69qAleIUsWCv87L8iaDtKfQBIdMfh1icg/0',
+          'xiaoyi05':'yMhOQPTKhLtT6gXc7vVWZrYUYJMtKDF2lPkliaGqibfLS08S4GlBm0YsL2w9fk5ticxuGpz1ls8frEWsB9KZlh8dg/0',
+           'xibao06':'yMhOQPTKhLtT6gXc7vVWZrYUYJMtKDF2KD1IfOrUib5lG1gXlFmxUYXK6wH2R2t6GTr3Z8NFHLqIibe0DB5HklvA/0',
+        'tongtong07':'yMhOQPTKhLtwVNgtZuNUJnbO5P487z38Pe5WL4JsCFwVdFpYxwtyVJiaia9q6klDyIC97at5ylLuzNKcqCy6YbeA/0',
+        'yunyun08':'yMhOQPTKhLt8UfGVxAqDTnhPOxglygBpswRlutibR5EJBu4ian97b5OXGY8uLO4f5B7ibBlCQLAfjmKJrrjzaSq8g/0',
+        'huanhuan09':'yMhOQPTKhLv8BwrLsqGqIG0C2TSeic1GpM2VnOv7HXTQNy4CiapxKsWOw8LaTQz3iaib6Xe0yCmwyCK9NK52XgzckA/0',
+          'lanlan10':'yMhOQPTKhLtwVNgtZuNUJnbO5P487z38ibFIicEQGCicVyOgK3a5xrzfmcvxm1hnec1zDicOicOpJTTJo9OpdPOEvaQ/0',
+        'mingming11':'yMhOQPTKhLvCTbjPEx63GRglflnPllY1DxoOIb3SA8OLN91yQLv4TjmTeNDLOl2IDKBibfib2oEaHrX3jkolMFwg/0',
+        'jingjing12':'yMhOQPTKhLv8BwrLsqGqIG0C2TSeic1GpnGTxt7Qic6eZmtyRaxJich6Yvro6M1KCQKowzROJY7EM4fwAkNicaq6KQ/0',
+        'yuanyuan13':'yMhOQPTKhLtT6gXc7vVWZrYUYJMtKDF2qhtRnKNGeL07WI3YC8zxHrYMLQ3iaXDdQGs4bkfkS2GVWzteUPkbjbg/0/0',
+           'xinyu14':'yMhOQPTKhLtT6gXc7vVWZrYUYJMtKDF22Y6rP78D034rRCjtX8LAPHFd4C626tLTx2TQOufVgsaxB5k9MdmXCg/0',
+        'mengmeng15':'yMhOQPTKhLvCTbjPEx63GRglflnPllY1dSIAK1IaVs6upZv6dISpUGXsxfToCSg83A0gF9TibAMsic9lfoRicTWsw/0'
+        }
