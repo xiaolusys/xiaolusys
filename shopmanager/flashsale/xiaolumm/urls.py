8da6efb9fbe574fb445cc91a74596f967d700d35
linjie <1051631394@qq.com>
2015-06-12 16:40:50 +0800

    put stragiht the file ,make it read easyly

--- shopmanager/flashsale/xiaolumm/urls.py
+++ shopmanager/flashsale/xiaolumm/urls.py
@@ -3,7 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.admin.views.decorators import staff_member_required  
 
 
-from . import views
+from . import views ,views_duokefu
 from .views_register import MamaRegisterView,MamaConfirmView
 
 
@@ -27,12 +27,12 @@ urlpatterns = patterns('',
     url(r'^mama_verify/$',staff_member_required(views.mama_Verify),name="mama_verify"),
     url(r'^mama_verify_action/$',staff_member_required(views.mama_Verify_Action),name="mama_verify_action"),
 
-    url(r'^duokefu_customer/$',views.kf_Customer,name="kf_Customer"),
-    url(r'^duokefu_search/$',views.kf_Search_Page,name="kf_Search_Page"),
-    url(r'^duokefu_search_by_mobile/$',views.kf_Search_Order_By_Mobile,name="search_Order_By_Mobile"),
-    url(r'^duokefu_weixin_order/$',views.kf_Weixin_Order,name="weixin_Order"),
-    url(r'^duokefu_order_detail/$',views.kf_Search_Order_Detail,name="kf_Search_Order_Detail"),
-    url(r'^duokefu_find_more/$',views.ke_Find_More_Weixin_Order,name="ke_Find_More_Weixin_Order"),
+    url(r'^duokefu_customer/$',views_duokefu.kf_Customer,name="kf_Customer"),
+    url(r'^duokefu_search/$',views_duokefu.kf_Search_Page,name="kf_Search_Page"),
+    url(r'^duokefu_search_by_mobile/$',views_duokefu.kf_Search_Order_By_Mobile,name="search_Order_By_Mobile"),
+    url(r'^duokefu_weixin_order/$',views_duokefu.kf_Weixin_Order,name="weixin_Order"),
+    url(r'^duokefu_order_detail/$',views_duokefu.kf_Search_Order_Detail,name="kf_Search_Order_Detail"),
+    url(r'^duokefu_find_more/$',views_duokefu.ke_Find_More_Weixin_Order,name="ke_Find_More_Weixin_Order"),
 
 
     url(r'^top50/click/$', views.xlmm_Click_Top, name="xlmm_Click_Top"),
