51c14a39c8b8298ae925bad3413d650b54938245
imeron <xiuqing.mei@huyi.so>
2015-06-12 17:47:50 +0800

    fix clickcount and clickrebeta

--- shopmanager/flashsale/clickcount/admin.py
+++ shopmanager/flashsale/clickcount/admin.py
@@ -10,7 +10,7 @@ class ClickCountAdmin(admin.ModelAdmin):
 
     list_display_links = ['linkid', 'username']
     list_filter = ('date', 'username')
-    
+    date_hierarchy = 'date'
     search_fields = ['=linkid','=mobile']
     
 admin.site.register(ClickCount, ClickCountAdmin)
