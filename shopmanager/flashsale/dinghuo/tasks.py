6ee823732ad3156272e8f4b29a0d4a93faaa4db8
huangyan <907790764@qq.com>
2015-06-29 13:04:32 +0800

    [xlmm][flashsale][point] add point stats

--- shopmanager/flashsale/dinghuo/tasks.py
+++ shopmanager/flashsale/dinghuo/tasks.py
@@ -9,6 +9,7 @@ import datetime
 import function_of_task
 import urllib2
 import re
+from django.db import connection
 
 
 @task(max_retry=3, default_retry_delay=5)
@@ -159,6 +160,8 @@ from flashsale.dinghuo.models_user import MyUser, MyGroup
 @task(max_retry=3, default_retry_delay=5)
 def task_daily_stat_group_point():
     try:
+        today = datetime.date.today()
+        """统计一段时间内的订货单号得分情况"""
         all_ding_huo = OrderList.objects.exclude(status=u'作废').exclude(status=u'7')
         for item in all_ding_huo:
             content = "D" + str(item.id)
@@ -173,7 +176,17 @@ def task_daily_stat_group_point():
                     record_point[0].get_point = 0
                 record_point[0].record_time = item.created
                 record_point[0].save()
-
+        target_week_from = today - datetime.timedelta(days=today.weekday())
+        prev_week_from = today - datetime.timedelta(days=today.weekday() + 7)
+        prev_week_to = today - datetime.timedelta(days=today.weekday() + 1)
+        user_a = MyUser.objects.values("user__username").filter(group=1)
+        user_b = MyUser.objects.values("user__username").filter(group=2)
+        user_c = MyUser.objects.values("user__username").filter(group=3)
+        user_a = [val["user__username"] for val in user_a]
+        user_b = [val["user__username"] for val in user_b]
+        user_c = [val["user__username"] for val in user_c]
+        save_point_by_time(target_week_from, today, user_a, user_b, user_c)
+        save_point_by_time(prev_week_from, prev_week_to, user_a, user_b, user_c)
     except Exception, exc:
         raise task_daily_stat_group_point.retry(exc=exc)
 
@@ -211,3 +224,130 @@ def get_reach_standard_by_item(ding_huo):
             return False
     else:
         return False
+
+
+def get_sale_amount_by_product(product):
+    shelve_from = product.sale_time
+    time_to = shelve_from + datetime.timedelta(days=14)
+    pro_outer_id = product.outer_id
+    cost = product.cost or 0
+    agent_price = product.agent_price or 0
+    order_sql = "select id,outer_id,sum(num) as sale_num from " \
+                "shop_trades_mergeorder where sys_status='IN_EFFECT' " \
+                "and merge_trade_id in (select id from shop_trades_mergetrade where type not in ('reissue','exchange') " \
+                "and status in ('WAIT_SELLER_SEND_GOODS','WAIT_BUYER_CONFIRM_GOODS','TRADE_BUYER_SIGNED','TRADE_FINISHED') " \
+                "and sys_status not in('INVALID','ON_THE_FLY') " \
+                "and id not in (select id from shop_trades_mergetrade where sys_status='FINISHED' and is_express_print=False))" \
+                "and gift_type !=4 " \
+                "and (pay_time between '{0}' and '{1}') " \
+                "and char_length(outer_id)>=9 " \
+                "and outer_id=420 " \
+                "group by outer_id".format(shelve_from, time_to, pro_outer_id)
+    print order_sql
+    try:
+        cursor = connection.cursor()
+        cursor.execute(order_sql)
+        raw = cursor.fetchall()
+    finally:
+        cursor.close()
+    if len(raw) > 0:
+        sale_num = raw[2]
+        sale_amount = sale_num * agent_price
+        gain_amount = sale_num * (agent_price - cost)
+        if sale_num > 5000:
+            point = 3
+        elif sale_num > 2000:
+            point = 1
+        else:
+            point = 0
+        data_dict = {"pro_id": pro_outer_id, "sale_num": sale_num, "point": point, "sale_amount": sale_amount,
+                     "gain_amount": gain_amount}
+    else:
+        data_dict = {"pro_id": pro_outer_id, "sale_num": 0, "point": 0, "sale_amount": 0, "gain_amount": 0}
+    return data_dict
+
+
+def save_point_by_time(time_from, time_to, user_a, user_b, user_c):
+    all_product_a = Product.objects.filter(sale_time__range=(time_from, time_to)).filter(sale_charger__in=user_a)
+    all_product_b = Product.objects.filter(sale_time__range=(time_from, time_to)).filter(sale_charger__in=user_b)
+    all_product_c = Product.objects.filter(sale_time__range=(time_from, time_to)).filter(sale_charger__in=user_c)
+    total_sale = {"A-sale": 0, "B-sale": 0, "C-sale": 0, "A-gain": 0, "B-gain": 0, "C-gain": 0}
+    for product in all_product_a:
+        product_dict = get_sale_amount_by_product(product)
+        total_sale["A-sale"] += product_dict["sale_amount"]
+        total_sale["A-gain"] += product_dict["gain_amount"]
+        point = product_dict["point"]
+        content = product_dict["pro_id"] + "-" + time_from.strftime("%Y%m%d")
+        record_point = RecordGroupPoint.objects.get_or_create(point_type=u'2', point_content=content)
+        my_user = MyUser.objects.filter(user__username=product.sale_charger)
+        if my_user.count() > 0:
+            record_point[0].group_id = my_user[0].group.id
+            record_point[0].group_name = my_user[0].group.name
+            record_point[0].get_point = point
+            record_point[0].record_time = product.sale_time
+            record_point[0].save()
+    for product in all_product_b:
+        product_dict = get_sale_amount_by_product(product)
+        total_sale["B-sale"] += product_dict["sale_amount"]
+        total_sale["B-gain"] += product_dict["gain_amount"]
+        point = product_dict["point"]
+        content = product_dict["pro_id"] + "-" + time_from.strftime("%Y%m%d")
+        record_point = RecordGroupPoint.objects.get_or_create(point_type=u'2', point_content=content)
+        my_user = MyUser.objects.filter(user__username=product.sale_charger)
+        if my_user.count() > 0:
+            record_point[0].group_id = my_user[0].group.id
+            record_point[0].group_name = my_user[0].group.name
+            record_point[0].get_point = point
+            record_point[0].record_time = product.sale_time
+            record_point[0].save()
+    for product in all_product_c:
+        product_dict = get_sale_amount_by_product(product)
+        total_sale["C-sale"] += product_dict["sale_amount"]
+        total_sale["C-gain"] += product_dict["gain_amount"]
+        point = product_dict["point"]
+        content = product_dict["pro_id"] + "-" + time_from.strftime("%Y%m%d")
+        record_point = RecordGroupPoint.objects.get_or_create(point_type=u'2', point_content=content)
+        my_user = MyUser.objects.filter(user__username=product.sale_charger)
+        if my_user.count() > 0:
+            record_point[0].group_id = my_user[0].group.id
+            record_point[0].group_name = my_user[0].group.name
+            record_point[0].get_point = point
+            record_point[0].record_time = product.sale_time
+            record_point[0].save()
+    if total_sale["A-sale"] > total_sale["B-sale"] and total_sale["A-sale"] > total_sale["C-sale"]:
+        group_id = 1
+        group_name = "采购A"
+    elif total_sale["B-sale"] > total_sale["C-sale"]:
+        group_id = 2
+        group_name = "采购B"
+    elif total_sale["C-sale"] > total_sale["B-sale"]:
+        group_id = 3
+        group_name = "采购C"
+    else:
+        return ""
+
+    content = "X"+time_from.strftime("%Y")+time_from.strftime("%W")
+    record_point = RecordGroupPoint.objects.get_or_create(point_type=u'2', point_content=content)
+    record_point[0].group_id = group_id
+    record_point[0].group_name = group_name
+    record_point[0].get_point = 5
+    record_point[0].record_time = time_to
+    record_point[0].save()
+    if total_sale["A-gain"] > total_sale["B-gain"] and total_sale["A-gain"] > total_sale["C-gain"]:
+        group_id = 1
+        group_name = "采购A"
+    elif total_sale["B-gain"] > total_sale["C-gain"]:
+        group_id = 2
+        group_name = "采购B"
+    elif total_sale["C-gain"] > total_sale["B-gain"]:
+        group_id = 3
+        group_name = "采购C"
+    else:
+        return ""
+    content = "M"+time_from.strftime("%Y")+time_from.strftime("%W")
+    record_point = RecordGroupPoint.objects.get_or_create(point_type=u'2', point_content=content)
+    record_point[0].group_id = group_id
+    record_point[0].group_name = group_name
+    record_point[0].get_point = 5
+    record_point[0].record_time = time_to
+    record_point[0].save()
