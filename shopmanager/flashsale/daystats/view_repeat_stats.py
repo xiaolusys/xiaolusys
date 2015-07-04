b13f689e56855a3fbb41cb15fdc7b100ba96a769
huangyan <907790764@qq.com>
2015-07-04 13:50:20 +0800

    [xlmm][flashsale][repeat] add all user

--- shopmanager/flashsale/daystats/view_repeat_stats.py
+++ shopmanager/flashsale/daystats/view_repeat_stats.py
@@ -1,3 +1,4 @@
+from __future__ import division
 # coding=utf-8
 __author__ = 'yann'
 from django.views.generic import View
@@ -43,6 +44,7 @@ class StatsRepeatView(View):
         stats_month_range = range(start_month, end_month)
         month_range = range(start_month + 1, end_month + 1)
         result_data_list = []
+        all_result_data_list = []
         try:
             for target_month in stats_month_range:
                 month_date_begin = datetime.datetime(start_date.year, target_month, 1)
@@ -54,12 +56,12 @@ class StatsRepeatView(View):
                 cursor = connection.cursor()
                 cursor.execute(user_sql)
                 user_data = cursor.fetchall()
+                all_user_num = len(user_data)
 
                 """找出目标月之前的所有用户"""
                 old_user_sql = 'select openid from flashsale_tongji_shopping where shoptime<="{0}" and shoptime>="{1}" group by openid'.format(
                     month_date_begin, month_march)
                 cursor.execute(old_user_sql)
-                print(old_user_sql)
                 old_user_data = cursor.fetchall()
 
                 new_user = get_new_user(user_data, old_user_data)
@@ -67,7 +69,6 @@ class StatsRepeatView(View):
                 result_data_dict = {"month": target_month, "new_user": new_user_quantity}
                 user_data_list = []
                 for i in month_range:
-                    temp_dict = {}
                     if target_month >= i:
                         user_data_list.append("None")
                     else:
@@ -82,9 +83,28 @@ class StatsRepeatView(View):
                         user_data_list.append(temp_dict)
                 result_data_dict["user_data"] = user_data_list
                 result_data_list.append(result_data_dict)
+
+                all_user_data_list = []
+                all_result_data_dict = {"month": target_month, "all_user_num": all_user_num}
+                print user_data
+                for i in month_range:
+                    if target_month >= i:
+                        all_user_data_list.append("None")
+                    else:
+                        all_user_data = [val[0] for val in user_data]
+                        stats_date_begin = datetime.datetime(start_date.year, i, 1)
+                        stats_date_end = datetime.datetime(start_date.year, i + 1, 1)
+                        count_month = StatisticsShopping.objects.filter(
+                            shoptime__range=(stats_date_begin, stats_date_end)).filter(openid__in=all_user_data).values(
+                            'openid').distinct().count()
+                        temp_dict = {"num": count_month, "rec_num": float(
+                            '%0.2f' % (count_month * 100 / all_user_num if all_user_num else 0))}
+                        all_user_data_list.append(temp_dict)
+                all_result_data_dict["user_data"] = all_user_data_list
+                all_result_data_list.append(all_result_data_dict)
         finally:
             cursor.close()
         return render_to_response("xiaolumm/data2repeatshop.html",
-                                  {"all_data": result_data_list, "start_date": start_date,
-                                   "end_date": end_date, "month_range": month_range},
+                                  {"all_data": result_data_list, "all_result_data_list": all_result_data_list,
+                                   "start_date": start_date, "end_date": end_date, "month_range": month_range},
                                   context_instance=RequestContext(request))
