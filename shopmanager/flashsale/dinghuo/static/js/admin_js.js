a51c32aa905b41846cb45cc1bb88427ddc48f1f5
huangyan <907790764@qq.com>
2015-07-11 10:16:24 +0800

    [xlmm][flashsale][dinghuo] change display

--- shopmanager/flashsale/dinghuo/static/js/admin_js.js
+++ shopmanager/flashsale/dinghuo/static/js/admin_js.js
@@ -4,17 +4,16 @@
 function show_pic(id) {
     var gallery = $('.popup-gallery');
     var gallery_img = $('.popup-gallery a');
-    gallery_img.remove()
+    gallery_img.remove();
     $.get('/sale/dinghuo/show_pic/' + id, function (result_data) {
         var obj = result_data.split(",");
         $.each(obj, function (index, value) {
-            gallery.append('<a class="example-image-link" style="display:None" href="'+ value +'" data-lightbox="example-1">ttt</a>');
+            gallery.append('<a class="example-image-link" style="display:None" href="' + value + '" data-lightbox="example-1">ttt</a>');
         });
         var example = $(".example-image-link");
-        if(example.length >0){
+        if (example.length > 0) {
             example.trigger("click");
         }
-
     });
 
 
