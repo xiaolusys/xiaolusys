6028d33fb852c3d13b1161b75b8b8f5b1187026b
imeron <xiuqing.mei@huyi.so>
2015-07-23 17:58:01 +0800

    [weixin]fix views

--- shopmanager/shopapp/weixin/views.py
+++ shopmanager/shopapp/weixin/views.py
@@ -1079,7 +1079,7 @@ class ResultView(View):
         url_key = KFKEYS[idx]
         kefu_url = IMG_URL_PREFIX + KFMAP[url_key]
 
-        sample_kefu_url = IMG_URL_PREFIX + KFMAP['tangbao'] #[KFMAP['xiangxiang0'],KFMAP['sisi40']][sidx]
+        sample_kefu_url = IMG_URL_PREFIX + [KFMAP['tangbao'],KFMAP['xiaoxi44']][sidx]
 
         response = render_to_response('weixin/invite_result1.html',
                                       {'wx_user':wx_user,
