7e1a848bc0d5162014e18f4b480238d18ded93d7
imeron <xiuqing.mei@huyi.so>
2015-07-30 19:57:09 +0800

    [trades]fix logistic serializer

--- shopmanager/shopback/trades/serializers.py
+++ shopmanager/shopback/trades/serializers.py
@@ -11,21 +11,21 @@ class MergeTradeSerializer(serializers.ModelSerializer):
 #             'buyer_message','seller_memo','sys_memo','pay_time','modified'
 #             ,'created','consign_time','out_sid','status','sys_status',
 #             'receiver_name')
-        exclude=('created',)
+        exclude=()
 
 class LogisticsCompanySerializer(serializers.ModelSerializer):
     """ docstring for RefundResource """
     class Meta:
         model = LogisticsCompany
         #fields = (
-        exclude=('id',)
+        exclude=()
 
 class MergeOrderSerializer(serializers.ModelSerializer):
   
     class Meta:
         model = MergeOrder
         #fields = (
-        exclude=('adjust_fee',)
+        exclude=()
 
 
 #2015-7-27  User
@@ -34,7 +34,7 @@ class UserSerializer(serializers.ModelSerializer):
     """ User  to json """
     class Meta:
         model = User
-        exclude= ('created_at',)
+        exclude= ()
 
 
 
