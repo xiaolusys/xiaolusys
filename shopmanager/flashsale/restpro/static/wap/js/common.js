0eba1430ba90188e5b0a005008077e1db873b0c7
huangyan <907790764@qq.com>
2015-07-30 16:22:24 +0800

    [xlmm][flashsale][restpro][shopcart] add shopcart

--- shopmanager/flashsale/restpro/static/wap/js/common.js
+++ shopmanager/flashsale/restpro/static/wap/js/common.js
@@ -106,3 +106,25 @@ function getCookie(name) {
     return cookieValue;
 }
 var csrftoken = getCookie('csrftoken');
+
+
+/*
+* 模拟toast
+* */
+var intervalCounter = 0;
+function hideToast() {
+    var alert = document.getElementById("toast");
+    alert.style.opacity = 0;
+    clearInterval(intervalCounter);
+}
+function drawToast(message) {
+    var alert = document.getElementById("toast");
+    if (alert == null) {
+        var toastHTML = '<div id="toast">' + message + '</div>';
+        document.body.insertAdjacentHTML('beforeEnd', toastHTML);
+    }
+    else {
+        alert.style.opacity = .9;
+    }
+    intervalCounter = setInterval("hideToast()", 1000);
+}
