8cf6fb5d472b893765033fa2b65e7a0083e2261a
imeron <xiuqing.mei@huyi.so>
2015-08-14 15:19:13 +0800

    [restpro]fix denglu and shangpinxq add name attr

--- shopmanager/flashsale/restpro/static/wap/js/tmsp.js
+++ shopmanager/flashsale/restpro/static/wap/js/tmsp.js
@@ -31,7 +31,7 @@ function Create_product_detailsku_dom(obj) {
     	if (sku.is_saleout === true || !obj.is_saleopen ){
     		sku.sku_class="disable";
     	}
-        sku_list[sku_list.length] = '<li class="{{sku_class}}" sku_id="{{id}}" sku_price="{{agent_price}}">{{name}}<i></i></li>'.template(sku);
+        sku_list[sku_list.length] = '<li class="{{sku_class}}" name="select-sku" sku_id="{{id}}" sku_price="{{agent_price}}">{{name}}<i></i></li>'.template(sku);
     });
 
     obj.sku_list = sku_list.join('');
