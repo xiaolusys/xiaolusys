8974580bb9b4183bc5f666bd7d09dac1951c3eea
huangyan <907790764@qq.com>
2015-08-21 20:59:27 +0800

    [xlmm][flashsale][restpro] change size

--- shopmanager/flashsale/restpro/static/wap/js/main.js
+++ shopmanager/flashsale/restpro/static/wap/js/main.js
@@ -201,7 +201,7 @@ function Create_item_dom(p_obj,close_model){
 	/* 
 	<li>
       <a href="pages/shangpinxq.html?id={{ id }}">
-        <img src="{{ pic_path }}">
+        <img src="{{ pic_path }}?imageMogr2/thumbnail/289x289">
         <p class="gname">{{ name }}</p>
         <p class="gprice">
           <span class="nprice"><em>¥</em> {{ agent_price }} </span>
@@ -217,7 +217,7 @@ function Create_item_dom(p_obj,close_model){
 	/* 
 	<li>
       <a href="tongkuan.html?id={{ product_model.id }}">
-        <img src="{{ product_model.head_imgs }}">
+        <img src="{{ product_model.head_imgs }}?imageMogr2/thumbnail/289x289">
         <p class="gname">{{ product_model.name }}</p>
         <p class="gprice">
           <span class="nprice"><em>¥</em> {{ agent_price }} </span>
