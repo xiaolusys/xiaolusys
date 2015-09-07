a893c74f1e165796dd2f3008550d3a7575f30167
huangyan <907790764@qq.com>
2015-09-07 21:12:51 +0800

    [xlmm][flashsale][dinghuo] async ding huo

---
+++ shopmanager/flashsale/dinghuo/static/js/dinghuo_async.js
@@ -0,0 +1,141 @@
+//定义多行字符串函数实现
+function hereDoc(f) {
+    return f.toString().replace(/^[^\/]+\/\*!?\s?/, '').replace(/\*\/[^\/]+$/, '');
+}
+//字符串模板
+String.prototype.template = function (data) {
+    var str = this;
+    if (data && data.sort) {
+        for (var i = 0; i < data.length; i++) {
+            str = str.replace(new RegExp("{\\{" + i + "}}", "gm"), data[i]);
+        }
+        return str;
+    }
+
+    var placeholder = str.match(new RegExp("{{.+?}}", 'ig'));
+    if (data && placeholder) {
+        for (var i = 0; i < placeholder.length; i++) {
+            var key = placeholder[i];
+            var value = proxy.call(data, key.replace(new RegExp("[{,}]", "gm"), ""));
+            key = key.replace(new RegExp("\\\.", "gm"), "\\.").replace("{{", "{\\{");
+            if (value == null)
+                value = "&nbsp;";
+            str = str.replace(new RegExp(key, "gm"), value);
+        }
+    }
+    return str;
+
+    function proxy(key) {
+        try {
+            return eval('this.' + key);
+        } catch (e) {
+            return "";
+        }
+    }
+};
+function get_data() {
+    var task_id = $("#task_id").val();
+    $.ajax({
+        url: "/djcelery/" + task_id + "/status/",
+        method: "GET",
+        success: function (res) {
+            console.log("1", res.task.status);
+            if (res.task.status == "SUCCESS") {
+                $("#loading").hide();
+                var result_data = res.task.result;
+                var tb = $('#resultTable tbody');
+                $.each(result_data, function (index, product) {
+                    console.log(product[1][0]);
+
+                    var result_dom;
+
+                    var sku_info = product[1];
+
+
+                    var data = {
+                        "sku_len": sku_info.length,
+                        "outer_id": product[0],
+                        "name": product[1][0].product_name,
+                        "pic_path": product[1][0].pic_path
+                    }
+
+
+
+                        var first_dom = create_first_normal_dom(data);
+
+                        result_dom = first_dom;
+
+                        $.each(sku_info, function (sku_index, sku) {
+                            var sku_data = {
+                                "sku_name": sku.sku_name,
+                                "ding_huo_status":sku.ding_huo_status,
+                                "sale_num": sku.sale_num,
+                                "product_id":sku.product_id,
+                                "sku_id":sku.sku_id,
+                                "ding_huo_num":sku.ding_huo_num,
+                                "arrival_num": sku.arrival_num,
+                                "sample_num": sku.sample_num,
+                                "ku_cun_num": sku.ku_cun_num
+                            }
+
+                            var second_dom = create_second_normal_dom(sku_data);
+                            var last_dom = create_last_dom();
+
+                            result_dom = result_dom + second_dom + last_dom;
+                            console.log(result_dom);
+
+
+                        });
+                    tb.append(result_dom);
+
+                });
+            } else {
+                setTimeout(get_data, 1000);
+            }
+        }
+    });
+}
+
+function create_first_normal_dom(obj) {
+    function first_normal_dom() {
+        /*
+         <tr>
+         <td rowspan="{{ sku_len }}">{{ name }}<br>
+         <a href="/sale/dinghuo/adddetail/{{ outer_id }}" target="_blank">编码:{{ outer_id }}</a>
+         </td>
+         <td rowspan="{{ sku_len }}">
+                <div class="portfolio-box"><img src="{{ pic_path }}" width="100px"
+                                                                        class="img-circle"></div>
+         */
+
+    };
+
+    return hereDoc(first_normal_dom).template(obj)
+}
+
+function create_second_normal_dom(obj) {
+    function second_normal_dom() {
+        /*
+         <td>{{ sku_name }}</td>
+         <td>{{ ding_huo_status }}</td>
+         <td>{{ sale_num }}</td>
+         <td><a href="/sale/dinghuo/statsbypid/{{ product_id }}" target="_blank"
+                                       id="ding_huo_num_{{ sku_id }}">{{ ding_huo_num }}</a></td>
+         <td>{{ arrival_num }}</td>
+         <td>{{ sample_num }}</td>
+         <td>{{ ku_cun_num }}</td>
+         */
+
+    };
+
+    return hereDoc(second_normal_dom).template(obj)
+}
+function create_last_dom() {
+    function last_dom() {
+        /*
+         </tr>
+         */
+    };
+
+    return hereDoc(last_dom).template()
+}
--- shopmanager/flashsale/dinghuo/tasks.py
+++ shopmanager/flashsale/dinghuo/tasks.py
@@ -392,3 +392,82 @@ def task_supplier_stat(start_date, end_date, group_name):
     cursor.execute(sql)
     raw = cursor.fetchall()
     return raw
+
+
+@task()
+def task_ding_huo(shelve_from, time_to, groupname, search_text, target_date, query_time, dhstatus):
+
+    order_sql = "select id,outer_id,sum(num) as sale_num,outer_sku_id,pay_time from " \
+                "shop_trades_mergeorder where sys_status='IN_EFFECT' " \
+                "and merge_trade_id in (select id from shop_trades_mergetrade where type not in ('reissue','exchange') " \
+                "and status in ('WAIT_SELLER_SEND_GOODS','WAIT_BUYER_CONFIRM_GOODS','TRADE_BUYER_SIGNED','TRADE_FINISHED') " \
+                "and sys_status not in('INVALID','ON_THE_FLY') " \
+                "and id not in (select id from shop_trades_mergetrade where sys_status='FINISHED' and is_express_print=False))" \
+                "and gift_type !=4 " \
+                "and (pay_time between '{0}' and '{1}') " \
+                "and char_length(outer_id)>=9 " \
+                "and (left(outer_id,1)='9' or left(outer_id,1)='8' or left(outer_id,1)='1') " \
+                "group by outer_id,outer_sku_id".format(shelve_from, time_to)
+    if groupname == 0:
+        group_sql = ""
+    else:
+        group_sql = "and sale_charger in (select username from auth_user where id in (select user_id from suplychain_flashsale_myuser where group_id = {0}))".format(
+            str(groupname))
+    if len(search_text) > 0:
+        search_text = str(search_text)
+        product_sql = "select A.id,A.product_name,A.outer_id,A.pic_path,B.outer_id as outer_sku_id,B.quantity,B.properties_alias,B.id as sku_id,C.exist_stock_num from " \
+                      "(select id,name as product_name,outer_id,pic_path from " \
+                      "shop_items_product where outer_id like '%%{0}%%' or name like '%%{0}%%' ) as A " \
+                      "left join (select id,product_id,outer_id,properties_alias,quantity from shop_items_productsku where status!='delete') as B " \
+                      "on A.id=B.product_id left join flash_sale_product_sku_detail as C on B.id=C.product_sku".format(
+            search_text)
+    else:
+        product_sql = "select A.id,A.product_name,A.outer_id,A.pic_path,B.outer_id as outer_sku_id,B.quantity,B.properties_alias,B.id as sku_id,C.exist_stock_num from " \
+                      "(select id,name as product_name,outer_id,pic_path from " \
+                      "shop_items_product where  sale_time='{0}' " \
+                      "and status!='delete' {1}) as A " \
+                      "left join (select id,product_id,outer_id,properties_alias,quantity from shop_items_productsku where status!='delete') as B " \
+                      "on A.id=B.product_id left join flash_sale_product_sku_detail as C on B.id=C.product_sku".format(
+            target_date, group_sql)
+    ding_huo_sql = "select B.outer_id,B.chichu_id,sum(if(A.status='草稿' or A.status='审核',B.buy_quantity,0)) as buy_quantity,sum(if(A.status='7',B.buy_quantity,0)) as sample_quantity," \
+                   "sum(if(status='5' or status='6' or status='有问题' or status='验货完成' or status='已处理',B.arrival_quantity,0)) as arrival_quantity,B.effect_quantity,A.status" \
+                   " from (select id,status from suplychain_flashsale_orderlist where status not in ('作废') and created between '{0}' and '{1}') as A " \
+                   "left join (select orderlist_id,outer_id,chichu_id,buy_quantity,arrival_quantity,(buy_quantity-inferior_quantity-non_arrival_quantity) as effect_quantity " \
+                   "from suplychain_flashsale_orderdetail) as B on A.id=B.orderlist_id group by outer_id,chichu_id".format(
+        shelve_from, query_time)
+    sql = "select product.outer_id,product.product_name,product.outer_sku_id,product.pic_path,product.properties_alias," \
+          "order_info.sale_num,ding_huo_info.buy_quantity,ding_huo_info.effect_quantity,product.sku_id,product.exist_stock_num," \
+          "product.id,ding_huo_info.arrival_quantity,ding_huo_info.sample_quantity " \
+          "from (" + product_sql + ") as product left join (" + order_sql + ") as order_info on product.outer_id=order_info.outer_id and product.outer_sku_id=order_info.outer_sku_id left join (" + ding_huo_sql + ") as ding_huo_info on product.outer_id=ding_huo_info.outer_id and product.sku_id=ding_huo_info.chichu_id"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    raw = cursor.fetchall()
+    trade_dict = {}
+    total_more_num = 0
+    total_less_num = 0
+    for product in raw:
+        sale_num = int(product[5] or 0)
+        ding_huo_num = int(product[6] or 0)
+        arrival_num = int(product[11] or 0)
+        sample_num = int(product[12] or 0)
+        ding_huo_status, flag_of_more, flag_of_less = functions.get_ding_huo_status(
+            sale_num, ding_huo_num, int(product[9] or 0), sample_num, arrival_num)
+        if flag_of_more:
+            total_more_num += (sample_num + int(product[9] or 0) + ding_huo_num + arrival_num - sale_num)
+        if flag_of_less:
+            total_less_num += (sale_num - sample_num - int(product[9] or 0) - arrival_num - ding_huo_num)
+        temp_dict = {"product_id": product[10], "sku_id": product[2], "product_name": product[1],
+                     "pic_path": product[3], "sale_num": sale_num or 0, "sku_name": product[4],
+                     "ding_huo_num": ding_huo_num, "effect_num": product[7] or 0,
+                     "ding_huo_status": ding_huo_status, "sample_num": sample_num,
+                     "flag_of_more": flag_of_more, "flag_of_less": flag_of_less,
+                     "sku_id": product[8], "ku_cun_num": int(product[9] or 0),
+                     "arrival_num": arrival_num}
+        if dhstatus == u'0' or ((flag_of_more or flag_of_less) and dhstatus == u'1') or (
+                    flag_of_less and dhstatus == u'2') or (flag_of_more and dhstatus == u'3'):
+            if product[0] not in trade_dict:
+                trade_dict[product[0]] = [temp_dict]
+            else:
+                trade_dict[product[0]].append(temp_dict)
+    trade_dict = sorted(trade_dict.items(), key=lambda d: d[0])
+    return trade_dict
