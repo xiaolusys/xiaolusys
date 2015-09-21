05756ebf83e855e25c89e634d61875591b44ee77
huangyan <907790764@qq.com>
2015-09-21 21:07:34 +0800

    [xlmm][supply][supplier] stats

--- shopmanager/flashsale/daystats/views_stats_performance.py
+++ shopmanager/flashsale/daystats/views_stats_performance.py
@@ -4,7 +4,7 @@ from shopback.categorys.models import ProductCategory
 from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 from rest_framework import permissions
 from rest_framework.response import Response
-from .tasks import task_calc_performance_by_user
+from .tasks import task_calc_performance_by_user, task_calc_performance_by_supplier
 import datetime
 
 
@@ -23,4 +23,22 @@ class StatsPerformanceView(generics.ListCreateAPIView):
         start_date = content.get("df", datetime.date.today().strftime("%Y-%m-%d"))
         end_date = content.get("dt", datetime.date.today().strftime("%Y-%m-%d"))
         start_task = task_calc_performance_by_user.s(start_date, end_date, category)()
+        return Response({"task_id": start_task, "category": category, "start_date": start_date, "end_date": end_date})
+
+
+class StatsSupplierView(generics.ListCreateAPIView):
+    """
+        供应商统计
+    """
+    queryset = ProductCategory.objects.all()
+    renderer_classes = (JSONRenderer, TemplateHTMLRenderer,)
+    template_name = "xiaolumm/data2supplier.html"
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        content = request.GET
+        category = content.get("category", "0")
+        start_date = content.get("df", datetime.date.today().strftime("%Y-%m-%d"))
+        end_date = content.get("dt", datetime.date.today().strftime("%Y-%m-%d"))
+        start_task = task_calc_performance_by_supplier.s(start_date, end_date, category)()
         return Response({"task_id": start_task, "category": category, "start_date": start_date, "end_date": end_date})
