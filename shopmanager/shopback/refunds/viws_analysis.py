05053d9d17a59912fa5abbf4c29495db7310b365
LinJie <1051631394@qq.com>
2015-10-24 22:12:43 +0800

    [shopback][refund][refundrate] add the regression function for rate

--- shopmanager/shopback/refunds/viws_analysis.py
+++ shopmanager/shopback/refunds/viws_analysis.py
@@ -9,8 +9,13 @@ from rest_framework import permissions
 from shopback.base import log_action, ADDITION, CHANGE
 from tasks import refund_analysis
 from flashsale.pay.models import SaleOrder, SaleTrade
+from shopback.refunds.models_refund_rate import PayRefundRate
 import logging
 import datetime
+from django.http import HttpResponse
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+from django.forms import model_to_dict
 
 logger = logging.getLogger('django.request')
 
@@ -30,3 +35,29 @@ class RefundAnaView(APIView):
         content = request.REQUEST
         arr = content.get("arr", None)
         return Response({"res": True})
+
+
+class RefundRateView(APIView):
+    renderer_classes = (JSONRenderer, TemplateHTMLRenderer)
+    template_name = "refunds/refund_rate_chart.html"
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request):
+        return Response()
+
+    def post(self, request, format=None):
+        content = request.REQUEST
+        date_from = content.get("date_from", None)
+        date_to = content.get("date_to", None)
+        year, montth, day = map(int, date_from.split('-'))
+        time_from = datetime.datetime(year, montth, day, 0, 0, 0)
+        year, montth, day = map(int, date_to.split('-'))
+        time_to = datetime.datetime(year, montth, day, 23, 59, 59)
+        rates = PayRefundRate.objects.filter(date_cal__gte=time_from, date_cal__lte=time_to).order_by('date_cal')
+        data = []
+        for rate in rates:
+            rate_dic = model_to_dict(rate, fields=['date_cal', 'ref_num', "pay_num", 'ref_rate'])
+            data.append(rate_dic)
+
+        return HttpResponse(json.dumps(data, cls=DjangoJSONEncoder),
+                            mimetype="application/json")
