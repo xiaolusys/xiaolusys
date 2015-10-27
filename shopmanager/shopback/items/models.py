9661bce754c4fb6d74b13654149b5b62422fe48c
LinJie <1051631394@qq.com>
2015-10-27 11:21:58 +0800

    [flashsale][restpro][index] fix the index lowest price show for same model

--- shopmanager/shopback/items/models.py
+++ shopmanager/shopback/items/models.py
@@ -419,10 +419,13 @@ class Product(models.Model):
         return ','.join(ds)
 
     def lowest_price(self):
-        skus = self.prod_skus.all()
         prcs = []
+        if self.model_id == 0:
+            skus = self.prod_skus.all()
+        else:
+            skus = ProductSku.objects.filter(product__model_id=self.model_id)
         for sku in skus:
-           prcs.append(sku.agent_price)
+            prcs.append(sku.agent_price)
         return min(prcs)
 
 from shopback.signals import signal_product_upshelf
