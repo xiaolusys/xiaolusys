166eebd0359383d0c995b8112b78368dd175a7d2
LinJie <1051631394@qq.com>
2015-11-02 15:19:35 +0800

    [flashsale][xlmm][view_info] search xlmm info  ,add the function to exit xlmm

--- shopmanager/flashsale/xiaolumm/viws_xlmminfo.py
+++ shopmanager/flashsale/xiaolumm/viws_xlmminfo.py
@@ -1,17 +1,22 @@
 # coding=utf-8
-from django.http import HttpResponse, Http404
-from django.shortcuts import redirect, render_to_response
+from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.contrib.auth.models import User
-from django.db.models import Sum, Q
-from django.views.decorators.csrf import csrf_exempt
-from .models import XiaoluMama, CarryLog
-import json
-from django.core.serializers.json import DjangoJSONEncoder
-from django.forms.models import model_to_dict
-import datetime
+from django.db.models import Sum
+from django.views.generic import View
+from django.shortcuts import HttpResponse
+from django.shortcuts import get_object_or_404
+
 from flashsale.clickcount.models import ClickCount
-from flashsale.clickrebeta.models import StatisticsShoppingByDay,StatisticsShopping
+from flashsale.clickrebeta.models import StatisticsShopping
+from .models import XiaoluMama, CarryLog
+from common.modelutils import update_model_fields
+from shopback.base import log_action, CHANGE
+
+import datetime
+import logging
+import json
+
+logger = logging.getLogger('django.request')
 
 
 def referal_From(mobile):
@@ -56,16 +61,6 @@ def carry_Log_By_date(left, right, xlmm):
     carry_log_all_sum = [sum_value_in / 100.0, sum_value_out / 100.0, sum_value_in_pending / 100.0,
                          sum_value_out_pending / 100.0]
 
-    # (ORDER_REBETA,u'订单返利'),
-    # (ORDER_BUY,u'消费支出'),
-    # (REFUND_RETURN,u'退款返现'),
-    # (CLICK_REBETA,u'点击兑现'),
-    # (CASH_OUT,u'钱包提现'),
-    # (DEPOSIT,u'押金'),
-    # (THOUSAND_REBETA,u'千元提成'),
-    # (AGENCY_SUBSIDY,u'代理补贴'),
-    # (MAMA_RECRUIT,u'招募奖金'),
-    # (ORDER_RED_PAC,u'订单红包')
     sum_order_reb = carry_Log(xlmm, left, right, log_type=CarryLog.ORDER_REBETA)  # 确定的记录
     sum_order_buy = carry_Log(xlmm, left, right, log_type=CarryLog.ORDER_BUY)  # 确定的记录
     sum_refund = carry_Log(xlmm, left, right, log_type=CarryLog.REFUND_RETURN)  # 确定的记录
@@ -94,59 +89,147 @@ def carry_Log_By_date(left, right, xlmm):
     return carry_log_all_sum, sum_detail_confirm, sum_detail_pending
 
 
-@csrf_exempt
-def xlmmInfo(request):
-    content = request.GET
-    today = datetime.datetime.today()
-    left = content.get('date_from') or ''
-    right = content.get('date_to') or ''
-    xlmm = content.get('xlmm') or 0
-    if left is '' or right is '':
-        right_date = today.date()
-    else:
-        year, month, day = right.split('-')
-        right_date = datetime.date(int(year), int(month), int(day))
-    xlmm = int(xlmm)
-    try:
-        xlmama = XiaoluMama.objects.get(id=xlmm)
-        charge_time = xlmama.charge_time
-        left_date = charge_time
-    except:
-        charge_time = datetime.date(2015, 4, 15)
-        left_date = charge_time
+class XlmmInfo(View):
+    template = 'xlmm_info/xlmm_info.html'
 
-    if right and xlmm:
+    def get(self, request):
+        date = datetime.date.today()
+        data = {"xlmm": 0, "left_date": date, "right": date}
+        return render_to_response(self.template, data, context_instance=RequestContext(request))
+
+    def post(self, request):
+        content = request.REQUEST
+        left = content.get('date_from', None)
+        right = content.get('date_to', None)
+        xlmm = map(int, [content.get('xlmm', 0)])[0]
+
+        year, month, day = map(int, right.split('-'))
+        right_date = datetime.date(year, month, day)
+        year, month, day = map(int, left.split('-'))
+        left_date = datetime.date(year, month, day)
+
         carry_log_all_sum, sum_detail_confirm, sum_detail_pending = carry_Log_By_date(left_date, right_date, xlmm)
         clickcounts = click_Count(xlmm, left_date, right_date)  # 点击状况
         total_clicks = clickcounts.aggregate(clis=Sum('valid_num')).get('clis') or 0  # 点击总数
         order_counts = order_Count(xlmm, left_date, right_date)  # 订单状况
-        total_orders = order_counts.count()# 订单总数(不包含取消的)
-        xlmms = XiaoluMama.objects.filter(id=xlmm)
+        total_orders = order_counts.count()  # 订单总数(不包含取消的)
 
-        allcarrylogs = CarryLog.objects.filter(xlmm=xlmm, carry_date__gte=left, carry_date__lte=right)
-        if xlmms.exists():
-            referals = referal_From(xlmms[0].mobile)  # 推荐代理状况
-            refs_num = referals.count()
+        xlmm_obj = XiaoluMama.objects.get(id=xlmm)
+        allcarrylogs = CarryLog.objects.filter(xlmm=xlmm, carry_date__gte=left_date, carry_date__lte=right_date)
+        referals = referal_From(xlmm_obj.mobile)  # 推荐代理状况
+        refs_num = referals.count()
+        data = {"xlmm_obj": xlmm_obj, "clickcounts": clickcounts, "carry_log_all_sum": carry_log_all_sum,
+                "order_counts": order_counts, "referals": referals, "allcarrylogs": allcarrylogs,
+                "xlmm": xlmm, "left_date": left_date,
+                "right": right, "sum_detail_pending": sum_detail_pending,
+                "sum_detail_confirm": sum_detail_confirm, 'total_clicks': total_clicks,
+                'total_orders': total_orders, 'refs_num': refs_num}
+        return render_to_response(self.template, data, context_instance=RequestContext(request))
 
-        return render_to_response("xlmm_info/xlmm_info.html",
-                                  {"xlmms": xlmms, "clickcounts": clickcounts, "carry_log_all_sum": carry_log_all_sum,
-                                   "order_counts": order_counts, "referals": referals, "allcarrylogs": allcarrylogs,
-                                   "xlmm": xlmm, "charge_time": charge_time.strftime("%Y-%m-%d"),
-                                   "today": today.strftime("%Y-%m-%d")
-                                      , "sum_detail_pending": sum_detail_pending,
-                                   "sum_detail_confirm": sum_detail_confirm, 'total_clicks':total_clicks,'total_orders':total_orders,'refs_num':refs_num},
-                                  context_instance=RequestContext(request))
-    if xlmm:
-        xlmms = XiaoluMama.objects.filter(id=xlmm)
-        if xlmms.exists():
-            referals = referal_From(xlmms[0].mobile)  # 推荐代理状况
-            return render_to_response("xlmm_info/xlmm_info.html",
-                                      {"xlmms": xlmms, "referals": referals, "xlmm": xlmm,
-                                       "charge_time": charge_time.strftime("%Y-%m-%d"),
-                                       "today": today.strftime("%Y-%m-%d")},
-                                      context_instance=RequestContext(request))
 
-    else:
-        return render_to_response("xlmm_info/xlmm_info.html",
-                                  {"xlmm": xlmm, "charge_time": charge_time.strftime("%Y-%m-%d"),
-                                   "today": today.strftime("%Y-%m-%d")}, context_instance=RequestContext(request))
+class XlmmExit(object):
+    """ 代理退出 """
+
+    def __init__(self, xlmm):
+        self.xlmm = xlmm
+
+    def check_xlmm_available(self, xlmm):
+        if xlmm.charge_status != XiaoluMama.CHARGED or xlmm.agencylevel not in (
+                XiaoluMama.A_LEVEL, XiaoluMama.VIP_LEVEL):
+            pass
+        else:
+            return xlmm
+
+    def check_carry_in(self, xlmm_id):
+        """ 代理总收入 """
+        clgs = CarryLog.objects.filter(xlmm=xlmm_id, carry_type=CarryLog.CARRY_IN, status=CarryLog.CONFIRMED)
+        value_in = clgs.aggregate(total_value=Sum('value')).get('total_value') or 0
+        return value_in
+
+    def check_carry_out(self, xlmm_id):
+        """ 代理总支出 """
+        clgs = CarryLog.objects.filter(xlmm=xlmm_id, carry_type=CarryLog.CARRY_OUT, status=CarryLog.CONFIRMED)
+        value_out = clgs.aggregate(total_value=Sum('value')).get('total_value') or 0
+        return value_out
+
+    def calculate_cost_flush(self, carry_ins, carry_outs):
+        """ 计算补差数额 """
+        # 加入就退出并且可以有30的奖励
+        flush_value = carry_ins - carry_outs
+        return flush_value
+
+    def create_flush_log(self, xlmm_id, flush_value):
+        """ 创建补差额记录 """
+        # 补差额生成 判断类型
+        if flush_value > 0:  # 收入大于支出 补差为支出
+            CarryLog.objects.create(xlmm=xlmm_id, log_type=CarryLog.COST_FLUSH, status=CarryLog.CONFIRMED,
+                                    carry_type=CarryLog.CARRY_OUT, value=flush_value)
+        elif flush_value < 0:  # 收入小于支出 补差为收入
+            CarryLog.objects.create(xlmm=xlmm_id, log_type=CarryLog.COST_FLUSH, status=CarryLog.CONFIRMED,
+                                    carry_type=CarryLog.CARRY_IN, value=flush_value)
+        else:  # 等于0 的情况则不去创建
+            pass
+
+    def modify_xlmm_base_info(self, xlmm):
+        """ 修改代理到初始状态 """
+        xlmm.cash = 0
+        xlmm.agencylevel = XiaoluMama.INNER_LEVEL
+        xlmm.charge_status = XiaoluMama.UNCHARGE
+        xlmm.target_complete = 0
+        xlmm.hasale = False
+        xlmm.user_group = None
+        xlmm.referal_from = ''
+        xlmm.pending = 0
+        xlmm.manager = 0
+        xlmm.charge_time = None
+        update_model_fields(xlmm,
+                            update_fields=['cash', 'agencylevel', 'charge_status', 'target_complete', 'hasale',
+                                           'user_group', 'referal_from', 'pending', 'manager', 'charge_time'])
+        return xlmm
+
+    def xlmm_buy_cash(self, xlmm):
+        clgs_buy = CarryLog.objects.filter(xlmm=xlmm.id, carry_type=CarryLog.CARRY_OUT, log_type=CarryLog.ORDER_BUY,
+                                           status=CarryLog.CONFIRMED)
+        clgs_refund = CarryLog.objects.filter(xlmm=xlmm.id, carry_type=CarryLog.CARRY_IN,
+                                              log_type=CarryLog.REFUND_RETURN, status=CarryLog.CONFIRMED)
+        value_buy = clgs_buy.aggregate(total_value=Sum('value')).get('total_value') or 0
+        value_refund = clgs_refund.aggregate(total_value=Sum('value')).get('total_value') or 0
+        return value_buy - value_refund
+
+    def modify_lowest_uncoushout(self, xlmm):
+        """ 将消费记录填写到最低不可提现金额里面　"""
+        value_buy = self.xlmm_buy_cash(xlmm)  # 消费
+        # 之前的充值是不能提现　的　所以都会产生在消费记录当中　　所以这里要将消费记录的数值写入最低不可提现字段
+        xlmm.lowest_uncoushout = value_buy / 100.0
+        update_model_fields(xlmm, update_fields=['lowest_uncoushout'])
+
+    def xlmmexit(self):
+        """ 代理退出操作 """
+        xlmm = self.check_xlmm_available(self.xlmm)  # 检查账户是否有效
+        if xlmm is None:
+            return "mm_error_status"
+        carry_ins = self.check_carry_in(self.xlmm.id)  # 计算收入
+        carry_outs = self.check_carry_out(self.xlmm.id)  # 计算支出
+        flush_value = self.calculate_cost_flush(carry_ins, carry_outs)  # 计算补差额
+        self.create_flush_log(self.xlmm.id, flush_value)  # 生成补差记录
+        self.modify_xlmm_base_info(self.xlmm)  # 修改该代理账户 基本信息
+        self.modify_lowest_uncoushout(xlmm)
+        return flush_value / 100.0
+
+
+
+
+def xlmmExitAction(request):
+    """ 执行代理退出 """
+    content = request.REQUEST
+    xlmm_id = content.get("xlmm_id", None)
+    if xlmm_id is None:
+        return HttpResponse({"error": "编号为空"})
+    xlmm = get_object_or_404(XiaoluMama, id=xlmm_id)
+    exit_xlmm = XlmmExit(xlmm)
+    flush_cash = exit_xlmm.xlmmexit()
+    log_action(request.user.id, xlmm, CHANGE, u'代理退出修改信息')
+    data = {"flush_cash": flush_cash}
+    return HttpResponse(json.dumps(data))
+
+
