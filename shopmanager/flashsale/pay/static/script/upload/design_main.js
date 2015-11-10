365cec8b9bcc6f96ead75db193e0af29ee6a74c4
huangyan <907790764@qq.com>
2015-11-10 17:17:48 +0800

    [xlmm][supplier]add schedule

--- shopmanager/flashsale/pay/static/script/upload/design_main.js
+++ shopmanager/flashsale/pay/static/script/upload/design_main.js
@@ -51,6 +51,59 @@ $(function () {
             }
         }
     });
+    var contentuploader = Qiniu.uploader({
+        runtimes: 'html5,flash,html4',
+        browse_button: 'content_chuan',
+        container: 'content_img',
+        drop_element: 'content_img',
+        max_file_size: '100mb',
+        flash_swf_url: 'js/plupload/Moxie.swf',
+        dragdrop: true,
+        chunk_size: '4mb',
+        uptoken_url: $('#uptoken_url').val(),
+        domain: $('#domain').val(),
+        auto_start: true,
+        init: {
+            'FilesAdded': function (up, files) {
+
+            },
+            'BeforeUpload': function (up, file) {
+
+            },
+            'UploadProgress': function (up, file) {
+
+            },
+            'UploadComplete': function () {
+                //$('#success').show();
+            },
+            'FileUploaded': function (up, file, info) {
+                var domain = up.getOption('domain');
+                var res = jQuery.parseJSON(info);
+                var sourceLink = domain + res.key; //获取上传成功后的文件的Url
+
+                var r_str = '<a class="example-image-link" href="'+sourceLink+'" ' +
+                'data-lightbox="example-1"> ' +
+                '<img src="'+sourceLink+'" alt="内容图" height="150px">' +
+                '</a>';
+                if($("#is_coverage").prop("checked")){
+                    $(".popup-gallery").append(r_str);
+                }else{
+                    $(".popup-gallery").html(r_str);
+                }
+
+                ajaxImage(CONTENT_TYPE, $("#model_id").html(), sourceLink)
+            },
+            'Error': function (up, err, errTip) {
+
+            }
+            ,
+            'Key': function (up, file) {
+                var timestamp = new Date().getTime();
+                var key = "MG-" + timestamp + "-" + file.name;
+                return key
+            }
+        }
+    });
     var all_tou = $(".toutu");
     $.each(all_tou, function (index, obj) {
         var product_id = all_tou.eq(index).attr("data-product");
@@ -86,7 +139,7 @@ $(function () {
                     var res = jQuery.parseJSON(info);
                     var sourceLink = domain + res.key; //获取上传成功后的文件的Url
                     $("#"+browse).attr("src", sourceLink);
-                    ajaxImage(SHANGPIN_TYPE, product_id, sourceLink)
+                    ajaxImage(SHANGPIN_TYPE, product_id, sourceLink);
                 },
                 'Error': function (up, err, errTip) {
 
@@ -114,12 +167,17 @@ $(function () {
     function ajaxImage(type, pro_id, pic_link) {
         var requestUrl = "/mm/chuantu/";
         var requestCallBack = function (data) {
-            console.log(data)
+            console.log(data);
+            if(data.result == "success"){
+                 swal("结果", "上传成功", "success");
+            }else if(data.result == "error"){
+                 swal("结果", "上传失败", "error");
+            }
         };
         $.ajax({
             type: 'post',
             url: requestUrl,
-            data: {'type': type, "pro_id": pro_id, "pic_link": pic_link},
+            data: {'type': type, "pro_id": pro_id, "pic_link": pic_link, "coverage": $("#is_coverage").prop("checked")},
             dataType: 'json',
             success: requestCallBack
         });
