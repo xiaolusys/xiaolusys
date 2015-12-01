66c3ac8c997271588d79f38aa66cd7029ff0725f
imeron <xiuqing.mei@xiaolu.so>
2015-12-01 14:35:41 +0800

    change task_settings.py queue

--- shopmanager/task_settings.py
+++ shopmanager/task_settings.py
@@ -21,7 +21,7 @@ BROKER_URL = 'amqp://user1:passwd1@10.132.179.237:5672/vhost1'
 # 非常重要,有些情况下可以防止死锁,如果有数量更时间限制应开启 
 #CELERYD_FORCE_EXECV = True    
 #WORKER每次取任务数
-CELERYD_PREFETCH_MULTIPLIER = 1
+#CELERYD_PREFETCH_MULTIPLIER = 1
 # BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 90}  
 # 任务发出后，经过一段时间还未收到acknowledge , 就将任务重新交给其他worker执行  
 # CELERY_DISABLE_RATE_LIMITS = True     
@@ -34,7 +34,7 @@ BROKER_POOL_LIMIT = 10 # 10 connections
 CELERYD_CONCURRENCY = 8 # 16 processes in paralle
 
 from kombu import Exchange, Queue
-CELERY_DEFAULT_QUEUE = 'peroid'
+CELERY_DEFAULT_QUEUE = 'default'
 CELERY_QUEUES = (
     Queue('default', routing_key='tasks.#'),
     Queue('notify', routing_key='notify.#'),
@@ -42,9 +42,9 @@ CELERY_QUEUES = (
     Queue('async', routing_key='async.#'),
 )
 
-CELERY_DEFAULT_EXCHANGE = 'peroid'
+CELERY_DEFAULT_EXCHANGE = 'default'
 CELERY_DEFAULT_EXCHANGE_TYPE = 'topic'
-CELERY_DEFAULT_ROUTING_KEY = 'peroid.default'
+CELERY_DEFAULT_ROUTING_KEY = 'default'
 
 CELERY_ROUTES = {
         'shopapp.notify.tasks.process_trade_notify_task': {
@@ -65,8 +65,8 @@ CELERY_ROUTES = {
         },
         #######################################################
         'flashsale.xiaolumm.tasks.task_Create_Click_Record': {
-            'queue': 'notify',
-            'routing_key': 'notify.push_xlmm_pending_cash',
+            'queue': 'default',
+            'routing_key': 'default.push_xlmm_pending_cash',
         },
         'flashsale.xiaolumm.tasks.task_Push_Pending_Carry_Cash': {
             'queue': 'notify',
@@ -102,17 +102,20 @@ SYNC_MODEL_SCHEDULE = {
     u'定时淘宝分销订单增量下载任务':{    #增量更新分销部分订单
         'task':'shopback.fenxiao.tasks.updateAllUserIncrementPurchasesTask',
         'schedule':crontab(minute="*/15"),
-        'args':()
+        'args':(),
+        'options' : {'queue':'peroid'} 
     },
     u'定时淘宝商城订单增量下载任务':{
         'task':'shopback.orders.tasks.updateAllUserIncrementTradesTask',
         'schedule':crontab(minute="0",hour="*/12"),
-        'args':()
+        'args':(),
+        'options' : {'queue':'peroid'} 
     },
     u'定时淘宝商城待发货订单下载任务':{
         'task':'shopback.orders.tasks.updateAllUserWaitPostOrderTask',
         'schedule':crontab(minute="30",hour="23"),
-        'args':()
+        'args':(),
+        'options' : {'queue':'peroid'} 
     },
     u'分段日期统计商品销售数据':{     #将昨日的订单数更新为商品的警告库位
          'task':'shopback.items.tasks.gradCalcProductSaleTask',
@@ -127,22 +130,26 @@ SYNC_MODEL_SCHEDULE = {
     u'定时更新设置提醒的订单入问题单':{     #更新定时提醒订单
          'task':'shopback.trades.tasks.regularRemainOrderTask',
          'schedule':crontab(minute="0",hour='0,12,17'),
-         'args':()
+         'args':(),
+         'options' : {'queue':'peroid'} 
      },
      u'定时更新商品待发数':{     #更新库存
         'task':'shopback.items.tasks.updateProductWaitPostNumTask',
         'schedule':crontab(minute="0",hour="5,13"),#
-        'args':()
+        'args':(),
+        'options' : {'queue':'peroid'} 
      },
      u'定时更新淘宝商品库存':{     #更新库存
         'task':'shopback.items.tasks.updateAllUserItemNumTask',
         'schedule':crontab(minute="0",hour="7"),#
-        'args':()
+        'args':(),
+        'options' : {'queue':'peroid'} 
     },
     u'定时更新分销商品库存':{     #更新库存
         'task':'shopback.items.tasks.updateAllUserPurchaseItemNumTask',
         'schedule':crontab(minute="0",hour="7"),#
-        'args':()
+        'args':(),
+        'options' : {'queue':'peroid'} 
     },
     u'定时生成每月物流信息报表':{     #更新库存
         'task':'shopback.trades.tasks.task_Gen_Logistic_Report_File_By_Month',
@@ -177,7 +184,8 @@ SHOP_APP_SCHEDULE = {
     u'定时抓取商品评价':{
         'task':'shopapp.comments.tasks.crawAllUserOnsaleItemComment',
         'schedule':crontab(minute="0",hour="8,10,12,14,16,18,20,22"),
-        'args':()
+        'args':(),
+        'options' : {'queue':'peroid'} 
     },
     u'定时上架任务':{  #定时上架任务
         'task':'shopapp.autolist.tasks.updateAllItemListTask',
