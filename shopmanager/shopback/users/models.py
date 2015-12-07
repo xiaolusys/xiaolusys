6203d044ff721b0b464158b43241370249884b13
imeron <xiuqing.mei@xiaolu.so>
2015-12-07 15:31:04 +0800

    [tmcnotify][users]user_login_in signals

--- shopmanager/shopback/users/models.py
+++ shopmanager/shopback/users/models.py
@@ -255,10 +255,11 @@ class User(models.Model):
             
 def add_taobao_user(sender, user,top_session,top_parameters, *args, **kwargs):
     """docstring for user_logged_in"""
-    
-    profiles = User.objects.filter(type__in=(User.SHOP_TYPE_B,User.SHOP_TYPE_C),user=user)
+    top_params = json.loads(top_parameters or '{}')
+    visitor_id = top_params and top_params.get('taobao_user_id') or None
+    profiles = User.objects.filter(type__in=(User.SHOP_TYPE_B,User.SHOP_TYPE_C)
+                                   ,visitor_id=visitor_id)
     for profile in profiles:
-        profile = user.get_profile()
         profile.populate_user_info(top_session,top_parameters)
         
         profile.verify_fenxiao_user()
