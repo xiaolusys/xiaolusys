9c83349e388099e33c76a655a4f5c3c73444362e
ahlinjie <1051631394@qq.com>
2016-01-15 15:12:32 +0800

    [flashsale][dinghuo][admin] add the calculate display in the dinghuo admin table

--- shopmanager/flashsale/dinghuo/static/js/admin_js.js
+++ shopmanager/flashsale/dinghuo/static/js/admin_js.js
@@ -18,3 +18,24 @@ function show_pic(id) {
 
 
 }
+
+$(document).ready(function () {
+    var model_num = 0;
+    var quantity = 0;
+    var amount = 0;
+    var lib_amount = 0;
+    for (var j = 0; j < $("tbody tr").length; j++) {
+        model_num += parseInt($("tbody tr:eq(" + j + ")").children("td:eq(5)").html());
+        quantity += parseInt($("tbody tr:eq(" + j + ")").children("td:eq(4)").html());
+        lib_amount += parseInt($("tbody tr:eq(" + j + ")").children("td:eq(3)").html());
+        amount += parseInt($("tbody tr:eq(" + j + ")").children("td:eq(2)").html());
+
+    }
+    var value = lib_amount - amount;
+    var tip_str = '共: ' + model_num + ' 款 ' + quantity + ' 件 ' +
+        amount + ' ￥ ' + "录入总价为：" + lib_amount + '　节省　' + value + '￥ (200秒消失)';
+    layer.tips(tip_str, $('input')[1], {
+        tips: [1, '#18A15F'],
+        time: 200000
+    });
+});
