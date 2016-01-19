119dcd1f0b54d3f6a326841b67ac94735501b819
ahlinjie <1051631394@qq.com>
2016-01-19 11:07:17 +0800

    [flashsale][restpro][xlmm] release the carrylog api url

--- shopmanager/flashsale/restpro/urls.py
+++ shopmanager/flashsale/restpro/urls.py
@@ -55,7 +55,7 @@ router.register(r'calcuprorefrcd', views_pro_ref.CalcuProRefRcd)
 
 
 router.register(r'xlmm', views_xlmm.XiaoluMamaViewSet)
-# router.register(r'carrylog', views_xlmm.CarryLogViewSet)
+router.register(r'carrylog', views_xlmm.CarryLogViewSet)
 router.register(r'cashout', views_xlmm.CashOutViewSet)
 # router.register(r'clickcount', views_xlmm.ClickCountViewSet)
 router.register(r'shopping', views_xlmm.StatisticsShoppingViewSet)
