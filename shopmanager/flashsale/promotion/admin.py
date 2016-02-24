926eb74ceae76e8d92d5a52b3cf26fb927015b02
ahlinjie <1051631394@qq.com>
2016-02-24 20:59:33 +0800

    [flashsale][promotion][admin] add the search field for promotion admin

--- shopmanager/flashsale/promotion/admin.py
+++ shopmanager/flashsale/promotion/admin.py
@@ -18,7 +18,7 @@ class XLSampleApplyAdmin(admin.ModelAdmin):
     list_display = ('id', 'from_customer', 'outer_id', 'sku_code', 'ufrom', 'mobile', 'vipcode', 'status')
     list_filter = ('status', 'ufrom')
     list_display_links = ('id', 'outer_id', )
-    search_fields = ['=id', '=outer_id', '=mobile', '=user_openid' ,'=vipcode' ]
+    search_fields = ['=id', '=outer_id', '=mobile', '=user_openid', '=vipcode', '=from_customer']
     list_per_page = 40
 
 
@@ -26,10 +26,12 @@ admin.site.register(XLSampleApply, XLSampleApplyAdmin)
 
 
 class XLSampleOrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'xlsp_apply', 'customer_id', 'outer_id', 'sku_code', 'vipcode', 'created', 'problem_score','award_status', 'status')
-    list_filter = ('status','award_status')
+    list_display = (
+        'id', 'xlsp_apply', 'customer_id', 'outer_id', 'sku_code', 'vipcode', 'created',
+        'problem_score', 'award_status', 'status')
+    list_filter = ('status', 'award_status')
     list_display_links = ('id', 'customer_id', )
-    search_fields = ['=id', '=customer_id', 'vipcode']
+    search_fields = ['=id', '=customer_id', 'vipcode', '=xlsp_apply']
     list_per_page = 40
 
 
@@ -52,21 +54,21 @@ class XLInviteCodeAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'mobile', 'vipcode', )
     search_fields = ['id', 'mobile', 'vipcode', ]
     list_per_page = 40
-    
-    
+
 
 admin.site.register(XLInviteCode, XLInviteCodeAdmin)
 
 
 class XLInviteCountAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer','apply_count', 'invite_count', 'click_count')
+    list_display = ('id', 'customer', 'apply_count', 'invite_count', 'click_count')
     search_fields = ['id', 'customer__mobile', 'customer__openid']
     list_per_page = 40
-    
+
     def get_readonly_fields(self, request, obj=None):
         if not request.user.is_superuser:
             return self.readonly_fields + ('customer',)
         return self.readonly_fields
+
 
 admin.site.register(XLInviteCount, XLInviteCountAdmin)
 
