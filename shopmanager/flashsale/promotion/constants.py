a386443b49de2d3de8424c25baf7843b9070494b
ahlinjie <1051631394@qq.com>
2016-02-25 21:20:44 +0800

    [flashsale][apprelease] add the qrcode and add the link in download page

--- shopmanager/flashsale/promotion/constants.py
+++ shopmanager/flashsale/promotion/constants.py
@@ -6,3 +6,9 @@ PROMOTION_LINKID_PATH = 'pmt'
 DEFAULT_NICK = '鹿小美'
 DEFAULT_PROFILE_IMAGE = 'http://7xogkj.com2.z0.glb.qiniucdn.com/1181123466.jpg'
 
+ANDROID_DOWNLOAD = 'http://7xogkj.com2.z0.glb.qiniucdn.com/Xlmm_v1.0.2_2016-02-24_xlmm_1.0.2_jiagu_sign.apk'
+ANDROID_DOWNLOAD_QRCODE = 'http://7xogkj.com2.z0.glb.qiniucdn.com/1456293315.svg'
+
+APP_STORE_DOWNLOAD = 'https://itunes.apple.com/us/app/xiao-lu-mei-mei/id1051166985'
+APP_STORE_DOWNLOAD_QRCODE = 'http://7xogkj.com2.z0.glb.qiniucdn.com/appstore_download_link.svg'
+
