cbc66a47e99e4828adf2103280870db2d8595d10
imeron <xiuqing.mei@xiaolu.so>
2016-03-04 20:26:57 +0800

    [items]recover wait_receive_num to admin

--- shopmanager/shopback/items/admin.py
+++ shopmanager/shopback/items/admin.py
@@ -125,7 +125,7 @@ class ProductAdmin(MyAdmin):
 
     list_display = ('id', 'outer_id_link', 'pic_link', 'collect_num',
                     'category_select', 'remain_num', 'wait_post_num',
-                    'lock_num', 'cost', 'std_sale_price', #, 'wait_receive_num'
+                    'lock_num', 'wait_receive_num', 'cost', 'std_sale_price', 
                     'agent_price', 'model_id', 'sync_stock','is_watermark', 
                     'sale_time_select', 'sale_charger','ware_select', 
                     'district_link', 'shelf_status')  #'charger_select',
