507a8dc36b9aedd55b9a42c37c6335a28ff89595
Zifei Zhong <zifei.zhong@huyi.so>
2016-03-23 11:55:47 +0800

    emoji refactor

--- shopmanager/flashsale/xiaolumm/util_emoji.py
+++ shopmanager/flashsale/xiaolumm/util_emoji.py
@@ -76,6 +76,20 @@ def gen_flower_emoji():
     return random.choice(emoji)
 
 
+def gen_emoji(desc):
+    import re
+    reg = re.compile('\[\d\]')
+    res_list = reg.findall(desc)
+    d = {}
+    for key in res_list:
+        if not key in d:
+            d[key] = gen_flower_emoji()
+    
+    for k,v in d.iteritems():
+        desc = desc.replace(k,v)
+    return desc
+
+
 def match_emoji(desc):
     import re
     reg = re.compile('U[\w]{5}')
