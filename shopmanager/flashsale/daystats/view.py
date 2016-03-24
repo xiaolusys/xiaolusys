c0d2d8187e07f62da20733f84eda35335ab97d7e
huangyan <763294911@qq.com>
2016-03-24 23:10:14 +0800

    [promotion]加了一列

--- shopmanager/flashsale/daystats/view.py
+++ shopmanager/flashsale/daystats/view.py
@@ -80,7 +80,7 @@ class DailyStatsViewSet(viewsets.GenericViewSet):
         data = None
         now = datetime.datetime.now()
         threshold = now - datetime.timedelta(days=3)
-
+        threshold2 = now - datetime.timedelta(days=5)
         if type_ == 1:
             q = MergeOrder.objects.filter(
                 merge_trade__type__in=[pcfg.SALE_TYPE, pcfg.DIRECT_TYPE,
@@ -93,22 +93,26 @@ class DailyStatsViewSet(viewsets.GenericViewSet):
 
             n_total = q.only('id').count()
             n_delay = q.filter(pay_time__lte=threshold).only('id').count()
-            data = {'n_total': n_total, 'n_delay': n_delay}
+            n_s_delay = q.filter(pay_time__lte=threshold2).only('id').count()
+            data = {'n_total': n_total, 'n_delay': n_delay, 'n_s_delay': n_s_delay}
         elif type_ == 2:
             q = SaleRefund.objects.filter(status__in=[SaleRefund.REFUND_WAIT_SELLER_AGREE, SaleRefund.REFUND_WAIT_RETURN_GOODS, SaleRefund.REFUND_CONFIRM_GOODS, SaleRefund.REFUND_APPROVE])
             n_total = q.only('id').count()
             n_delay = q.filter(created__lte=threshold).only('id').count()
-            data = {'n_total': n_total, 'n_delay': n_delay}
+            n_s_delay = q.filter(pay_time__lte=threshold2).only('id').count()
+            data = {'n_total': n_total, 'n_delay': n_delay, 'n_s_delay': n_s_delay}
         elif type_ == 3:
             q = SaleRefund.objects.filter(status=SaleRefund.REFUND_APPROVE)
             n_total = q.only('id').count()
             n_delay = q.filter(created__lte=threshold).only('id').count()
-            data = {'n_total': n_total, 'n_delay': n_delay}
+            n_s_delay = q.filter(pay_time__lte=threshold2).only('id').count()
+            data = {'n_total': n_total, 'n_delay': n_delay, 'n_s_delay': n_s_delay}
         elif type_ == 4:
             q = OrderList.objects.exclude(status__in=[OrderList.COMPLETED, OrderList.ZUOFEI])
             n_total = q.only('id').count()
             n_delay = q.filter(created__lte=threshold.date()).only('id').count()
-            data = {'n_total': n_total, 'n_delay': n_delay}
+            n_s_delay = q.filter(pay_time__lte=threshold2).only('id').count()
+            data = {'n_total': n_total, 'n_delay': n_delay, 'n_s_delay': n_s_delay}
         if data:
             return Response(data)
         return Response({'error': '参数错误'})
