b9ca9e9653537962f667adc7619610bb7cfdea00
imeron <xiuqing.mei@xiaolu.so>
2016-04-07 17:19:28 +0800

    fix shopmanager celery wsgi DJANGO_SETTINGS_MODULE

--- shopmanager/shopmanager/wsgi.py
+++ shopmanager/shopmanager/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shopmanager.settings")
 
 application = get_wsgi_application()
 
