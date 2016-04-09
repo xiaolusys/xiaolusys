ef721f3fd08161205efb2d0c74bc124c3a931aee
wej <enjun.wang@xiaolu.so>
2016-04-09 16:36:59 +0800

    [dinghuo]

--- shopmanager/flashsale/dinghuo/forms.py
+++ shopmanager/flashsale/dinghuo/forms.py
@@ -27,3 +27,7 @@ class InBoundListForm(BaseForm):
 class EditInBoundForm(InBoundListForm):
     inbound_id = forms.IntegerField(required=False, initial=0)
     skus = forms.CharField(required=False, initial='[]')
+
+class AdvanceDingHuoForm(BaseForm):
+    start_date = forms.DateField(required=True)
+    end_date = forms.DateField(required=True)
