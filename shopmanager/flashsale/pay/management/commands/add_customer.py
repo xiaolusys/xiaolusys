a60d06b8e68263d51671eae60c4f8ab30698f9cf
ahlinjie <1051631394@qq.com>
2016-04-15 19:14:31 +0800

    fix the child app jump bug, add the signal collect command

--- shopmanager/flashsale/pay/management/commands/add_customer.py
+++ shopmanager/flashsale/pay/management/commands/add_customer.py
@@ -1,1 +1,16 @@
+# coding=utf-8
 __author__ = 'jishu_linjie'
+from django.core.management.base import BaseCommand
+
+from flashsale.pay.models import Customer
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not settings.DEBUG:
+            return
+        user = User.objects.get(username='shanwei.zhao')
+        Customer.objects.get_or_create(user=user)
+
