08356d44fd7bc365d6006974f71c8c44e3089412
imeron <xiuqing.mei@xiaolu.so>
2016-04-26 22:37:19 +0800

    fix task_packageskuitem_update_productskustats

--- shopmanager/shopback/trades/models.py
+++ shopmanager/shopback/trades/models.py
@@ -1641,8 +1641,9 @@ post_save.connect(update_productskustats, sender=PackageSkuItem, dispatch_uid='p
 
 
 def update_productsku_salestats_num(sender, instance, created, **kwargs):
-    from shopback.trades.tasks import task_packageskuitem_update_productskusalestats_num
-    task_packageskuitem_update_productskusalestats_num.delay(instance.sku_id, instance.pay_time)
+    if created:
+        from shopback.trades.tasks import task_packageskuitem_update_productskusalestats_num
+        task_packageskuitem_update_productskusalestats_num.delay(instance.sku_id, instance.pay_time)
 
 
 post_save.connect(update_productsku_salestats_num, sender=PackageSkuItem,
