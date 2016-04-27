b24a9ba7be9594d3085ac6fd40c4a32f82295e58
imeron <xiuqing.mei@xiaolu.so>
2016-04-27 18:13:58 +0800

    [pay]add brandentry and brandproduct

--- shopmanager/flashsale/pay/models.py
+++ shopmanager/flashsale/pay/models.py
@@ -12,7 +12,7 @@ from shopback.items.models import DIPOSITE_CODE_PREFIX
 from shopback.items.models import Product, ProductSku
 from flashsale.pay.models_user import Register, Customer, UserBudget, BudgetLog
 from flashsale.pay.models_addr import District, UserAddress
-from flashsale.pay.models_custom import Productdetail, GoodShelf, ModelProduct, ActivityEntry
+from flashsale.pay.models_custom import Productdetail, GoodShelf, ModelProduct, ActivityEntry, BrandProduct, BrandEntry
 from flashsale.pay.models_refund import SaleRefund
 from flashsale.pay.models_envelope import Envelop
 from flashsale.pay.models_coupon import Integral, IntegralLog
