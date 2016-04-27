ad2dc4201d372101ea84c1704ab894dc39b3ad03
ahlinjie <1051631394@qq.com>
2016-04-27 21:58:41 +0800

    add the new coupon share models

--- shopmanager/flashsale/pay/managers.py
+++ shopmanager/flashsale/pay/managers.py
@@ -1,4 +1,6 @@
 # -*- coding:utf8 -*-
+import datetime
+import random
 from django.db import models
 from django.db.models import Q, Sum
 
@@ -33,3 +35,71 @@ class ShopProductCategoryManager(BaseManager):
         """ 女装产品 """
         pro_category = constants.FEMALE_CID_LIST
         return self.get_queryset().filter(pro_category__in=pro_category).order_by("-position")
+
+
+class UserCouponManager(BaseManager):
+    def create_by_template(self, buyer_id, template_id, trade_id=None, **kwargs):
+        """
+        发放不绑定交易的任何类型的优惠券
+        """
+        from flashsale.pay.models_share_coupon import UserCoupon, CouponTemplate
+
+        trade_id = trade_id or ''
+        if not (buyer_id and template_id):
+            return None, 6, u'没有发放'
+        try:
+            tpl = CouponTemplate.objects.get(id=int(template_id), valid=True)  # 获取优惠券模板
+            now = datetime.datetime.now()
+            if not (tpl.release_start_time <= now <= tpl.release_end_time):
+                return None, 5, u"没有发放"  # 不在模板定义的发放时间内
+        except CouponTemplate.DoesNotExist:
+            return None, 4, u"没有发放"
+        # 身份判定（判断身份是否和优惠券模板指定用户一致） 注意　这里是硬编码　和　XiaoluMama　代理级别关联
+        user_level = CouponTemplate.ALL_USER
+        from flashsale.pay.models import Customer
+
+        customer = Customer.objects.get(id=int(buyer_id))
+        if tpl.target_user != CouponTemplate.ALL_USER:  # 如果不是所有用户可领取则判定级别
+            xlmm = customer.getXiaolumm()
+            if xlmm:
+                user_level = xlmm.agencylevel  # 用户的是代理身份 内1 　VIP 2  A 3
+
+        if user_level != tpl.target_user:
+            # 如果用户领取的优惠券和用户身份不一致则不予领取
+            return None, 3, u"用户不一致"
+        coupons = UserCoupon.objects.filter(template_id=template_id)
+
+        tpl_release_count = coupons.count()
+        if tpl_release_count > tpl.nums:  # 如果大于定义的限制领取数量
+            return None, 1, u"优惠券已经发完了"
+        user_coupon_count = coupons.filter(customer=int(buyer_id)).count()
+        if tpl.type != CouponTemplate.SHARE:  # 分享类型没有领取限制(即不是分享类型的需要校验领取限制张数)
+            if user_coupon_count >= tpl.limit_num:
+                return None, 2, u"领取超过限制"
+
+        batch_no = kwargs.get("batch_no") or ''
+        value = tpl.value
+        start_use_time = tpl.start_use_time
+        deadline = tpl.deadline
+
+        if tpl.type == CouponTemplate.SHARE:
+            if tpl.is_random and tpl.max_value and tpl.min_value:  # 分享类型 并且设置随机
+                value = float('%.1f' % random.uniform(tpl.max_value, tpl.min_value))  # 生成随机的value
+            if tpl.valid_days:
+                start_use_time = 1  # 今天
+                deadline = 1  # 今天+tpl.valid_days
+        template_num_unique = str(tpl.id) + "_" + str(user_coupon_count + 1)  # 唯一键约束 是 优惠id + "_" + 该优惠券领取张数
+        cou = UserCoupon.objects.create(template_id=int(template_id),
+                                        title=tpl.title,
+                                        type=tpl.type,
+                                        way_type=tpl.way_type,
+                                        customer=int(buyer_id),
+                                        batch_no=batch_no,
+                                        nick=customer.nick,
+                                        thumbnail=customer.thumbnail,
+                                        value=value,
+                                        sale_trade=str(trade_id),
+                                        start_use_time=start_use_time,
+                                        deadline=deadline,
+                                        template_num_unique=template_num_unique)
+        return cou, 0, u"领取成功"
