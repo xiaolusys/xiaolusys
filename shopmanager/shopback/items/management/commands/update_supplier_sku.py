38d26489e0055e142284546273b80341589038a5
wej <enjun.wang@xiaolu.so>
2016-04-28 22:43:56 +0800

    update sku outer_id

--- shopmanager/shopback/items/management/commands/update_supplier_sku.py
+++ shopmanager/shopback/items/management/commands/update_supplier_sku.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 import chardet
 from core.options import log_action, CHANGE
+import datetime
 import re
 
 from django.core.management.base import BaseCommand
 
 from flashsale.pay.models import SaleOrder
-from shopback.trades.models import MergeOrder
+from shopback.trades.models import MergeOrder, PackageSkuItem
 from shopback.items.models import Product, ProductSku
 from supplychain.supplier.models import SaleProduct
 
@@ -15,7 +16,7 @@ ADMIN_ID = 1
 
 
 class Command(BaseCommand):
-    PATTERN = re.compile(r'^[\w\-_#]*$')
+    PATTERN = re.compile(r'^[\w\-_]*$')
 
     def process_abnormal(self, sku):
         print sku.id, sku.outer_id
@@ -27,7 +28,7 @@ class Command(BaseCommand):
         ProductSku.objects.filter(id=sku.id).update(outer_id=new_outer_id)
         log_action(ADMIN_ID, sku, CHANGE, '更新outer_id, %s->%s' % (old_outer_id, new_outer_id))
 
-        for sale_order in SaleOrder.objects.filter(item_id=str(sku.product.id), outer_sku_id=old_outer_id):
+        for sale_order in SaleOrder.objects.filter(sku_id=str(sku.id)):
             SaleOrder.objects.filter(id=sale_order.id).update(outer_sku_id=new_outer_id)
             log_action(ADMIN_ID, sale_order, CHANGE, '更新outer_sku_id, %s->%s' % (old_outer_id, new_outer_id))
 
@@ -35,9 +36,12 @@ class Command(BaseCommand):
             MergeOrder.objects.filter(id=merge_order.id).update(outer_sku_id=new_outer_id)
             log_action(ADMIN_ID, merge_order, CHANGE, '更新outer_sku_id, %s->%s' % (old_outer_id, new_outer_id))
 
+        for package_sku_item in PackageSkuItem.objects.filter(sku_id=str(sku.id)):
+            PackageSkuItem.objects.filter(id=package_sku_item.id).update(outer_sku_id=new_outer_id)
+            log_action(ADMIN_ID, package_sku_item, CHANGE, '更新outer_sku_id, %s->%s' % (old_outer_id, new_outer_id))
+
+
 
     def handle(self, *args, **kwargs):
-        for sku in ProductSku.objects.filter(status=ProductSku.NORMAL):
-            m = self.PATTERN.match(sku.outer_id)
-            if not m:
-                self.process_abnormal(sku)
+        for sku in ProductSku.objects.raw('select * from shop_items_productsku where LENGTH(outer_id) != CHAR_LENGTH(outer_id) or outer_id regexp "#"'):
+            self.process_abnormal(sku)
