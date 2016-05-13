2b650a479658eb5984cf564b7e0a689e9b525b88
ahlinjie <1051631394@qq.com>
2016-05-13 16:43:01 +0800

    add the staff limit in stastics url

--- shopmanager/statistics/urls.py
+++ shopmanager/statistics/urls.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 __author__ = 'jie.lin'
 from django.conf.urls import url
+from django.contrib.admin.views.decorators import staff_member_required
 from django.views.decorators.csrf import csrf_exempt
 from statistics.views_pay import SaleNumberStatiticsView
 
 urlpatterns = (
-    url(r'^pay/salenum$', csrf_exempt(SaleNumberStatiticsView.as_view())),
+    url(r'^pay/salenum$', csrf_exempt(staff_member_required(SaleNumberStatiticsView.as_view()))),
 )
 
