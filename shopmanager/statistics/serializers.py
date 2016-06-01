c1e22aa9af6c660054ded04ebc5c0eaff794cb04
ahlinjie <1051631394@qq.com>
2016-06-01 09:31:42 +0800

    add the stats api for stats

--- shopmanager/statistics/serializers.py
+++ shopmanager/statistics/serializers.py
@@ -6,3 +6,40 @@ from statistics.models import SaleStats
 class StatsSerializer(serializers.ModelSerializer):
     class Meta:
         model = SaleStats
+        fields = ('id',
+                  'parent_id',
+                  'current_id',
+                  'date_field',
+                  'name',
+                  'pic_path',
+                  'num',
+                  'payment',
+                  'status',
+                  'get_status_display',
+                  'timely_type',
+                  'get_timely_type_display',
+                  'record_type',
+                  'get_record_type_display')
+
+
+class StatsAllNumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SaleStats
+        fields = ('id',
+                  'parent_id',
+                  'current_id',
+                  'date_field',
+                  'name',
+                  'pic_path',
+                  'num',
+                  'payment',
+                  'status',
+                  'get_status_display',
+                  'timely_type',
+                  'get_timely_type_display',
+                  'record_type',
+                  'get_record_type_display',
+                  "paid_num",
+                  'cancel_num',
+                  'out_stock_num',
+                  'return_goods_num')
