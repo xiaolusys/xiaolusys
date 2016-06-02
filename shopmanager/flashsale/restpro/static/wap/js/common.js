482b30597f770bb9e7a1b5ec632854a8626e7093
imeron <xiuqing.mei@xiaolu.so>
2016-06-02 15:18:36 +0800

    fix wap getLinkParam

--- shopmanager/flashsale/restpro/static/wap/js/common.js
+++ shopmanager/flashsale/restpro/static/wap/js/common.js
@@ -44,7 +44,7 @@ function isNone(value) {
 function getLinkParam(url,name){
     var reg = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var r = url.substr(1).match(reg);
-    if (r != null) return unescape(r[１]);
+    if (r != null) return unescape(r[1]);
     return null;
 }
 
