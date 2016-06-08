c0e540b581cacfdf646f01347d90abb0b8985e38
dh <hui.deng@example.com>
2016-06-08 21:15:16 +0800

    migrate

---
+++ shopmanager/flashsale/dinghuo/migrations/0015_orderlist_bill_method.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dinghuo', '0014_remove_orderlist_bill_method'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='orderlist',
+            name='bill_method',
+            field=models.IntegerField(default=11, verbose_name='\u4ed8\u6b3e\u7c7b\u578b', choices=[(11, '\u8d27\u5230\u4ed8\u6b3e'), (12, '\u9884\u4ed8\u6b3e'), (13, '\u4ed8\u6b3e\u63d0\u8d27'), (14, '\u5176\u5b83')]),
+        ),
+    ]
