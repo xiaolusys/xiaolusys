03458c856c69d01311ea79ef052adadf1ed0194a
ahlinjie <1051631394@qq.com>
2016-06-16 21:31:44 +0800

    change the nine pic upload page

--- shopmanager/flashsale/xiaolumm/serializers.py
+++ shopmanager/flashsale/xiaolumm/serializers.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from models import CashOut, CarryLog, XiaoluMama
+from models import CashOut, CarryLog, XiaoluMama, NinePicAdver
 from rest_framework import serializers
 
 
@@ -30,3 +30,23 @@ class CarryLogSerializer(serializers.ModelSerializer):
         model = CarryLog
         fields = ('xlmm', 'order_num', 'buyer_nick', 'value', 'value_money', 'log_type',
                   'log_type_name', 'carry_type', 'carry_type_name', 'status_name', 'carry_date')
+
+
+class NinePicAdverSerializer(serializers.ModelSerializer):
+    # start_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+    cate_gory_display = serializers.CharField(source='get_cate_gory_display', read_only=True)
+
+    class Meta:
+        model = NinePicAdver
+        fields = (
+            "id",
+            "auther",
+            "title",
+            "description",
+            "cate_gory",
+            "pic_arry",
+            "start_time",
+            "turns_num",
+            "is_pushed",
+            "detail_modelids",
+            "cate_gory_display")
