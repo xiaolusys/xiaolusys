726ebb0005392229be9aeb432d7320ae3aae1989
imeron <xiuqing.mei@xiaolu.so>
2016-06-20 13:21:45 +0800

    fix pay models envelop

--- shopmanager/flashsale/pay/models_user.py
+++ shopmanager/flashsale/pay/models_user.py
@@ -480,8 +480,11 @@ class BudgetLog(PayBaseModel):
 
 
 def budgetlog_update_userbudget(sender, instance, created, **kwargs):
+
+    logger.warning('budgetlog create update userbudget:%s, %s, %s, %s'%
+                   (instance.customer_id, instance.flow_amount, instance.referal_id, instance.status))
     from flashsale.pay.tasks import task_budgetlog_update_userbudget
-    task_budgetlog_update_userbudget.delay(instance)
+    task_budgetlog_update_userbudget(instance)
 
 
 post_save.connect(budgetlog_update_userbudget, sender=BudgetLog)
