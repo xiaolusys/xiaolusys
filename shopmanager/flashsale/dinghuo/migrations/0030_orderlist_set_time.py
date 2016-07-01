f07fff95f41d597149b6865b6a803efd0cb9bf13
huangyan <763294911@qq.com>
2016-07-01 15:20:41 +0800

    重设字段null

---
+++ shopmanager/flashsale/dinghuo/migrations/0030_orderlist_set_time.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dinghuo', '0029_orderlist_arrival_process'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='orderlist',
+            name='checked_time',
+            field=models.DateTimeField(default=None, null=True, verbose_name='\u68c0\u51fa\u65f6\u95f4'),
+        ),
+        migrations.AlterField(
+            model_name='orderlist',
+            name='paid_time',
+            field=models.DateTimeField(default=None, null=True, verbose_name='\u652f\u4ed8\u5b8c\u6210\u65f6\u95f4'),
+        ),
+        migrations.AlterField(
+            model_name='orderlist',
+            name='pay_time',
+            field=models.DateTimeField(default=None, null=True, verbose_name='\u5f00\u59cb\u652f\u4ed8\u65f6\u95f4'),
+        ),
+        migrations.AlterField(
+            model_name='orderlist',
+            name='receive_time',
+            field=models.DateTimeField(default=None, null=True, verbose_name='\u5f00\u59cb\u6536\u8d27\u65f6\u95f4'),
+        ),
+        migrations.AlterField(
+            model_name='orderlist',
+            name='received_time',
+            field=models.DateTimeField(default=None, null=True, verbose_name='\u5f00\u59cb\u7ed3\u7b97\u65f6\u95f4'),
+        ),
+    ]
