de8532d1ec51357e80d1670d018a356daa6ca982
ahlinjie <1051631394@qq.com>
2016-07-05 15:21:32 +0800

    add list api with diffrent serilizer for supplier

--- shopmanager/supplychain/supplier/views/views_apis.py
+++ shopmanager/supplychain/supplier/views/views_apis.py
@@ -179,6 +179,17 @@ class SaleScheduleViewSet(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = serializers.SaleProductManageSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class SaleScheduleDetailViewSet(viewsets.ModelViewSet):
     """
