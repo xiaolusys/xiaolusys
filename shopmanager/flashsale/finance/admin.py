673fbc02fc793a23ff57859a4701fc509d1f1afc
dh <hui.deng@example.com>
2016-07-09 20:38:57 +0800

    excel

--- shopmanager/flashsale/finance/admin.py
+++ shopmanager/flashsale/finance/admin.py
@@ -4,11 +4,13 @@ from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
+from django.http import HttpResponse
 
 import common.utils
 from core.filters import DateFieldListFilter
 from flashsale.finance.models import Bill, BillRelation
-
+import xlsxwriter
+from cStringIO import StringIO
 
 class BillAdmin(admin.ModelAdmin):
     list_display = (
@@ -118,13 +120,89 @@ class BillAdmin(admin.ModelAdmin):
 
     action_merge.short_description = u'合并账单'
 
-    actions = ['action_merge']
+    def format_billrelation(self,dict={}):
+        a = {'订货付款':[],'订货回款':[],'退货收款':[]}
+        # a= {}
+        pay = ''
+        back = ''
+        get = ''
+        for i,j in dict.iteritems():
+            if i=='订货付款':
+                pay = '订货付款：'
+                for k in j:
+                    pay = pay + k.object_id
+            elif i=='订货回款':
+                back = '订货回款：'
+                for k in j:
+                    back = back + k.object_id
+            #     a['订货回款'].append((k.object_id for k in j))
+            elif i=='退货收款':
+                get = '退货收款：'
+                for k in j:
+                    get = get + k.object_id
+            #     a['退货收款'].append((k.object_id for k in j))
+            # a[i].append(j)
+        return  pay+back+get
+
+
+    def generate_excel(self, request, queryset):
+        buff = StringIO()
+        workbook = xlsxwriter.Workbook(buff)
+        worksheet = workbook.add_worksheet()
+        bold_format = workbook.add_format({"bold":True})
+        money_format = workbook.add_format({"num_format":'0.00'})
+        worksheet.set_column('A:A',18)
+        worksheet.set_column('B:B',30)
+        worksheet.set_column('C:C',30)
+        worksheet.set_column('D:D',30)
+        worksheet.set_column('E:E',30)
+        worksheet.set_column('F:F',30)
+        worksheet.set_column('G:G',30)
+        worksheet.set_column('H:H',30)
+        worksheet.set_column('I:I',30)
+        worksheet.set_column('J:J',30)
+        worksheet.set_column('K:K',30)
+        worksheet.write('A6','ID',bold_format)
+        worksheet.write('B6','供应商',bold_format)
+        worksheet.write('C6','账单类型',bold_format)
+        worksheet.write('D6','关联信息',bold_format)
+        worksheet.write('E6','计划款额',bold_format)
+        worksheet.write('F6','实收款额',bold_format)
+        worksheet.write('G6','创建人', bold_format)
+        worksheet.write('H6','支付方式',bold_format)
+        worksheet.write('I6','创建日期',bold_format)
+        worksheet.write('J6','账单状态',bold_format)
+        worksheet.write('K6','备注',bold_format)
+        row = 7
+        for bill in queryset:
+            worksheet.write(row,0,bill.id)
+            worksheet.write(row,1,bill.supplier.supplier_name)
+            worksheet.write(row,2,bill.get_type_display())
+            worksheet.write(row,3,self.format_billrelation(bill.relation_objects))
+            worksheet.write(row,4,bill.plan_amount)
+            worksheet.write(row,5,bill.amount)
+            worksheet.write(row,6,bill.creater.username)
+            worksheet.write(row,7,bill.get_pay_method_display())
+            worksheet.write(row,8,bill.created)
+            worksheet.write(row,9,bill.get_status_display())
+            worksheet.write(row,10,bill.note)
+            row += 1
+        workbook.close()
+        filename = '%s-%s.xlsx' % (queryset[0],queryset[len(queryset)-1])
+        response = HttpResponse(
+            buff.getvalue(),
+            content_type=
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment;filename=%s' % filename
+        return response
+
+    generate_excel.short_description = u'导出excel'
+
+    actions = ['generate_excel','action_merge']
 
     def get_actions(self, request):
         actions = super(BillAdmin, self).get_actions(request)
-        if 'delete_selected' in actions:
-            del actions['delete_selected']
+        actions.pop('delete_selected')
         return actions
-
 
 admin.site.register(Bill, BillAdmin)
