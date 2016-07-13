879dcc7b6f1b5a7ebbc483fc02ef2202b4123e6f
huangyan <763294911@qq.com>
2016-07-13 21:37:32 +0800

    [promotion]hy-xiaolu-admin

--- shopmanager/games/weixingroup/admin.py
+++ shopmanager/games/weixingroup/admin.py
@@ -17,17 +17,29 @@ admin.site.register(XiaoluAdministrator, XiaoluAdministratorAdmin)
 
 class GroupMamaAdministratorAdmin(BaseModelAdmin):
     search_fields = ['admin__id', 'admin__username', 'admin__nick', 'mama_id']
-    list_display = ['id', 'mama_id', 'admin__id', 'admin__username', 'admin__nick', 'status', ]
+    list_display = ['id', 'mama_id', 'mama_nick', 'mama_mobile', 'admin__id', 'admin__username', 'admin__nick',  'status']
     list_filter = ['status']
 
     def admin__id(self, obj):
         return obj.admin.id
+    admin__id.short_description = u'小鹿管理员ID'
 
     def admin__username(self, obj):
         return obj.admin.username
+    admin__username.short_description = u'小鹿管理员用户名'
 
     def admin__nick(self, obj):
         return obj.admin.nick
+    admin__nick.short_description = u'小鹿管理员昵称'
+
+    def mama_nick(self, obj):
+        return obj.mama.manager_name
+    mama_nick.short_description = u'小鹿妈妈用户名'
+
+    def mama_mobile(self, obj):
+        return
+    mama_mobile.short_description = u'小鹿妈妈手机'
+
 
 admin.site.register(GroupMamaAdministrator, GroupMamaAdministratorAdmin)
 
--- shopmanager/games/weixingroup/views.py
+++ shopmanager/games/weixingroup/views.py
@@ -14,8 +14,7 @@ from flashsale.promotion.models import ActivityEntry
 from flashsale.xiaolumm.models import XiaoluMama
 from flashsale.xiaolumm.serializers import XiaoluMamaSerializer
 from shopapp.weixin.models import WeixinUserInfo
-import logging
-log = logging.getLogger(__name__)
+
 
 class XiaoluAdministratorViewSet(WeixinAuthMixin, viewsets.GenericViewSet):
     """
@@ -43,7 +42,6 @@ class XiaoluAdministratorViewSet(WeixinAuthMixin, viewsets.GenericViewSet):
                 if not self.valid_openid(unionid):
                     # 4. if we still dont have openid, we have to do oauth
                     redirect_url = self.get_snsuserinfo_redirct_url(request)
-                    log.error("redirect_url:" + redirect_url)
                     return redirect(redirect_url)
             xiaoumama = XiaoluMama.objects.filter(openid=unionid).first()
         if not xiaoumama:
