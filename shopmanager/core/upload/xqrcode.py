adb7a2ff07a63509086965159ca06d16a96e5ae3
imeron <xiuqing.mei@xiaolu.so>
2016-07-16 14:12:13 +0800

    fix inbound add template forecast id

--- shopmanager/core/upload/xqrcode.py
+++ shopmanager/core/upload/xqrcode.py
@@ -4,8 +4,11 @@ import cStringIO as StringIO
 
 from .upload import upload_public_to_remote, generate_public_url
 
-def push_qrcode_to_remote(key,qrlink):
-    """ 根据链接生成二维码，上传到第三方，并返回图片链接 """
+def push_qrcode_to_remote(key, qrlink):
+    """ 根据链接生成二维码，上传到第三方，并返回图片链接:
+        key: 二维码存储文件名;
+        qrlink： 二维码链接内容;
+    """
     qr = qrcode.QRCode(version=1, box_size=8, border=1)
     qr.add_data(qrlink)
     qr.make(fit=True)
