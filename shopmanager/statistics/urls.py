3a5c60af4ab3eff2d81a628e308d7d48e231e0cb
ahlinjie <1051631394@qq.com>
2016-07-19 16:12:51 +0800

    fix statistic bug

--- shopmanager/statistics/urls.py
+++ shopmanager/statistics/urls.py
@@ -4,8 +4,16 @@ from django.conf.urls import patterns, include, url
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.decorators.csrf import csrf_exempt
 from statistics.views import SaleNumberStatiticsView
+from rest_framework import routers
+from statistics.views import SaleStatsViewSet
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'salestats', SaleStatsViewSet)
+router_urls = router.urls
+router_urls += ([])
 
 urlpatterns = (
     url(r'^pay/salenum$', csrf_exempt(staff_member_required(SaleNumberStatiticsView.as_view()))),
+    url(r'^stats/', include(router_urls, namespace='stats')),
 )
 
--- shopmanager/statistics/views/__init__.py
+++ shopmanager/statistics/views/__init__.py
@@ -1,4 +1,4 @@
-from .views_api import *
+from .views_apis import *
 from .views import *
 from .views_stats import *
 from .views_pay import *
