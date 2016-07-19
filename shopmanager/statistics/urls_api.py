4e9a3780b7f658c42274ff502318b425c7740f43
ahlinjie <1051631394@qq.com>
2016-07-19 16:01:02 +0800

    statistic salestats base api

--- shopmanager/statistics/urls_api.py
+++ shopmanager/statistics/urls_api.py
@@ -4,11 +4,11 @@ from django.views.generic.base import TemplateView
 from django.views.decorators.cache import cache_page
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework import routers
-from .views import views_api
+from .views import views_apis
 
 
 router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'stats', views_api.SaleStatsViewSet)
+router.register(r'stats', views_apis.SaleStatsViewSet)
 
 v1_router_urls = router.urls
 v1_router_urls += ([])
