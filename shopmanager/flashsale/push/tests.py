bb00b5a1ff4bd39f10a7871b986280931ce94189
bo <bo.zhang@xiaolumeimei.com>
2016-08-04 20:50:49 +0800

    remove test

--- shopmanager/flashsale/push/tests.py
+++ shopmanager/flashsale/push/tests.py
@@ -1,18 +1,6 @@
 # coding: utf-8
 from django.test import TestCase
-from flashsale.pay.models.product import ModelProduct
-
-from .mipush import (
-    mipush_of_android,
-)
 
 
 class PushTestCase(TestCase):
-
-    def test_push_product_to_customer(self):
-        customer_id = 913405
-        model_id = 1
-        model = ModelProduct.objects.get(id=model_id)
-        target_url = 'com.jimei.xlmm://app/v1/products?product_id=http://m.xiaolumeimei.com/mall/product/details/%s' % model_id
-        msg = '您收藏的商品%s上架了，点击查看>>' % model.name
-        mipush_of_android.push_to_account(customer_id, {'target_url': target_url}, description=msg)
+    pass
