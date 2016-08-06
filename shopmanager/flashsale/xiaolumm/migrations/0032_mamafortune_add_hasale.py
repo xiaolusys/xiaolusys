8211382ab1d5ca23e4a1ab3971601a6ef08ec324
huangyan <763294911@qq.com>
2016-08-06 23:22:46 +0800

    [bugfix]hy-migrate

--- shopmanager/flashsale/xiaolumm/migrations/0032_mamafortune_add_hasale.py
+++ shopmanager/flashsale/xiaolumm/migrations/0032_mamafortune_add_hasale.py
@@ -7,10 +7,25 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('xiaolumm', '0031_mamafortune_add_active_num'),
+        ('xiaolumm', '0031_add_field_lessontopic_is_show'),
     ]
 
     operations = [
+        migrations.AddField(
+            model_name='mamafortune',
+            name='active_all_num',
+            field=models.IntegerField(default=0, verbose_name='\u603b\u6fc0\u6d3b\u6570'),
+        ),
+        migrations.AddField(
+            model_name='mamafortune',
+            name='active_normal_num',
+            field=models.IntegerField(default=0, verbose_name='\u666e\u901a\u5988\u5988\u6fc0\u6d3b\u6570'),
+        ),
+        migrations.AddField(
+            model_name='mamafortune',
+            name='active_trial_num',
+            field=models.IntegerField(default=0, verbose_name='\u8bd5\u7528\u5988\u5988\u6fc0\u6d3b\u6570'),
+        ),
         migrations.AddField(
             model_name='mamafortune',
             name='hasale_all_num',
