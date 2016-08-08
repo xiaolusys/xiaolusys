d98942f48bafb6a874c324098fdf248043da92ad
bo <bo.zhang@xiaolumeimei.com>
2016-08-08 19:40:00 +0800

    xiaolumm

--- shopmanager/flashsale/daystats/views_customer.py
+++ shopmanager/flashsale/daystats/views_customer.py
@@ -59,40 +59,50 @@ def execute_sql(cursor, sql):
 def index(req):
     now = datetime.now()
     p_start_date = req.GET.get('start_date', '2016-07-01')
-    p_end_date = req.GET.get('end_date', '%s-%s-%s' % (now.year, now.month, now.day))
+    p_end_date = req.GET.get('end_date', '%s-%s-%s' % (now.year, now.month, now.day+1))
     start_date = datetime.strptime(p_start_date, '%Y-%m-%d')
     end_date = datetime.strptime(p_end_date, '%Y-%m-%d')
 
-    # customers = Customer.objects.using('product').filter(
-    #     created__gte=start_date, created__lt=end_date).values('created')
+    cursor = connections['product'].cursor()
 
     where = ' created > "{0}" and created < "{1}" '.format(p_start_date, p_end_date)
 
-    cursor = connections['product'].cursor()
-    sql = 'SELECT DATE(created) as day, count(DATE(created)) FROM xiaoludb.flashsale_customer where {0} group by DATE(created);'.format(where)
-    print sql
+    sql = """SELECT DATE(created) as day, count(DATE(created))
+             FROM xiaoludb.flashsale_customer where {0} group by DATE(created);""".format(where)
     customers = execute_sql(cursor, sql)
 
-    sql = 'SELECT DATE(created), count(DATE(created)) FROM xiaoludb.flashsale_trade where {0} group by DATE(created)'.format(where)
-    print sql
+    sql = """SELECT DATE(created), count(DATE(created))
+             FROM xiaoludb.xiaolumm_xiaolumama WHERE {0} GROUP by DATE(created) """.format(where)
+    xiaolumm = execute_sql(cursor, sql)
+
+    sql = """SELECT DATE(created), count(DATE(created))
+             FROM xiaoludb.flashsale_trade where {0} group by DATE(created)""".format(where)
     trades_all = execute_sql(cursor, sql)
 
-    sql = 'SELECT DATE(created), count(DATE(created)) FROM xiaoludb.flashsale_trade where pay_time is not null and {0} group by DATE(created)'.format(where)
-    print sql
+    sql = """SELECT DATE(created), count(DATE(created))
+             FROM xiaoludb.flashsale_trade where pay_time is not null and {0} group by DATE(created)""".format(where)
     trades_pay = execute_sql(cursor, sql)
 
-    # trades_all = SaleTrade.objects.using('product').filter(
-        # created__gte=start_date, created__lt=end_date).values('created')
-    # trades_pay = SaleTrade.objects.using('product').filter(
-        # created__gte=start_date, created__lt=end_date, pay_time__isnull=False).values('created')
+    sql = """
+        SELECT DATE(flashsale_trade.pay_time), count(DATE(flashsale_trade.pay_time))
+        FROM xiaoludb.flashsale_trade
+        join xiaoludb.flashsale_customer on flashsale_customer.id=flashsale_trade.buyer_id
+        join xiaoludb.xiaolumm_xiaolumama on flashsale_customer.unionid=xiaolumm_xiaolumama.openid
+        where flashsale_trade.created > "{0}"
+            and flashsale_trade.created < "{1}"
+            and flashsale_trade.pay_time is not null
+        group by DATE(flashsale_trade.created)
+    """.format(p_start_date, p_end_date)
+    xiaolumm_trades = execute_sql(cursor, sql)
 
     customer_items = {
-        # '增加用户数': process_data(customers),
-        '增加用户数': [int(x[1]) for x in customers],
+        '新增小鹿妈妈': [int(x[1]) for x in xiaolumm],
+        '新增用户数': [int(x[1]) for x in customers],
     }
     trade_items = {
         '付款订单数': [int(x[1]) for x in trades_pay],
-        'trade_all_data': [int(x[1]) for x in trades_all],
+        '所有订单（含未付款）': [int(x[1]) for x in trades_all],
+        '来自小鹿妈妈订单': [int(x[1]) for x in xiaolumm_trades],
     }
 
     x_axis = [x.strftime('%Y-%m-%d') for x in generate_date(start_date, end_date)]
