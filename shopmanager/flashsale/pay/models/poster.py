37aa3a7c1340e641ce4034cdc228bb8dead0d51d
wulei <crackwl@163.com>
2016-08-09 11:50:41 +0800

    女装童装入口图片修改

--- shopmanager/flashsale/pay/models/poster.py
+++ shopmanager/flashsale/pay/models/poster.py
@@ -67,10 +67,10 @@ class GoodShelf(PayBaseModel):
     def get_cat_imgs(self):
         return [
             {'id': 5, 'name': u'童装专区',
-             'cat_img': 'http://img.xiaolumeimei.com/top101469803077209child.png',
+             'cat_img': 'http://img.xiaolumeimei.com/top101470714441794child.jpg',
              'cat_link': '/mall/product/list/child'},
             {'id': 8, 'name': u'女装专区',
-             'cat_img': 'http://img.xiaolumeimei.com/top101469803308483women.png',
+             'cat_img': 'http://img.xiaolumeimei.com/top101470714481475women.jpg',
              'cat_link': '/mall/product/list/lady'},
         ]
 
