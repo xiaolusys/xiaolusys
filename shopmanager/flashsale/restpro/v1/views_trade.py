5a61ea7d77e912383d4bfc2d91436b4b0b6ef448
imeron <xiuqing.mei@xiaolu.so>
2016-08-09 09:44:35 +0800

    fix restpro v2 viewstrade

--- shopmanager/flashsale/restpro/v1/views_trade.py
+++ shopmanager/flashsale/restpro/v1/views_trade.py
@@ -893,7 +893,7 @@ class SaleTradeViewSet(viewsets.ModelViewSet):
         cookies = dict([(k, v) for k, v in request.COOKIES.items() if k in ('mm_linkid', 'ufrom')])
         logger.info({'code': 0, 'info': u'付款请求', 'channel': data.get('channel'),
                      'user_agent':request.META.get('HTTP_USER_AGENT'), 'cookies':cookies,
-                     'stype': 'restpro.trade', 'tid': data.get('uuid')})
+                     'stype': 'restpro.trade', 'tid': data.get('uuid'), 'payment': data.get('payment')})
 
     @list_route(methods=['post'])
     def shoppingcart_create(self, request, *args, **kwargs):
