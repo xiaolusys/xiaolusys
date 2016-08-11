0bc1bfdb3f935c3e337f6e165784ce490bfeb83b
ahlinjie <1051631394@qq.com>
2016-08-11 20:51:34 +0800

    add finance api

---
+++ shopmanager/flashsale/finance/urls_api.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+from django.conf.urls import patterns, include, url
+from rest_framework import routers
+from . import views_apis
+
+router = routers.DefaultRouter(trailing_slash=False)
+router_urls = router.urls
+
+router_urls += ([
+    url(r'^channel_pay_stats$', views_apis.FinanceChannelPayApiView.as_view(), name='v1-finance-channel-pay-stats'),
+    url(r'^refund_stats$', views_apis.FinanceRefundApiView.as_view(), name='v1-finance-refund-status-stats'),
+    url(r'^deposit_stats$', views_apis.FinanceDepositApiView.as_view(), name='v1-finance-deposit-stats'),
+    url(r'^cost_stats$', views_apis.FinanceCostApiView.as_view(), name='v1-finance-cost-stats'),
+    url(r'^stock_stats$', views_apis.FinanceStockApiView.as_view(), name='v1-finance-stock-stats'),
+])
+
+urlpatterns = patterns('',
+                       url(r'^v1/', include(router_urls, namespace='finance_v1')),
+                       )
+
