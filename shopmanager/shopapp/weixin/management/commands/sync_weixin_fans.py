3e391ea18ff4b43d8be4bf2bef04273f26ce558b
bo <bo.zhang@xiaolumeimei.com>
2016-08-12 18:25:35 +0800

    监听微信公众好关注、取关事件

--- shopmanager/shopapp/weixin/management/commands/sync_weixin_fans.py
+++ shopmanager/shopapp/weixin/management/commands/sync_weixin_fans.py
@@ -9,31 +9,55 @@ from shopapp.weixin.models_base import WeixinFans
 
 
 class Command(BaseCommand):
+    def sync_fans(self, appkey):
+        num = 0
+        next_openid = ''
+
+        while 1:
+            wxapi = WeiXinAPI()
+            wxapi.setAccountId(appKey=appkey)
+            json = wxapi.getFollowersID(next_openid=next_openid)
+            openids = json.get('data', {}).get('openid', [])
+            next_openid = json.get('next_openid', None)
+            total = json.get('total', None)
+
+            openids_in_db = WeixinFans.objects.filter(app_key=appkey).values('openid')
+            openids_in_db = [x['openid'] for x in openids_in_db]
+
+            openids = [x for x in openids if x not in openids_in_db]
+
+            for openid in openids:
+                fans = WeixinFans.objects.filter(app_key=appkey, openid=openid).first()
+                if not fans:
+                    try:
+                        user_info = wxapi.getCustomerInfo(openid)
+                    except Exception:
+                        wxapi = WeiXinAPI()
+                        wxapi.setAccountId(appKey=appkey)
+                        user_info = wxapi.getCustomerInfo(openid)
+
+                    unionid = user_info.get('unionid', '')
+                    subscribe = True if user_info.get('subscribe', False) else False
+                    subscribe_time = datetime.fromtimestamp(user_info['subscribe_time']) if subscribe else None
+
+                    fans = WeixinFans()
+                    fans.openid = openid
+                    fans.app_key = appkey
+                    fans.unionid = unionid
+                    fans.subscribe = subscribe
+                    fans.subscribe_time = subscribe_time
+                    fans.save()
+                num = num + 1
+                print '%s/%s' % (num, total)
+
+            if not next_openid:
+                break
+
     def handle(self, *args, **options):
-        appkey = settings.WXPAY_APPID  # 小鹿美美公众号
+        appkeys = [
+            settings.WXPAY_APPID,  # 小鹿美美公众号
+            # settings.WEIXIN_APPID,  # 小鹿特卖
+        ]
 
-        wxapi = WeiXinAPI()
-        wxapi.setAccountId(appKey=appkey)
-        json = wxapi.getFollowersID()
-        openids = json.get('data', {}).get('openid', [])
-
-        for openid in openids[:100]:
-            fans = WeixinFans.objects.filter(app_key=appkey, openid=openid).first()
-            if not fans:
-                user_info = wxapi.getCustomerInfo(openid)
-
-                unionid = user_info.get('unionid', '')
-                subscribe = True if user_info.get('subscribe', False) else False
-                subscribe_time = datetime.fromtimestamp(user_info['subscribe_time']) if subscribe else None
-
-                fans = WeixinFans()
-                fans.openid = openid
-                fans.app_key = appkey
-                fans.unionid = unionid
-                fans.subscribe = subscribe
-                fans.subscribe_time = subscribe_time
-                fans.save()
-
-                print '==>', 'insert ', openid, unionid, subscribe
-            else:
-                print '==>', 'has ', openid
+        for appkey in appkeys:
+            self.sync_fans(appkey)
