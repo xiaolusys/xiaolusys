e318ff74d57b76e37478dc9659a9ee7eca3cfcf5
bo <bo.zhang@xiaolumeimei.com>
2016-08-15 18:18:06 +0800

    fix bug

--- shopmanager/shopback/items/tasks_stats.py
+++ shopmanager/shopback/items/tasks_stats.py
@@ -2,8 +2,6 @@ from django.db import IntegrityError
 from django.db.models import Sum
 from celery.task import task
 
-from flashsale.push import push_product
-
 import logging
 logger = logging.getLogger(__name__)
 
