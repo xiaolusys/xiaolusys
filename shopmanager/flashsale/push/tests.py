c779ec354dd2c4b3122b0a0be0e22860db70df3a
bo <bo.zhang@xiaolumeimei.com>
2016-08-19 11:53:49 +0800

    透传测试

--- shopmanager/flashsale/push/tests.py
+++ shopmanager/flashsale/push/tests.py
@@ -11,4 +11,5 @@ if __name__ == '__main__':
     import django
     django.setup()
     ordercarry = OrderCarry.objects.get(id=1)
-    AppPush.push_mama_ordercarry(ordercarry)
+    # AppPush.push_mama_ordercarry(ordercarry)
+    AppPush.push_pass_through(913405)
