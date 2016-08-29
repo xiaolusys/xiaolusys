51f4bbf38e2783523b5d9c5b93ad888a2a80a54f
imeron <xiuqing.mei@xiaolu.so>
2016-08-29 12:11:52 +0800

    fix mama mision referal order payment and week range

--- shopmanager/core/utils/timeutils.py
+++ shopmanager/core/utils/timeutils.py
@@ -1,17 +1,24 @@
+import datetime
 from datetime import timedelta
 
 def week_range(date):
+    if isinstance(date, datetime.datetime):
+        date = datetime.datetime.combine(date.date(), datetime.time.min)
+
     year, week, dow = date.isocalendar()
 
     # Find the first day of the week.
-    if dow == 7:
+    if dow == 1:
         # Since we want to start with Sunday, let's test for that condition.
         start_date = date
     else:
+        delta = dow - 1
         # Otherwise, subtract `dow` number days to get the first day
-        start_date = date - timedelta(dow)
+        start_date = date - timedelta(delta)
 
     # Now, add 6 for the last day of the week (i.e., count up to Saturday)
     end_date = start_date + timedelta(6)
+    if isinstance(start_date, datetime.datetime):
+        end_date = datetime.datetime.combine(end_date ,datetime.time.max)
 
     return (start_date, end_date)
