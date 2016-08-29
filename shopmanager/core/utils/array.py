e5a755ed9e2c4011e57ab77936e94716511d3896
imeron <xiuqing.mei@xiaolu.so>
2016-08-29 17:32:50 +0800

    fix items add item muying template

--- shopmanager/core/utils/array.py
+++ shopmanager/core/utils/array.py
@@ -1,2 +1,14 @@
+from collections import defaultdict
 
-flatten = lambda x: [y for l in x for y in flatten(l)] if type(x) is list else [x]
+def iterable(obj):
+    try: iter(obj)
+    except: return False
+    return True
+
+flatten = lambda x: [y for l in x for y in flatten(l)] if iterable(x) else [x]
+
+def group_count(list):
+    d = defaultdict(int)
+    for key in list:
+        d[key] += 1
+    return d
