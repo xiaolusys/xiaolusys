9f9b1b743929484278d58bf1a647e9cd501b56c5
hui.deng3 <hlkysf@163.com>
2016-08-31 14:13:20 +0800

    论坛推送

--- shopmanager/flashsale/luntan/urls.py
+++ shopmanager/flashsale/luntan/urls.py
@@ -1,12 +1,21 @@
 from django.conf.urls import url,include
 import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import routers
+
 # urlpatterns = [
 #     url(r'at_push/(?P<customer_id>\d+)/$',views.at_push,name='at_push')
 # ]
+# urlpatterns = [
+#     url(r'at_push/(?P<customer_id>\d+)/$', views.LuntanPushViewSet.as_view('list')),
+# ]
+#
+# urlpatterns += format_suffix_patterns(urlpatterns)
 
 urlpatterns = [
-    url(r'at_push/(?P<customer_id>\d+)/$', views.LuntanPushViewSet.as_view('list')),
+
 ]
 
-urlpatterns += format_suffix_patterns(urlpatterns)
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'at_push',views.LuntanPushViewSet,'at_push')
+urlpatterns += router.urls
