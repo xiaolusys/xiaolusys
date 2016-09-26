b683d6aa758c2a187dd668c9718e6aeda01198c9
huangyan <763294911@qq.com>
2016-09-26 20:20:58 +0800

    [promotion]hy-stats-admin

--- shopmanager/flashsale/dinghuo/models/inbound.py
+++ shopmanager/flashsale/dinghuo/models/inbound.py
@@ -538,6 +538,11 @@ class InBound(models.Model):
             完成质检
         :return:
         """
+        self.status = InBound.COMPLETED
+        self.checked = True
+        self.check_time = datetime.datetime.now()
+        self.set_stat()
+        self.save()
         for inbound_detail_id in data:
             inbound_detail = InBoundDetail.objects.get(id=inbound_detail_id)
             if inbound_detail.checked:
@@ -547,11 +552,6 @@ class InBound(models.Model):
                 inbound_detail.set_quantity(data[inbound_detail_id]["arrivalQuantity"],
                                             data[inbound_detail_id]["inferiorQuantity"])
                 inbound_detail.finish_check2()
-        self.status = InBound.COMPLETED
-        self.checked = True
-        self.check_time = datetime.datetime.now()
-        self.set_stat()
-        self.save()
         self.update_orderlist_inbound()
 
     def need_return(self):
