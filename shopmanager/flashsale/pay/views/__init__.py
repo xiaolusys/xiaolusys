349ebfafd757b12e807900071426f3ffc0a01ac7
ahlinjie <1051631394@qq.com>
2016-10-18 13:58:15 +0800

    adjust refund code

--- shopmanager/flashsale/pay/views/__init__.py
+++ shopmanager/flashsale/pay/views/__init__.py
@@ -4,7 +4,7 @@ from .aggregate import AggregateProductView, ModelProductView, CheckModelExistVi
     AggregateProductCheckView, ChuanTuAPIView, ModelChangeAPIView
 from .login import flashsale_login, productlist_redirect, weixin_login, weixin_test, weixin_auth_and_redirect
 from .address import AddressList, UserAddressDetail, DistrictList
-from .refund import  RefundPopPageView, RefundReason, RefundAnaList
+from .refund import  RefundReason, RefundAnaList
 from .product import productsku_quantity_view, ProductDetailView
 from .order import order_flashsale, time_rank, sale_state, refund_state, refunding_state, preorder_flashsale, \
     nextorder_flashsale, search_flashsale, change_sku_item, refund_fee, update_memo, sent_sku_item_again, get_mrgid
--- shopmanager/flashsale/restpro/v1/views_refund.py
+++ shopmanager/flashsale/restpro/v1/views_refund.py
@@ -1,9 +1,7 @@
 # coding=utf-8
 import datetime
-import math
 import logging
 from core.options import log_action, ADDITION, CHANGE
-from django.shortcuts import get_object_or_404
 from rest_framework import exceptions
 from common.modelutils import update_model_fields
 from flashsale.pay.models import SaleOrder
