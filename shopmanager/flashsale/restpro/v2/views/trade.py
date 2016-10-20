137fea2603e83949846e8800798612b7b2cae73f
wulei <crackwl@163.com>
2016-10-20 20:29:03 +0800

    购买优惠券订单支付不要校验地址

--- shopmanager/flashsale/restpro/v2/views/trade.py
+++ shopmanager/flashsale/restpro/v2/views/trade.py
@@ -564,7 +564,7 @@ class SaleTradeViewSet(viewsets.ModelViewSet):
         try:
             cart_discount += self.calc_extra_discount(pay_extras,**extra_params)
         except Exception, exc:
-            logger.warn({'cointde':3, 'message':exc, 'stype':'restpro.trade',
+            logger.warn({'code':3, 'message':exc, 'stype':'restpro.trade',
                          'user_agent': user_agent, 'tid':tuuid , 'data': '%s'%CONTENT})
             return Response({'code':3,'info':exc.message})
 
