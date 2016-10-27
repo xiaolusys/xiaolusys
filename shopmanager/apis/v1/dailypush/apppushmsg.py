1c4a51115818391642ff29374dc6eb80ce831f3e
ahlinjie <1051631394@qq.com>
2016-10-27 19:29:45 +0800

    change code style

---
+++ shopmanager/apis/v1/dailypush/apppushmsg.py
@@ -0,0 +1,87 @@
+# coding=utf-8
+__ALL__ = [
+    'get_app_push_msg_by_id',
+    'get_minutes_failed_msgs',
+    'app_msg_push_2_client_by_id',
+]
+import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def get_app_push_msg_by_id(id):
+    # type: (int) -> APPFullPushMessge
+    from flashsale.protocol.models import APPFullPushMessge
+
+    return APPFullPushMessge.objects.get(id=id)
+
+
+def get_minutes_failed_msgs(minutes=30):
+    # type: (int) -> List[APPFullPushMessge]
+    """指定（默认30）分钟内推送失败的（没有推送成功的）推送记录
+    """
+    from flashsale.protocol.models import APPFullPushMessge
+
+    now = datetime.datetime.now()
+    thirth_minute_ago = now - datetime.timedelta(minutes=minutes)  # 30分钟之前的时间 failed msg
+    allpushsmss = APPFullPushMessge.objects.filter(push_time__gte=thirth_minute_ago,
+                                                   push_time__lt=now,
+                                                   status=APPFullPushMessge.FAIL).order_by('-push_time')
+    return allpushsmss
+
+
+def app_msg_push_2_client_by_id(id):
+    # type: (int) -> None
+    """推送消息给客户端
+    """
+    from flashsale.protocol import get_target_url
+    from flashsale.xiaolumm import util_emoji
+    from flashsale.push.app_push import AppPush
+    from flashsale.protocol.models import APPFullPushMessge
+
+    now = datetime.datetime.now()
+    push_msg = get_app_push_msg_by_id(id)
+    if now < push_msg.push_time:  # 定义的推送时间　没有到　则不推送
+        return
+    raise Exception(u'上线去掉!')
+    target_url = get_target_url(push_msg.target_url, push_msg.params)
+    msg = util_emoji.match_emoji(push_msg.desc)  # 生成推送内容
+
+    if push_msg.platform == APPFullPushMessge.PL_ALL:
+        resp = AppPush.push_to_all(target_url, msg)
+    elif push_msg.platform in [APPFullPushMessge.PL_IOS, APPFullPushMessge.PL_ANDROID]:
+        resp = AppPush.push_to_platform(push_msg.platform, target_url, msg)
+    else:
+        resp = AppPush.push_to_topic(push_msg.platform, target_url, msg)
+
+    push_msg.result = resp
+    success = resp.get('android', {}).get('result', None) or resp.get('ios', {}).get('result', None)
+    if success and success.lower() == 'ok':
+        push_msg.status = APPFullPushMessge.SUCCESS  # 保存推送成功状态
+    push_msg.save(update_fields=['status', 'modified'])
+
+    logger.info({
+        'action': 'dailypush.app_msg_push_2_client_by_id',
+        'msg_id': id
+    })
+
+
+def push_msg_right_now_by_id(id):
+    # type: (id, Optional[int]) -> bool
+    """执行则立即推送（执行task）
+    """
+    from flashsale.protocol.tasks import task_site_push
+    from flashsale.protocol.models import APPFullPushMessge
+
+    push_msg = get_app_push_msg_by_id(id)
+    if not push_msg.push_time or push_msg.status == APPFullPushMessge.SUCCESS:
+        return False
+    try:
+        task_site_push.delay(push_msg.id)
+    except Exception, exc:
+        resp = {'error': exc.message}
+        push_msg.result = resp
+        push_msg.save(update_fields=['result', 'modified'])
+        return False
+    return True
--- shopmanager/flashsale/protocol/admin.py
+++ shopmanager/flashsale/protocol/admin.py
@@ -1,10 +1,9 @@
-# -*- coding:utf-8 -*-
+# coding=utf-8
 from django.contrib import admin
 from .models import APPFullPushMessge
-import logging
-from flashsale.xiaolumm import util_emoji
 from django.contrib import messages
-from flashsale.protocol.tasks import task_site_push
+
+import logging
 
 logger = logging.getLogger('django.request')
 
@@ -17,64 +16,33 @@ class APPFullPushMessgeAdmin(admin.ModelAdmin):
     list_per_page = 40
 
     # -------------- 页面布局 --------------
-    fieldsets = (('基本信息:', {
-        'classes': ('expand',),
-        'fields': (
-            'target_url',
-            'platform',
-            'push_time',
-            'desc'
-        )
-    }),
-                 ('内部信息:', {
-                     'classes': ('collapse',),
-                     'fields': (('cat', 'status', 'regid'),
-                                ('params', 'result',))
+    fieldsets = (
+        ('基本信息:', {
+            'classes': ('expand',),
+            'fields': (
+                'target_url',
+                'platform',
+                'push_time',
+                'desc'
+            )
+        }),
+        ('内部信息:', {
+            'classes': ('collapse',),
+            'fields': (('cat', 'status', 'regid'),
+                       ('params', 'result',))
 
-                 }),)
-
-    # def save_model(self, request, obj, form, change):
-    # """
-    # Given a model instance save it to the database.
-    #     """
-    #     try:
-    #         from flashsale.push import mipush
-    #         from flashsale.protocol import get_target_url
-    #         resp = {}
-    #         params = {'target_url': get_target_url(obj.target_url, obj.params)}
-    #
-    #         desc = util_emoji.match_emoji(obj.desc)
-    #
-    #         if obj.platform == APPFullPushMessge.PL_IOS:
-    #             resp = mipush.mipush_of_ios.push_to_all(params, description=desc)
-    #         else:
-    #             resp = mipush.mipush_of_android.push_to_all(params, description=desc)
-    #     except Exception, exc:
-    #         logger.error(exc.message or 'app push error', exc_info=True)
-    #         resp = {'error': exc.message}
-    #     success = resp.get('result')
-    #     if success and success.lower() == 'ok':
-    #         obj.status = APPFullPushMessge.SUCCESS
-    #         self.message_user(request, u"=======, %s用户全推成功." % obj.get_platform_display())
-    #     else:
-    #         self.message_user(request, u"xxxxxxx 很抱歉,%s用户全推失败!xxxxxxxx" % obj.get_platform_display())
-    #     obj.result = resp
-    #     obj.save()
+        }),
+    )
 
     def push_action(self, request, queryset):
         if queryset.count() == 1:
             obj = queryset[0]
-            if not obj.push_time:
-                return self.message_user(request, u'没有设置推送时间前不予手动推送', level=messages.WARNING)
-            try:
-                task_site_push.delay(obj)
-                return self.message_user(request, u'推送成功')
-            except Exception, exc:
-                logger.error(exc.message or 'Site push error', exc_info=True)
-                resp = {'error': exc.message}
-                obj.result = resp
-                obj.save()
+            from apis.v1.dailypush.apppushmsg import push_msg_right_now_by_id
 
+            is_push = push_msg_right_now_by_id(obj.id)
+            if is_push:
+                return self.message_user(request, u'推送成功!')
+            return self.message_user(request, u'推送失败!', level=messages.ERROR)
         return self.message_user(request, u'勾选一个推送项', level=messages.WARNING)
 
     push_action.short_description = u'站点推送'
