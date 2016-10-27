9df3f02b40364862275920f9f41709f1e99fa3e8
ahlinjie <1051631394@qq.com>
2016-10-27 20:18:32 +0800

    change fun name

--- shopmanager/flashsale/protocol/tasks.py
+++ shopmanager/flashsale/protocol/tasks.py
@@ -10,9 +10,9 @@ def task_site_push(id=None):
     # type: (Optional[int]) -> None
     """如果有半个小时内的推送设置记录（未推送状态的）　则　执行推送该消息给客户端程序
     """
-    from apis.v1.dailypush.apppushmsg import app_msg_push_2_client_by_id, get_minutes_failed_msgs
+    from apis.v1.dailypush.apppushmsg import push_app_push_msg_2_client_by_id, get_minutes_failed_msgs
     if id:
-        app_msg_push_2_client_by_id(id)
+        push_app_push_msg_2_client_by_id(id)
     else:
         for obj in get_minutes_failed_msgs():
-            app_msg_push_2_client_by_id(obj.id)
+            push_app_push_msg_2_client_by_id(obj.id)
