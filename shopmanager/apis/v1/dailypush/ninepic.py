d088a64feea94ef141c9331a6e3d8dfafa9acddb
ahlinjie <1051631394@qq.com>
2016-10-27 15:02:16 +0800

    adjust code for apis

--- shopmanager/apis/v1/dailypush/ninepic.py
+++ shopmanager/apis/v1/dailypush/ninepic.py
@@ -2,8 +2,46 @@
 __ALL__ = [
     'get_nine_pic_advertisement_by_id',
     'get_nine_pic_advertisements_by_ids',
+    'create_nine_pic_advertisement',
+    'delete_nine_pic_advertisement_by_id',
+    'update_nine_pic_advertisement_by_id',
     'NinePicAdvertisement',
 ]
+import datetime
+
+
+def _init_time(assign_date=None):
+    # type: (Optional[datetime.date]) -> datetime.datetime
+    now = datetime.datetime.now() if assign_date is None else datetime.datetime(assign_date.year,
+                                                                                assign_date.month,
+                                                                                assign_date.day, 0, 0)
+    return datetime.datetime(now.year, now.month, now.day, 0, 0, 0)
+
+
+def _calculate_create_assign_turns_num(assign_datetime=None):
+    # type: (Optional[datetime.datetime]) -> int
+    from flashsale.xiaolumm.models import NinePicAdver
+
+    init_time = NinePicAdver.init_time(assign_datetime.date())
+    end_time = datetime.datetime(init_time.year, init_time.month, init_time.day, 23, 59, 59)
+    return NinePicAdver.objects.filter(start_time__gte=init_time, start_time__lte=end_time).count()
+
+
+def _resort_turns_num(date):
+    # type: (datetime.date) -> None
+    """指定日期的九张图推广轮数重新排序
+    """
+    from flashsale.xiaolumm.models import NinePicAdver
+
+    init_time = _init_time(date)
+    end_time = datetime.datetime(init_time.year, init_time.month, init_time.day, 23, 59, 59)
+    count = 1
+    for ninepic in NinePicAdver.objects.filter(start_time__gte=init_time,
+                                               start_time__lte=end_time).order_by('start_time'):
+        ninepic.turns_num = count
+        ninepic.save(update_fields=['turns_num'])
+        count += 1
+    return
 
 
 def get_nine_pic_advertisement_by_id(nine_pic_id):
@@ -18,6 +56,79 @@ def get_nine_pic_advertisements_by_ids(ids):
     from flashsale.xiaolumm.models import NinePicAdver
 
     return NinePicAdver.objects.filter(id__in=ids)
+
+
+def create_nine_pic_advertisement(author, title, start_time, **kwargs):
+    # type: (text_type, text_type, datetime.datetime, *Any, **Any) -> NinePicAdver
+    from flashsale.xiaolumm.models import NinePicAdver
+
+    pic_arry = kwargs.get('pic_arry') or None
+    description = kwargs.get('description') or ''
+    advertisement_type = kwargs.get('advertisement_type') or 9
+    category_id = kwargs.get('category_id') or None
+    is_pushed = kwargs.get('is_pushed') or False
+    redirect_url = kwargs.get('redirect_url') or ''
+    detail_modelids = kwargs.get('detail_modelids') or ''
+    memo = kwargs.get('memo') or ''
+
+    turns_num = _calculate_create_assign_turns_num(start_time)  # 轮数
+    verify_turns_num = NinePicAdver.objects.filter(start_time__gte=_init_time(start_time.date()),
+                                                   start_time__lt=start_time).count()
+    if turns_num != verify_turns_num:
+        raise Exception(u'请设置 **开始时间** 在当前最后一轮以后!')
+    n = NinePicAdver(auther=author,
+                     title=title,
+                     start_time=start_time,
+                     description=description,
+                     cate_gory=advertisement_type,
+                     sale_category=category_id,
+                     pic_arry=pic_arry,
+                     turns_num=turns_num + 1,
+                     category_id=category_id,
+                     is_pushed=is_pushed,
+                     detail_modelids=detail_modelids,
+                     redirect_url=redirect_url,
+                     memo=memo)
+    n.save()
+    return n
+
+
+def delete_nine_pic_advertisement_by_id(id):
+    # type: () -> bool
+    """删除记录
+    1. 删除记录
+    2. 重新排轮数
+    """
+    ninepic = get_nine_pic_advertisement_by_id(id)
+    date = ninepic.start_time.date()
+    ninepic.delete()
+    _resort_turns_num(date)
+    return True
+
+
+def update_nine_pic_advertisement_by_id(id, **kwargs):
+    # type: (int, **Any) -> NinePicAdver
+    """更新记录
+    １. 如果有时间变化，则重新排轮数
+    """
+    ninepic = get_nine_pic_advertisement_by_id(id)
+    if kwargs.has_key('turns_num'):  # 不更新传入的turns_num
+        kwargs.pop('turns_num')
+    if kwargs.has_key('sale_category'):
+        kwargs.update({'sale_category_id': kwargs.pop('sale_category')})
+    if not kwargs.has_key('start_time'):  # 没有重新设置时间则不去更新时间和　turns_num
+        kwargs.update({'turns_num': ninepic.turns_num})
+    else:
+        start_time = datetime.datetime.strptime(kwargs.get('start_time'), '%Y-%m-%d %H:%M:%S')
+        old_start_time_date = ninepic.start_time.date()
+        if old_start_time_date != start_time.date():  # 不相等则都重新排序修改　轮数
+            _resort_turns_num(old_start_time_date)
+        _resort_turns_num(start_time.date())
+    for k, v in kwargs.iteritems():
+        if hasattr(ninepic, k) and getattr(ninepic, k) != v:
+            setattr(ninepic, k, v)
+    ninepic.save()
+    return ninepic
 
 
 class NinePicAdvertisement(object):
@@ -37,31 +148,3 @@ class NinePicAdvertisement(object):
         self.redirect_url = ''  # 跳转链接
         self.detail_modelids = ''
         self.memo = ''
-
-    @classmethod
-    def create(cls, auther, title, start_time, **kwargs):
-        # type: (text_type, text_type, datetime.datetime, *Any, **Any) -> NinePicAdvertisement
-        from flashsale.xiaolumm.models import NinePicAdver
-        pic_arry = kwargs.get('pic_arry') or None
-        description = kwargs.get('description') or ''
-        advertisement_type = kwargs.get('advertisement_type') or 9
-        category_id = kwargs.get('category_id') or None
-        is_pushed = kwargs.get('is_pushed') or False
-        redirect_url = kwargs.get('redirect_url') or ''
-        detail_modelids = kwargs.get('detail_modelids') or ''
-        memo = kwargs.get('memo') or ''
-        return NinePicAdver.create(auther, title, start_time,
-                                   pic_arry=pic_arry, description=description, advertisement_type=advertisement_type,
-                                   category_id=category_id, is_pushed=is_pushed, redirect_url=redirect_url,
-                                   detail_modelids=detail_modelids, memo=memo)
-
-    @classmethod
-    def update(cls, pk, **kwargs):
-        # type: (int, **Any) -> NinePicAdver
-        ninepic = get_nine_pic_advertisement_by_id(pk)
-        return ninepic.update(**kwargs)
-
-    @classmethod
-    def destroy(cls, pk):
-        ninepic = get_nine_pic_advertisement_by_id(pk)
-        return ninepic.destroy()
