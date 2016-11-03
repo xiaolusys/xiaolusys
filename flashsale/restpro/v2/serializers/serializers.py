6c421424a2a650d730b3cae13e3113ffef043894
ahlinjie <1051631394@qq.com>
2016-11-03 17:41:27 +0800

    change the trade detail package api

--- flashsale/restpro/v2/serializers/serializers.py
+++ flashsale/restpro/v2/serializers/serializers.py
@@ -882,24 +882,25 @@ class SaleTradeDetailSerializer(serializers.HyperlinkedModelSerializer):
         if obj.status not in SaleTrade.INGOOD_STATUS:
             return []
         package_list = PackageOrderSerializer(obj.package_orders, many=True).data
-        for sale_order in obj.sale_orders.all():
-            if not sale_order.is_packaged():
-                package_sku_item = sale_order.package_sku
-                package_list.insert(0, {
-                    'id': '',
-                    'logistics_company': None,
-                    'process_time': package_sku_item and package_sku_item.process_time,
-                    'pay_time': package_sku_item and package_sku_item.pay_time or obj.pay_time,
-                    'book_time': package_sku_item and package_sku_item.book_time,
-                    'assign_time': package_sku_item and package_sku_item.assign_time,
-                    'finish_time': package_sku_item and package_sku_item.finish_time,
-                    'cancel_time': package_sku_item and package_sku_item.cancel_time,
-                    'assign_status_display': package_sku_item and package_sku_item.get_assign_status_display() or '',
-                    'ware_by_display': package_sku_item and package_sku_item.get_ware_by_display() or '',
-                    'out_sid': '',
-                    'note': ''
-                })
-                break
+
+        # for sale_order in obj.sale_orders.all():
+        #     if not sale_order.is_packaged():
+        #         package_sku_item = sale_order.package_sku
+        #         package_list.insert(0, {
+        #             'id': '',
+        #             'logistics_company': None,
+        #             'process_time': package_sku_item and package_sku_item.process_time,
+        #             'pay_time': package_sku_item and package_sku_item.pay_time or obj.pay_time,
+        #             'book_time': package_sku_item and package_sku_item.book_time,
+        #             'assign_time': package_sku_item and package_sku_item.assign_time,
+        #             'finish_time': package_sku_item and package_sku_item.finish_time,
+        #             'cancel_time': package_sku_item and package_sku_item.cancel_time,
+        #             'assign_status_display': package_sku_item and package_sku_item.get_assign_status_display() or '',
+        #             'ware_by_display': package_sku_item and package_sku_item.get_ware_by_display() or '',
+        #             'out_sid': '',
+        #             'note': ''
+        #         })
+        #         break
         package_list.sort(key=lambda x: x['id'])
         return package_list
 
