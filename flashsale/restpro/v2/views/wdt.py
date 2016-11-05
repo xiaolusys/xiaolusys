2b588db023585f6b345a072f2d60c049dfd154e1
bo <bo.zhang@xiaolumeimei.com>
2016-11-05 11:01:34 +0800

    wdt

---
+++ flashsale/restpro/v2/views/wdt.py
@@ -0,0 +1,22 @@
+# encoding=utf8
+from datetime import date
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import (
+    authentication,
+    permissions,
+    exceptions,
+)
+
+from common.wdt import WangDianTong
+
+
+class WangDianTongViewSet(viewsets.ViewSet):
+    """
+    ## GET /rest/v2/wdt/logistics
+    """
+
+    def logistics(self, request, *args, **kwargs):
+        wdt = WangDianTong()
+        resp = wdt.receive_logistics(request)
+        return Response(resp)
