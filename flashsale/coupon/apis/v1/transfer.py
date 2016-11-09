73f63b2aad60410085a097df6f9edc9931184425
ahlinjie <1051631394@qq.com>
2016-11-09 20:40:12 +0800

    add api to save the transfer record for coupon release in admin page

---
+++ flashsale/coupon/apis/v1/transfer.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+__ALL__ = [
+    'create_coupon_transfer_record',
+]
+import datetime
+from ...models.transfer_coupon import CouponTransferRecord
+
+
+def create_present_coupon_transfer_record(customer, template, coupon_id, order_id):
+    # type: (Customer, CouponTemplate, int, int)
+    """创建赠送优惠券流通记录
+    """
+    to_mama = customer.get_charged_mama()
+    to_mama_nick = customer.nick
+    to_mama_thumbnail = customer.thumbnail
+
+    coupon_to_mama_id = to_mama.id
+    init_from_mama_id = to_mama.id
+
+    coupon_from_mama_id = 0
+    from_mama_thumbnail = 'http://7xogkj.com2.z0.glb.qiniucdn.com/222-ohmydeer.png?imageMogr2/thumbnail/60/format/png'
+    from_mama_nick = 'SYSTEM'
+
+    transfer_type = CouponTransferRecord.IN_BUY_COUPON
+    date_field = datetime.date.today()
+    transfer_status = CouponTransferRecord.DELIVERED
+    uni_key = "%s-%s-%s" % (to_mama.id, coupon_id, order_id)
+    order_no = 'present-%s' % order_id
+    coupon_value = int(template.value)
+    product_img = template.extras.get("product_img") or ''
+
+    try:
+        coupon = CouponTransferRecord(coupon_from_mama_id=coupon_from_mama_id, from_mama_thumbnail=from_mama_thumbnail,
+                                      from_mama_nick=from_mama_nick, coupon_to_mama_id=coupon_to_mama_id,
+                                      to_mama_thumbnail=to_mama_thumbnail, to_mama_nick=to_mama_nick,
+                                      coupon_value=coupon_value,
+                                      init_from_mama_id=init_from_mama_id, order_no=order_no, template_id=template.id,
+                                      product_img=product_img, coupon_num=1, transfer_type=transfer_type,
+                                      uni_key=uni_key, date_field=date_field, transfer_status=transfer_status)
+        coupon.save()
+    except Exception as e:
+        return e
