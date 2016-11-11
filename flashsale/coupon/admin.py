6723c3d8e874c81575d3ba1539cdef5d483591f5
Zifei Zhong <zfz@xiaolumeimei.com>
2016-11-11 21:43:40 +0800

    fix transfer_status

--- flashsale/coupon/admin.py
+++ flashsale/coupon/admin.py
@@ -109,7 +109,7 @@ class CouponTransferRecordAdmin(admin.ModelAdmin):
     def total_num(self, obj):
         res = CouponTransferRecord.objects.filter(
             date_field=obj.date_field,transfer_type=obj.transfer_type,
-            transfer_status=transfer_status).aggregate(n=Sum('coupon_num'))
+            transfer_status=obj.transfer_status).aggregate(n=Sum('coupon_num'))
         return res['n'] or 0
 
 admin.site.register(CouponTransferRecord, CouponTransferRecordAdmin)
