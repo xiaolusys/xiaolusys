5601e0b992c9424e16a2b288e413a0bb063c25e6
ahlinjie <1051631394@qq.com>
2016-11-15 13:36:57 +0800

    change activity pro update api

--- flashsale/pay/apis/v1/product.py
+++ flashsale/pay/apis/v1/product.py
@@ -1,1 +1,11 @@
-__author__ = 'jishu_linjie'
+# coding=utf-8
+__ALL__ = [
+    'get_modelproduct_by_id',
+]
+
+from ...models.product import ModelProduct
+
+
+def get_modelproduct_by_id(id):
+    # type: (int) -> ModelProduct
+    return ModelProduct.objects.get(id=id)
