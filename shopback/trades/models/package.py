b7bb98ea55c9fed0bece3d03bdaaacdb9f52373b
huangyan <763294911@qq.com>
2016-11-21 09:45:28 +0800

    [bugfix]hy-send-package-change-assign-num

--- shopback/trades/models/package.py
+++ shopback/trades/models/package.py
@@ -159,11 +159,12 @@ class PackageOrder(models.Model):
             sku_item.out_sid = self.out_sid
             sku_item.logistics_company_name = self.logistics_company.name
             sku_item.logistics_company_code = self.logistics_company.code
-            # sku_item.set_status_sent()
-            sku_item.assign_status = PackageSkuItem.FINISHED
-            sku_item.status = PSI_STATUS.SENT
-            sku_item.set_assign_status_time()
-            sku_item.save()
+            sku_item.set_status_sent()
+            sku_item.set_status_sent()
+            # sku_item.assign_status = PackageSkuItem.FINISHED
+            # sku_item.status = PSI_STATUS.SENT
+            # sku_item.set_assign_status_time()
+            # sku_item.save()
             sale_order = sku_item.sale_order
             sale_order.status = SaleOrder.WAIT_BUYER_CONFIRM_GOODS
             sale_order.consign_time = datetime.datetime.now()
