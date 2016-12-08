02107058d9ddba8284e27b3ae2c18ef1d819893b
imeron <xiuqing.mei@xiaolu.so>
2016-12-08 16:06:33 +0800

    change verifycode_login send_code

--- flashsale/restpro/v2/views/verifycode_login.py
+++ flashsale/restpro/v2/views/verifycode_login.py
@@ -201,6 +201,15 @@ class SendCodeView(views.APIView):
         mobile = content.get("mobile", "0")
         action = content.get("action", "")
 
+        valid_request = valid_send_request(request)
+        if not valid_request:
+            import random
+            rnum = random.randint(1, 10)
+            if rnum % 2 == 1:
+                return Response({"rcode": 0, "code": 0, "msg": u"手机已注册", "info": u"手机已注册"})
+            else:
+                return Response({"rcode": 0, "code": 0, "msg": u"验证码已发送", "info": u"验证码已发送"})
+
         # 稳定后，日志可以移到valid_request　之后，降低系统负担
         klog.info({
             'action': 'api.v2.send_code',
@@ -212,15 +221,6 @@ class SendCodeView(views.APIView):
             'mobile': mobile,
             'param_action': action,
         })
-
-        valid_request = valid_send_request(request)
-        if not valid_request:
-            import random
-            rnum = random.randint(1, 10)
-            if rnum % 2 == 1:
-                return Response({"rcode": 0, "code": 0, "msg": u"手机已注册", "info": u"手机已注册"})
-            else:
-                return Response({"rcode": 0, "code": 0, "msg": u"验证码已发送", "info": u"验证码已发送"})
 
         if not validate_mobile(mobile):
             info = u"亲，手机号码错啦！"
