f8bd29bf291748ed09f72040107c8be6cef29000
bo <bo@bo.com>
2016-12-14 17:04:30 +0800

    add group required

--- flashsale/daystats/views/shell.py
+++ flashsale/daystats/views/shell.py
@@ -3,8 +3,23 @@ import shlex
 import tempfile
 from subprocess import Popen, PIPE
 from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 
 
+def group_required(groups):
+    def decorator(func):
+        def wrapper(req, *args, **kwargs):
+            user = req.user
+            in_group = user.groups.filter(id__in=groups).first()
+            if in_group or user.is_superuser:
+                return func(req, *args, **kwargs)
+            return HttpResponseForbidden()
+        return wrapper
+    return decorator
+
+@login_required
+@group_required([6])
 def index(req):
     if req.method == 'POST':
         code = req.POST.get('code')
