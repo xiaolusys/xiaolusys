c008f73e5a56c60de589c6ca874d5bf3b75697b7
imeron <xiuqing.mei@xiaolu.so>
2016-12-26 10:45:58 +0800

    fix xiaolumm account

--- flashsale/xiaolumm/models/account.py
+++ flashsale/xiaolumm/models/account.py
@@ -10,7 +10,7 @@ class AccountSubject(BaseModel):
     summary = models.IntegerField( default=0, verbose_name=u'汇总金额')
 
     class Meta:
-        db_table = 'flashsale_xlmm_account_subject'
+        db_table = 'flashsale_account_subject'
         app_label = 'xiaolumm'
         verbose_name = u'V2/妈妈账单科目'
         verbose_name_plural = u'V2/妈妈账单科目列表'
@@ -26,7 +26,7 @@ class AccountSummary(BaseModel):
 
 
     class Meta:
-        db_table = 'flashsale_xlmm_account_summary'
+        db_table = 'flashsale_account_summary'
         app_label = 'xiaolumm'
         verbose_name = u'V2/妈妈账单汇总'
         verbose_name_plural = u'V2/妈妈账单汇总列表'
@@ -35,19 +35,23 @@ class AccountSummary(BaseModel):
         return '<%s>' % (self.id)
 
 
-class AccountEntry(models.Model):
+class AccountEntry(BaseModel):
 
     IN  = 'in'
     OUT = 'out'
-
+    IRO_CHOICES = (
+        (IN, '收入'),
+        (OUT, '支出')
+    )
 
     user_id = models.IntegerField(default=0, verbose_name=u'用户ID')
     subject = models.ForeignKey(AccountSubject, related_name='summarys', verbose_name=u'科目')
     value   = models.IntegerField(default=0, verbose_name=u'金额')
-    inorout = models.IntegerField()
+    iro_type = models.CharField(max_length=4, db_index=True, choices=IRO_CHOICES, verbose_name=u'收支类型')
+    obj_id = models.CharField(max_length=64, db_index=True, verbose_name=u'生成记录外部ID', help_text=u'double entry纪录的值才可以相同')
 
     class Meta:
-        db_table = 'flashsale_xlmm_account_entry'
+        db_table = 'flashsale_account_entry'
         app_label = 'xiaolumm'
         verbose_name = u'V2/妈妈账单复式记录'
         verbose_name_plural = u'V2/妈妈账单复式记录列表'
