25a3089423888d03a9333e2b862ae4ff6c61b730
imeron <xiuqing.mei@xiaolu.so>
2016-12-26 16:37:40 +0800

    fix xiaolumm accounts

--- flashsale/xiaolumm/models/account.py
+++ flashsale/xiaolumm/models/account.py
@@ -19,11 +19,24 @@ class AccountSubject(BaseModel):
         return '<%s>' % (self.id)
 
 class AccountSummary(BaseModel):
+    """ 该存档汇总记录只供参考 """
+    NOW_SUM   = 'now'
+    WEEK_SUM  = 'week'
+    MONTH_SUM = 'month'
+    YEAR_SUM  = 'year'
+
+    SUMMARY_TYPE_CHOICES = (
+        (NOW_SUM, u'实际发生值'),
+        (WEEK_SUM, u'周存档'),
+        (MONTH_SUM, u'月存档'),
+        (YEAR_SUM, u'年存档'),
+    )
 
     user_id = models.IntegerField(default=0, verbose_name=u'用户ID')
     subject = models.ForeignKey(AccountSubject, related_name='summarys', verbose_name=u'科目')
-    value   = models.IntegerField(default=0, verbose_name=u'金额')
-
+    value   = models.IntegerField(default=0, verbose_name=u'发生金额')
+    sum_type = models.CharField(max_length=8, db_index=True, verbose_name=u'存档类型')
+    sum_key  = models.CharField(max_length=32, db_index=True, verbose_name=u'存档标识', help_text=u'如([年]-[月]), 2016-12')
 
     class Meta:
         db_table = 'flashsale_account_summary'
@@ -48,7 +61,8 @@ class AccountEntry(BaseModel):
     subject = models.ForeignKey(AccountSubject, related_name='summarys', verbose_name=u'科目')
     value   = models.IntegerField(default=0, verbose_name=u'金额')
     iro_type = models.CharField(max_length=4, db_index=True, choices=IRO_CHOICES, verbose_name=u'收支类型')
-    obj_id = models.CharField(max_length=64, db_index=True, verbose_name=u'生成记录外部ID', help_text=u'double entry纪录的值才可以相同')
+    obj_id   = models.CharField(max_length=64, db_index=True, verbose_name=u'生成记录外部ID', help_text=u'double entry纪录的值才必须相同')
+    memo    = models.IntegerField(default=0, verbose_name=u'备注说明')
 
     class Meta:
         db_table = 'flashsale_account_entry'
