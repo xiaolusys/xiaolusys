be4e6bf171ba55b2ec4b3f4afad9ba3f655e0164
ahlinjie <1051631394@qq.com>
2017-01-06 17:56:00 +0800

    add the api to send the envlop in userbudget

--- flashsale/pay/views/envelop.py
+++ flashsale/pay/views/envelop.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import decimal
 from django.shortcuts import redirect
 from django.contrib import messages
 from django.db.models import Sum
@@ -6,9 +7,13 @@ from django.conf import settings
 from django.views.generic import View
 
 from rest_framework.renderers import JSONRenderer
-from core.options import log_action, ADDITION, CHANGE
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from flashsale.pay.models import Envelop
+from core.options import log_action, ADDITION, CHANGE
+from flashsale.pay.models import Envelop, BudgetLog, UserBudget
+
 
 class EnvelopConfirmSendView(View):
     renderer_classes = (JSONRenderer,)
@@ -41,3 +46,55 @@ class EnvelopConfirmSendView(View):
         messages.add_message(request, messages.INFO, u'已成功发送 %s 个红包，总金额：%s！' % (envelop_count, total_amount / 100.0))
 
         return redirect(origin_url)
+
+
+class SendBudgetEnvelopAPIView(APIView):
+    """发送红包: 创建用户budget_log记录
+    """
+    queryset = UserBudget.objects.all()
+    renderer_classes = (JSONRenderer,)
+    permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser, permissions.DjangoModelPermissions)
+
+    def get_budget_data(self, customer_id):
+        # type: (int) -> UserBudget
+        budget = self.queryset.filter(user_id=customer_id).first()
+        data = {'id': budget.id,
+                'customer_id': customer_id,
+                'nick': budget.user.nick,
+                'mobile': budget.user.mobile,
+                'cash': budget.budget_cash}
+        return data
+
+    def get(self, request):
+        # type: (HttpRequest) -> Response
+        """获取用户钱包信息
+        """
+        content = request.POST or request.data
+        customer_id = content.get('customer_id') or None
+        customer_id = int(customer_id)
+        data = self.get_budget_data(customer_id)
+        return Response({'code': 0, 'info': 'success', 'data': data})
+
+    def post(self, request):
+        # type: (HttpRequest) -> Response
+        """发送红包
+        """
+        content = request.POST or request.data
+        customer_id = content.get('customer_id') or None
+        customer_id = int(customer_id)
+        amount = content.get('amount') or '0'
+        memo = content.get('memo') or None
+
+        flow_amount = int(decimal.Decimal(amount) * 100)  # 以分为单位
+        if flow_amount == 0:
+            return Response({'code': 1, 'info': '红包金额不能为0', 'data': self.get_budget_data(customer_id)})
+
+        # 添加红包钱包记录
+        budget_log = BudgetLog.create(customer_id=int(customer_id),
+                                      budget_type=BudgetLog.BUDGET_IN,
+                                      flow_amount=flow_amount,
+                                      budget_log_type=BudgetLog.BG_ENVELOPE)
+        log_action(request.user, budget_log, ADDITION, u'赠送红包 %s' % memo)
+
+        data = self.get_budget_data(customer_id)
+        return Response({'code': 0, 'info': 'success', 'data': data})
