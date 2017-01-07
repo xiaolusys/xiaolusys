f822491d40da694494239450bdb5a5c7499d90b0
wulei <crackwl@163.com>
2017-01-07 18:06:43 +0800

    show transfer record template name

--- flashsale/restpro/v2/serializers/coupon_serializers.py
+++ flashsale/restpro/v2/serializers/coupon_serializers.py
@@ -8,16 +8,21 @@ from flashsale.coupon.models import (
 
 
 class CouponTransferRecordSerializer(serializers.ModelSerializer):
-    
+    template_name = serializers.SerializerMethodField()
     class Meta:
         model = CouponTransferRecord
         fields = ('id', 'coupon_from_mama_id', 'from_mama_thumbnail', 'from_mama_nick', 'coupon_to_mama_id',
                   'to_mama_thumbnail', 'to_mama_nick', 'template_id', 'product_img', 'coupon_value', 'coupon_num',
                   'transfer_type', 'transfer_status', 'status', 'uni_key', 'date_field', 'month_day', 'elite_level',
-                  'product_model_id',
+                  'product_model_id', 'template_name',
                   'hour_minute', 'transfer_status_display', 'is_cancelable', 'is_processable', 'modified', 'created')
 
 
-
+    def get_template_name(self, obj):
+        from flashsale.coupon.models import CouponTemplate
+        ct = CouponTemplate.objects.filter(id=obj.template_id).first()
+        if ct:
+            return ct.title
+        return ''
 
 
