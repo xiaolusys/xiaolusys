497bd55b1306ffb9a5859b7b2b84ac39cf6a3cfa
ahlinjie <1051631394@qq.com>
2017-01-11 13:41:58 +0800

    add the raise in confirm_sign_order func if the order status is error

--- flashsale/pay/models/trade.py
+++ flashsale/pay/models/trade.py
@@ -1120,6 +1120,8 @@ class SaleOrder(PayBaseModel):
 
     def confirm_sign_order(self):
         """确认签收 修改该订单状态到 确认签收状态"""
+        if self.status != SaleOrder.WAIT_BUYER_CONFIRM_GOODS:
+            raise Exception('没有发货不能签收')
         self.status = SaleOrder.TRADE_BUYER_SIGNED
         self.sign_time = datetime.datetime.now()
         self.save(update_fields=['status', 'sign_time'])
