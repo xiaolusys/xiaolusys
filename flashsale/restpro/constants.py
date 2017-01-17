3dadbf1d629f1c29fd467a03d21bf0b34b9eda47
wulei <crackwl@163.com>
2017-01-17 22:17:41 +0800

    add xiaolucoin pay check

--- flashsale/restpro/constants.py
+++ flashsale/restpro/constants.py
@@ -7,16 +7,19 @@ type:0,表示折扣优惠;1,表示虚拟支付,如余额;2,表示实际支付；
 ETS_APPCUT = '1'
 ETS_COUPON = '2'
 ETS_BUDGET = '3'
+ETS_XIAOLUCOIN = '4'
 
 DISCOUNT = 0
 BUDGET = 1
 REALPAY = 2
+XIAOLUCOIN = 3
 
 PAY_EXTRAS = {
     ETS_APPCUT: {'pid': 1, 'type': DISCOUNT, 'value': 2, 'name': 'APP支付减2元'}, #现取消
     ETS_COUPON: {'pid': 2, 'type': DISCOUNT, 'use_coupon_allowed': 1, 'value': 2, 'name': '优惠券'},
     # 1,能使用优惠券,0,不能使用,value:2兼容v1版本接口0
     ETS_BUDGET: {'pid': 3, 'type': BUDGET, 'value': 0, 'name': '余额支付' ,'use_budget_allowed':1 ,'channel':'budget'},
+    ETS_XIAOLUCOIN: {'pid': 4, 'type': XIAOLUCOIN, 'value': 0, 'name': '小鹿币支付' ,'use_coin_allowed':1 ,'channel':'xiaolucoin'},
 }
 
 SHARE_LINK = ""
