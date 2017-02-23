830ca78120efac264cca60fa54cbfb081770f73a
wulei <crackwl@163.com>
2017-02-23 10:45:16 +0800

    fix boutique check bug

---
+++ shopback/apis/v1/product.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import
+
+__ALL__ = [
+    'get_product_by_id',
+]
+
+from shopback.items.models.product import Product
+
+
+def get_product_by_id(id):
+    # type: (int) -> Product
+    return Product.objects.get(id=id)
+
+
+
