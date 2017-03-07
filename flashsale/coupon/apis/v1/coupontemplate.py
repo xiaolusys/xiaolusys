b5cb4ce399abf88dce3c89883bb0a66ec490768a
wulei <crackwl@163.com>
2017-03-07 15:36:03 +0800

    list coupon exchg modelid

--- flashsale/coupon/apis/v1/coupontemplate.py
+++ flashsale/coupon/apis/v1/coupontemplate.py
@@ -10,3 +10,13 @@ __ALL__ = [
 def get_coupon_template_by_id(id):
     # type: (int) -> CouponTemplate
     return CouponTemplate.objects.get(id=id)
+
+
+def get_boutique_coupon_modelid_by_templateid(id):
+    from flashsale.coupon.models import CouponTemplate
+    ct = CouponTemplate.objects.filter(id=id).first()
+    if ct:
+        product_model_id = ct.extras.get("product_model_id")
+        return product_model_id
+    return None
+
