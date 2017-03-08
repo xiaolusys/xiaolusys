b96cb5cdf350d63582754ad319635b34952dbbd9
imeron <xiuqing.mei@xiaolu.so>
2017-03-08 16:33:20 +0800

    fix .drone.yml and outware auth

--- outware/models/pms.py
+++ outware/models/pms.py
@@ -1,2 +1,11 @@
 # coding: utf8
 from __future__ import absolute_import, unicode_literals
+
+from django.db import models
+from core.models import BaseModel
+
+
+class OutwareSupplier(BaseModel):
+
+    outware_account = models.ForeignKey('outware.OutwareAccount', verbose_name=u'关联账号')
+
