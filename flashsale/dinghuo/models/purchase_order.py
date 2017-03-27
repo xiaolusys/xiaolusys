3cb81a96223ffef33393841843655eace4a9cb83
huangyan <763294911@qq.com>
2017-03-27 18:01:37 +0800

    [bugfix]hy-export-supplier-skucode

--- flashsale/dinghuo/models/purchase_order.py
+++ flashsale/dinghuo/models/purchase_order.py
@@ -785,7 +785,7 @@ class OrderList(models.Model):
                               '', MENGNUO_DATA.get(int(p.sku_id), ''), p.product_sku.product.name,
                               str(p.product_sku.color_size), str(p.num), str(p.product_sku.cost), str(p.num * p.product_sku.cost)])
         elif format == 'third_package':
-            columns = [u'订单号', u'产品条码', u'订单状态', u'买家id', u'子订单编号', u'供应商编码', u'买家昵称', u'商品名称', u'产品规格', u'商品单价',
+            columns = [u'订单号', u'产品条码', u'订单状态', u'买家id', u'子订单编号', u'供应商编码',  u'供应商sku编码', u'买家昵称', u'商品名称', u'产品规格', u'商品单价',
                        u'商品数量',
                        u'商品总价', u'运费', u'购买优惠信息', u'总金额', u'买家购买附言', u'收货人姓名', u'收货地址', u'邮编',
                        u'收货人手机', u'收货人电话', u'买家选择运送方式', u'卖家备忘内容', u'订单创建时间', u'付款时间', u'物流公司', u'物流单号', u'发货附言',
@@ -799,7 +799,7 @@ class OrderList(models.Model):
                 o = p.package_order
                 saleproduct = p.product_sku.product.get_sale_product()
                 items.append([str(o.pid), '', o.sys_status, str(o.buyer_id), str(p.id),
-                              saleproduct.supplier_sku if saleproduct else '', str(o.buyer_nick),
+                              saleproduct.supplier_sku if saleproduct else '', str(p.product_sku.supplier_skucode if p.product_sku.supplier_skucode else ''), str(o.buyer_nick),
                               str(p.product_sku.product.name), str(p.product_sku.properties_name),
                               str(p.product_sku.cost), str(p.num), str(p.num * p.product_sku.cost), '0', '0', '0', '', str(o.receiver_name),
                               str(o.receiver_address_detail), '', o.receiver_mobile, '', '', '',
