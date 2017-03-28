5c151c3d3bf74df16369fbc803bc8b21a758b388
imeron <xiuqing.mei@xiaolu.so>
2017-03-28 11:14:10 +0800

    fix restpro ocr

--- flashsale/restpro/v2/views/ocr.py
+++ flashsale/restpro/v2/views/ocr.py
@@ -40,15 +40,32 @@ class OcrIndentifyViewSet(viewsets.GenericViewSet):
         card_base64 = data.get('card_base64') or ''
 
         if not card_base64 or not card_side:
-            return Response({'code':1, 'info': 'object_id,side和card_base64参数不能为空'})
+            logger.info({
+                'action': 'idcard_indentify',
+                'action_time': datetime.datetime.now(),
+                'code': 1,
+                'message': 'side和card_base64参数不能为空',
+            })
+            return Response({'code':1, 'info': 'side和card_base64参数不能为空'})
 
         try:
             resp = idcard.identify(card_side, card_base64)
         except Exception, exc:
-            logger.error(str(exc), exc_info=True)
+            logger.error({
+                'action': 'idcard_indentify',
+                'action_time': datetime.datetime.now(),
+                'code': 2,
+                'message': '请求异常:%s'%str(exc),
+            }, exc_info=True)
             return Response({'code': 2, 'info': '请求异常:%s'%str(exc)})
 
         if not resp['success']:
+            logger.info({
+                'action': 'idcard_indentify',
+                'action_time': datetime.datetime.now(),
+                'code': 3,
+                'message': '未识别成功, 请调整位置重新拍摄',
+            })
             return Response({'code': 3, 'info': '未识别成功, 请调整位置重新拍摄'})
 
         center  =  resp['face_rect']['center']
@@ -59,10 +76,24 @@ class OcrIndentifyViewSet(viewsets.GenericViewSet):
             img = Image.open(StringIO(base64.b64decode(card_base64)))
             img_size = img.size
             if (img_size[0] - h_size_x - center['x']) < 0.05 * img_size[0] or h_size_y < 10 or 20 < ract_angle % 90 < 60:
+                logger.info({
+                    'action': 'idcard_indentify',
+                    'action_time': datetime.datetime.now(),
+                    'code': 4,
+                    'message': '未识别成功: img_size=(%s,%s)'%(img_size[0], img_size[1]),
+                    'data': resp,
+                })
                 return Response({'code': 4, 'info': '请在明光下拍摄，并调整角度至边框平行'})
 
             if (h_size_x * h_size_y * 1.0) / (img_size[0] * img_size[1]) < 0.015:
-                return Response({'code': 5, 'info': '请调整拍摄距离，保证证件占据屏幕80%大小'})
+                logger.info({
+                    'action': 'idcard_indentify',
+                    'action_time': datetime.datetime.now(),
+                    'code': 5,
+                    'message': '未识别成功: img_size=(%s,%s)' % (img_size[0], img_size[1]),
+                    'data': resp,
+                })
+                return Response({'code': 5, 'info': '请调整拍摄距离让身份证居中'})
 
         customer = Customer.getCustomerByUser(request.user)
         resp.update({
