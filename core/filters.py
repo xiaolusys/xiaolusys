68eccc85b985bad9f779171eab82775b95709e2d
huangyan <763294911@qq.com>
2017-03-31 18:33:31 +0800

    [bugfix]hy-field-condition

--- core/filters.py
+++ core/filters.py
@@ -169,9 +169,13 @@ class ConditionFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         condition = {}
         for k in view.search_fields:
-            v = request.GET.get(k)
-            if type(k) is tuple and k[1] is list and v:
+            if type(k) is tuple and k[1] is list:
+                v = request.GET.get(k[0])
                 v = v.split(',')
-            if v:
-                condition[k] = v
+                if v:
+                    condition[k[0]] = v
+            else:
+                v = request.GET.get(k)
+                if v:
+                    condition[k] = v
         return queryset.filter(**condition)
