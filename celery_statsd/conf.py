cf31fe76d14d3f4f7cfb9853cd4de57ef3218b08
imeron <xiuqing.mei@xiaolu.so>
2017-04-07 20:38:33 +0800

    fix flower queue lenght uri

--- celery_statsd/conf.py
+++ celery_statsd/conf.py
@@ -2,6 +2,6 @@
 from __future__ import absolute_import, unicode_literals
 
 
-FLOWER_QUEUE_LENGTH_API = 'http://flower.xiaolumm.com/api/queues/length'
+FLOWER_QUEUE_LENGTH_API = 'http://120.27.225.142:30680/api/queues/length'
 FLOWER_USERNAME = 'xiaolu'
 FLOWER_PASSWORD = '2016xiaoluyear'
