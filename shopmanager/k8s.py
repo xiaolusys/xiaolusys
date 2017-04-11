338294b1030aa126e25130dda3330719d4b0eb82
imeron <xiuqing.mei@xiaolu.so>
2017-04-11 22:52:27 +0800

    fix change k8s

--- shopmanager/k8s.py
+++ shopmanager/k8s.py
@@ -29,9 +29,9 @@ SITE_URL = 'http://staging.xiaolumm.com/'
 M_SITE_URL = 'http://staging.xiaolumm.com'
 
 MYSQL_HOST = 'rm-bp17ea269uu21f9i1o.mysql.rds.aliyuncs.com'
-MYSQL_AUTH = 'Xiaolu_test123'
+MYSQL_AUTH = os.environ.get('MYSQL_AUTH')
 
-REDIS_HOST = '55a32ec47c8d41f7.m.cnhza.kvstore.aliyuncs.com:6379'
+REDIS_HOST = 'redis.default.svc.cluster.local:6379'
 REDIS_AUTH = os.environ.get('REDIS_AUTH')
 # REDIS_AUTH = ''
 
