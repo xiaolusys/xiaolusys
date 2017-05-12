eea043e149ad737c365081c51fb39e16afdae18e
hui.deng3 <hlkysf@163.com>
2017-05-12 14:56:00 +0800

    dh

--- shopback/items/models/stats.py
+++ shopback/items/models/stats.py
@@ -164,6 +164,8 @@ class SkuStock(models.Model):
     @property
     def wait_assign_num(self):
         if self.product.type == 0:
+            if self.sold_num - self.assign_num - self.post_num < 0:
+                return 0
             return self.sold_num - self.assign_num - self.post_num
         else:
             return 0
