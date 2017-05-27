79e4e8a691ebdddf14c1f97f9738b284d7fd6825
hui.deng3 <hlkysf@163.com>
2017-05-27 11:40:56 +0800

    dh

--- shopback/trades/management/commands/set_status_virtual_booked.py
+++ shopback/trades/management/commands/set_status_virtual_booked.py
@@ -8,7 +8,7 @@ from shopback.dinghuo.models.purchase_order import OrderList
 from django.conf import settings
 from django.contrib.auth.models import User
 
-##用法 python manage.py gen_arrangement -id 123
+##用法 python manage.py set_status_virtual_booked -id 123 -unikey 1232
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
@@ -16,6 +16,13 @@ class Command(BaseCommand):
             type=str,
         )
 
+        parser.add_argument(
+            '-unikey', '--purchase_order_unikey', action='store', dest='purchase_order_unikey',
+            type=str,
+        )
+
     def handle(self, *args, **options):
         id = options.get("packageskuitem_id")
-        PackageSkuItem.objects.filter(id=id).first().gen_arrangement()
+        purchase_order_unikey = options.get('purchase_order_unikey')
+        print id,purchase_order_unikey
+        PackageSkuItem.objects.filter(id=id).first().set_status_virtual_booked(purchase_order_unikey)
