7f02d426f5c5c8825fa46ad78aa0b47bbd3a125a
hui.deng3 <hlkysf@163.com>
2017-06-03 11:17:33 +0800

    清理qiyue家居

--- shopback/trades/management/commands/reset_uni_sku_stock.py
+++ shopback/trades/management/commands/reset_uni_sku_stock.py
@@ -17,10 +17,15 @@ class Command(BaseCommand):
     #     )
 
     def handle(self, *args, **options):
-        ss = SkuStock.objects.filter(product__category__in=[8,5,9,12,13,14,15,16,17,18,19,20,21,22,25,26,27,59,60,61,62,63,64])
+        # ss = SkuStock.objects.filter(product__category__in=[8,5,9,12,13,14,15,16,17,18,19,20,21,22,25,26,27,59,60,61,62,63,64,])
+        ss = SkuStock.objects.filter(product_id__in=SkuStock.filter_by_supplier(28802))
         for i in ss:
             if i.realtime_quantity != 0:
                 i.history_quantity = i.history_quantity - i.realtime_quantity
                 i.save()
+        # for i in ss:
+        #     if i.realtime_quantity != 0:
+        #         i.history_quantity = i.history_quantity - i.realtime_quantity
+        #         i.save()
 
 
