4f133a9878cfde72612fafd1ad07c07db8d9126c
imeron <xiuqing.mei@xiaolu.so>
2017-06-03 11:42:10 +0800

    fix task_periodic_flush_elasticsearch_logging

--- celery_statsd/tasks.py
+++ celery_statsd/tasks.py
@@ -33,10 +33,15 @@ def task_celery_queue_message_statsd():
 def task_periodic_flush_elasticsearch_logging(remain_days=7):
     today = datetime.datetime.now()
     try:
-        for i in (remain_days, 10):
+        for i in (remain_days, 15):
             dt = today - datetime.timedelta(days=i)
             req_url = 'http://{}/{}'.format(settings.ELASTICSEARCH_LOGGING_HOST, '-' + dt.strftime('%Y.%m.%d'))
             resp = requests.delete(req_url)
+            if resp.status_code > 400 and resp.status_code != 404:
+                logger.error('delete-es-logging-error:%s, %s' % (req_url, resp.text))
+
+            req_url1 = 'http://{}/{}'.format(settings.ELASTICSEARCH_LOGGING_HOST, 'logstash-' + dt.strftime('%Y.%m.%d'))
+            resp = requests.delete(req_url1)
             if resp.status_code > 400 and resp.status_code != 404:
                 logger.error('delete-es-logging-error:%s, %s' % (req_url, resp.text))
     except Exception, exc:
