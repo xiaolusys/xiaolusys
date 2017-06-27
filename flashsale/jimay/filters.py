b7a6a83bbfda1057f9bfbd881311f442a377412c
imeron <xiuqing.mei@xiaolu.so>
2017-06-27 09:52:54 +0800

    fix AgentInviteCountFieldListFilter

--- flashsale/jimay/filters.py
+++ flashsale/jimay/filters.py
@@ -1,7 +1,7 @@
 # coding: utf8
 from __future__ import absolute_import, unicode_literals
 
-import urllib
+import sys
 from django.contrib.admin.options import IncorrectLookupParameters
 from django.core.exceptions import ImproperlyConfigured, ValidationError
 from django.utils.encoding import smart_unicode, smart_text
@@ -13,11 +13,13 @@ from .models import JimayAgentStat
 class AgentInviteCountFieldListFilter(FieldListFilter):
     def __init__(self, field, request, params, model, model_admin, field_path):
         self.lookup_kwarg_gte = '%s__gte' % field_path
-        self.lookup_kwarg_lt = '%s__lt' % field_path
+        self.lookup_kwarg_lt = '%s__lte' % field_path
         gte = request.GET.get(self.lookup_kwarg_gte)
-        lt  = request.GET.get(self.lookup_kwarg_lt)
+        lte  = request.GET.get(self.lookup_kwarg_lt)
         self.lookup_kwarg_gte_val = gte and gte.isdigit() and float(gte)
-        self.lookup_kwarg_lt_val  = lt and lt.isdigit() and float(lt)
+        if lte.lower() == 'inf':
+            lte = str(sys.maxint)
+        self.lookup_kwarg_lte_val = lte and lte.isdigit() and float(lte)
         field.verbose_name = '邀请人数区间'
         super(AgentInviteCountFieldListFilter, self).__init__(
             field, request, params, model, model_admin, field_path)
@@ -26,11 +28,11 @@ class AgentInviteCountFieldListFilter(FieldListFilter):
         return [self.lookup_kwarg_gte, self.lookup_kwarg_lt]
 
     def queryset(self, request, queryset):
-        if not (self.lookup_kwarg_gte_val and self.lookup_kwarg_lt_val):
+        if not (self.lookup_kwarg_gte_val and self.lookup_kwarg_lte_val):
             return queryset
         agent_ids = list(JimayAgentStat.objects.filter(
             direct_invite_num__gte=self.lookup_kwarg_gte_val,
-            direct_invite_num__lt=self.lookup_kwarg_lt_val,
+            direct_invite_num__lte=self.lookup_kwarg_lte_val,
         ).values_list('agent', flat=True))
         try:
             return queryset.filter(id__in=agent_ids)
@@ -46,20 +48,20 @@ class AgentInviteCountFieldListFilter(FieldListFilter):
             'display': _('All')
         }
         count_ranges =(
-            (1, 10),
-            (11, 20),
-            (21, 30),
-            (31, 40),
-            (41, 50),
-            (51, 60),
-            (61, 'Inf'),
+            (1, 9),
+            (10, 19),
+            (20, 29),
+            (30, 39),
+            (40, 49),
+            (50, 59),
+            (60, 'Inf'),
         )
 
         for lookup in count_ranges:
             title = '%s ~ %s' % lookup
             yield {
                 'selected': smart_text(lookup[0]) == self.lookup_kwarg_gte_val
-                            and smart_text(lookup[1]) == self.lookup_kwarg_lt_val,
+                            and smart_text(lookup[1]) == self.lookup_kwarg_lte_val,
                 'query_string': changelist.get_query_string(
                     {self.lookup_kwarg_gte: lookup[0], self.lookup_kwarg_lt: lookup[1]},
                     [self.lookup_kwarg_gte, self.lookup_kwarg_lt]
